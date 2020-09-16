e6ffa94f259e9783986ae65410ca7e32c32007b4
synchrovision <trinitybluster@yahoo.co.jp>
2020-09-16 10:41:07 +0900

    change :wrench: change layout of Catpow panel

--- functions/catpow/panel.php
+++ functions/catpow/panel.php
@@ -1,21 +1,16 @@
 <?php namespace Catpow; ?>
 <ul>
 	<li>
-		<h3>テーマ</h3>
-		<p>新規のCatpow対応テーマを作成、または設定ファイルからテーマのファイルを生成します。</p>
+		<h3>テーマ作成</h3>
+		<p>新規のCatpow対応テーマを作成します。</p>
 		<ul class="buttons">
 			<li>
 				<?php button('<i class="fas fa-folder-plus"></i>新規テーマ作成','action','replace',['setup_type'=>'theme']);?>
-				<small>新規のcatpowテーマを作成します</small>
-			</li>
-			<li>
-				<?php button('<i class="fas fa-file-code"></i>テンプレート生成','action','replace',['setup_type'=>'template']);?>
-				<small>設定ファイルからテンプレートファイルを生成します。</small>
 			</li>
 		</ul>
 	</li>
 	<li>
-		<h3>データ</h3>
+		<h3>セットアップ</h3>
 		<p>テーマの設定ファイルの設定をWordPressの投稿・設定に反映します。</p>
 		<ul class="buttons">
 			<li>
@@ -30,17 +25,18 @@
 				<?php button('<i class="fas fa-bars"></i>メニュー生成','action','replace',['setup_type'=>'menu']);?>
 				<small>$menu_datasの設定を元にメニューを更新します</small>
 			</li>
+			<li>
+				<?php button('<i class="fas fa-database"></i>データベース更新','action','replace',['setup_type'=>'database']);?>
+				<small>拡張データベースを生成・削除・更新します。</small>
+			</li>
 		</ul>
 	</li>
 	<li>
-		<h3>データベース</h3>
-		<p>
-			system_config.phpの設定を元にデータベーステーブルを生成・削除・更新します。
-			この操作はsystem_config.phpにおいて削除されたデータベースのテーブル、あるいはカラムを削除します。
-		</p>
+		<h3>テンプレート生成</h3>
+		<p>テーマの設定ファイルからテンプレートを生成します。</p>
 		<ul class="buttons">
 			<li>
-				<?php button('<i class="fas fa-database"></i>データベース更新','action','replace',['setup_type'=>'database']);?>
+				<?php button('<i class="fas fa-file-code"></i>テンプレート生成','action','replace',['setup_type'=>'template']);?>
 			</li>
 		</ul>
 	</li>
