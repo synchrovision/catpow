8e46d5e1fb1c17854f1216b537d85624298c9edf
synchrovision <trinitybluster@yahoo.co.jp>
2020-09-25 20:42:55 +0900

    feat :+1: adjust position of texts of ChartText component

--- components/ChartText/component.js
+++ components/ChartText/component.js
@@ -1,40 +1,61 @@
 Catpow.ChartText = function (props) {
-	var keys = ['title', 'unit', 'rowTitle', 'rowUnit', 'colTitle', 'colUnit'];
-	var ds = ['row', 'col'];
+	var keys = ['title', 'unit', 'rowTitle', 'rowUnit'];
+	var rows = props.rows,
+	    cols = props.cols,
+	    _props$width = props.width,
+	    width = _props$width === undefined ? 300 : _props$width,
+	    _props$height = props.height,
+	    height = _props$height === undefined ? 300 : _props$height,
+	    _props$padding = props.padding,
+	    padding = _props$padding === undefined ? 50 : _props$padding,
+	    total = props.total;
+
+
+	var graphHeight = height - padding * 2,
+	    graphWidth = width - padding * 2,
+	    graphOrg = { x: padding, y: height - padding };
+	var rowStep = graphWidth / rows.length;
+	var legendItemWidths = cols.map(function (col) {
+		return parseInt(20 + col.label.length * 12);
+	});
+	var legendWidth = legendItemWidths.reduce(function (val, w) {
+		return w + val;
+	}, 0);
+	var legendItemPos = [{ x: width / 2 - legendWidth / 2, y: height - 15 }];
+	legendItemWidths.map(function (w, c) {
+		legendItemPos.push({
+			x: legendItemPos[c].x + w,
+			y: legendItemPos[c].y
+		});
+	});
 
 	props.pos = Object.assign({
 		title: function title() {
-			return { x: 150, y: 10 };
+			return { x: width / 2, y: padding / 2 };
 		},
 		unit: function unit() {
-			return { x: 100, y: 100 };
+			return { x: width - padding / 2, y: padding / 2 };
 		},
 		rowTitle: function rowTitle() {
-			return { x: 100, y: 100 };
+			return { x: padding / 2, y: height - padding + 15 };
 		},
 		rowUnit: function rowUnit() {
-			return { x: 100, y: 100 };
-		},
-		colTitle: function colTitle() {
-			return { x: 100, y: 100 };
-		},
-		colUnit: function colUnit() {
-			return { x: 100, y: 100 };
+			return { x: width - padding / 2, y: height - padding + 15 };
 		},
 		rowLabel: function rowLabel(r) {
-			return { x: 50, y: r * 20 };
+			return { x: r * rowStep + rowStep / 2 + padding, y: height - padding + 15 };
 		},
-		colLabel: function colLabel(c) {
-			return { x: c * 20, y: 20 };
+		legend: function legend(c) {
+			return legendItemPos[c];
 		},
 		val: function val(r, c) {
-			return { x: r * 10, y: c * 10 };
+			return { x: r * rowStep + rowStep / 2, y: c };
 		}
 	}, props.pos);
 
 	return wp.element.createElement(
 		'g',
-		{ className: 'texts' },
+		{ className: 'ChartText' },
 		keys.map(function (key) {
 			var pos = props.pos[key]();
 			return wp.element.createElement(
@@ -43,20 +64,41 @@ Catpow.ChartText = function (props) {
 				props[key]
 			);
 		}),
-		ds.map(function (d) {
-			return wp.element.createElement(
-				'g',
-				{ className: d + 'Label' },
-				props[d + 's'].map(function (item, i) {
-					var pos = props.pos[d + 'Label'](i);
-					return wp.element.createElement(
+		wp.element.createElement(
+			'g',
+			{ className: 'rowLabel' },
+			props.rows.map(function (item, i) {
+				var pos = props.pos['rowLabel'](i);
+				return wp.element.createElement(
+					'text',
+					{ className: item.classes.replace('row', ''), x: pos.x, y: pos.y },
+					item.label
+				);
+			})
+		),
+		wp.element.createElement(
+			'g',
+			{ className: 'ledgend' },
+			props.cols.map(function (col, c) {
+				var pos = props.pos.legend(c);
+				return wp.element.createElement(
+					'g',
+					{ className: 'ledgendItem' },
+					wp.element.createElement('rect', {
+						className: cols[c].classes.replace('col', 'ledgendRect'),
+						x: pos.x,
+						y: pos.y - 6,
+						width: 12,
+						height: 12
+					}),
+					wp.element.createElement(
 						'text',
-						{ className: item.classes.replace(d, ''), x: pos.x, y: pos.y },
-						item.label
-					);
-				})
-			);
-		}),
+						{ className: 'ledgendText', x: pos.x + 15, y: pos.y },
+						col.label
+					)
+				);
+			})
+		),
 		wp.element.createElement(
 			'g',
 			{ className: 'vals' },
--- components/ChartText/component.jsx
+++ components/ChartText/component.jsx
@@ -1,39 +1,63 @@
 ﻿Catpow.ChartText=function(props){
-	const keys=['title','unit','rowTitle','rowUnit','colTitle','colUnit'];
-	const ds=['row','col'];
+	const keys=['title','unit','rowTitle','rowUnit'];
+	var {rows,cols,width=300,height=300,padding=50,total}=props;
+	
+	var graphHeight=height-padding*2, graphWidth=width-padding*2,graphOrg={x:padding,y:height-padding};
+	var rowStep=graphWidth/rows.length;
+	const legendItemWidths=cols.map((col)=>parseInt(20+col.label.length*12));
+	const legendWidth=legendItemWidths.reduce((val,w)=>w+val,0);
+	var legendItemPos=[{x:width/2-legendWidth/2,y:height-15}];
+	legendItemWidths.map((w,c)=>{
+		legendItemPos.push({
+			x:legendItemPos[c].x+w,
+			y:legendItemPos[c].y,
+		});
+	});
 	
 	props.pos=Object.assign({
-		title:()=>{return {x:150,y:10}},
-		unit:()=>{return {x:100,y:100}},
-		rowTitle:()=>{return {x:100,y:100}},
-		rowUnit:()=>{return {x:100,y:100}},
-		colTitle:()=>{return {x:100,y:100}},
-		colUnit:()=>{return {x:100,y:100}},
-		rowLabel:(r)=>{return {x:50,y:r*20}},
-		colLabel:(c)=>{return {x:c*20,y:20}},
-		val:(r,c)=>{return {x:r*10,y:c*10}},
+		title:()=>{return {x:width/2,y:padding/2}},
+		unit:()=>{return {x:width-padding/2,y:padding/2}},
+		rowTitle:()=>{return {x:padding/2,y:height-padding+15}},
+		rowUnit:()=>{return {x:width-padding/2,y:height-padding+15}},
+		rowLabel:(r)=>{return {x:r*rowStep+rowStep/2+padding,y:height-padding+15}},
+		legend:(c)=>{return legendItemPos[c]},
+		val:(r,c)=>{return {x:r*rowStep+rowStep/2,y:c}},
 	},props.pos);
 	
+	
 	return (
-		<g className='texts'>
+		<g className='ChartText'>
 			{keys.map((key)=>{
 				let pos=props.pos[key]();
 				return (
 					<text className={key} x={pos.x} y={pos.y}>{props[key]}</text>
 				);
 			})}
-			{ds.map((d)=>{
-				return (
-					<g className={d+'Label'}>
-						{props[d+'s'].map((item,i)=>{
-							let pos=props.pos[d+'Label'](i);
-							return (
-								<text className={item.classes.replace(d,'')} x={pos.x} y={pos.y}>{item.label}</text>
-							);
-						})}
-					</g>
-				);
-			})}
+			<g className={'rowLabel'}>
+				{props.rows.map((item,i)=>{
+					let pos=props.pos['rowLabel'](i);
+					return (
+						<text className={item.classes.replace('row','')} x={pos.x} y={pos.y}>{item.label}</text>
+					);
+				})}
+			</g>
+			<g className="ledgend">
+				{props.cols.map((col,c)=>{
+					const pos=props.pos.legend(c);
+					return (
+						<g className="ledgendItem">
+							<rect
+								className={cols[c].classes.replace('col','ledgendRect')}
+								x={pos.x}
+								y={pos.y-6}
+								width={12}
+								height={12}
+							/>
+							<text className="ledgendText" x={pos.x+15} y={pos.y}>{col.label}</text>
+						</g>
+					);
+				})}
+			</g>
 			<g className="vals">
 				{props.rows.map((row,r)=>{
 					return row.vals.map((val,c)=>{
