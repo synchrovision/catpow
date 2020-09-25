a15faad4d75f031b236b8435aeb121827555d387
synchrovision <trinitybluster@yahoo.co.jp>
2020-09-25 16:44:42 +0900

    feat :+1: input progress with ui

--- classes/meta/progress.php
+++ classes/meta/progress.php
@@ -1,8 +1,8 @@
 <?php
 namespace Catpow\meta;
 
-class progress extends number{
-	
+class Progress extends UI{
+	public static $defaultParam=['max'=>100,'step'=>5];
 	public static function output($meta,$prm){
 		return sprintf(
 			'<progress value="%d" max="%d"/>%1$d</progress>',
