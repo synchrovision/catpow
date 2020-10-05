e212e72972cb60cff4852ff1c17951d50971aa1a
synchrovision <trinitybluster@yahoo.co.jp>
2020-10-05 11:20:34 +0900

    feat :+1: add SelectPostColor meta type

---
+++ classes/meta/SelectPostColor.php
@@ -0,0 +1,36 @@
+<?php
+namespace Catpow\meta;
+
+class SelectPostColor extends UI{
+	static $ui='SelectColor';
+	public static function get($data_type,$data_name,$id,$meta_name,$conf){
+		global $wpdb;
+		if($color=$wpdb->get_var($wpdb->prepare(
+			"SELECT meta_value FROM {$wpdb->postmeta} ".
+			"WHERE post_id = %d ".
+			"AND meta_key = 'post_class' ".
+			"AND meta_value LIKE 'color%'",
+			$id
+		))){return [$color];}
+		return ['color0'];
+	}
+	public static function set($data_type,$data_name,$id,$meta_name,$vals,$conf){
+		global $wpdb;
+		$wpdb->query($wpdb->prepare(
+			"DELETE FROM {$wpdb->postmeta} ".
+			"WHERE post_id = %d ".
+			"AND meta_key = 'post_class' ".
+			"AND meta_value LIKE 'color%'",
+			$id
+		));
+		foreach($vals as $val){
+			add_post_meta($id,'post_class',$val);
+		}
+	}
+	public static function output($meta,$prm){
+		$val=$meta->value;
+		if($prm=='chip'){return sprintf('<span class="colorChip fillColor %s"></span>',$val);}
+		return $val;
+	}
+}
+?>
