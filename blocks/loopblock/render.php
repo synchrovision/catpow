0889cadda23f95ea37ffe7cdb20b710a3859c7f7
synchrovision <trinitybluster@yahoo.co.jp>
2020-10-09 12:05:10 +0900

    feat :+1: enable to define default content at LoopBlock block

--- blocks/loopblock/render.php
+++ blocks/loopblock/render.php
@@ -1,9 +1,17 @@
 <?php
+preg_match_all(
+	'|<loopBlockContent name="(?P<name>[\w_]+)">(?P<content>.*?)</loopBlockContent>|s',
+	$content,
+	$matches,
+	PREG_SET_ORDER
+);
+$contents=array_column($matches,null,'name');
 
 $id=uniqid();
 $GLOBALS['loop_block_data'][$id]=[
 	'path'=>$attr['content_path'],
 	'query'=>$attr['query'],
-	'content'=>$content
+	'content'=>$contents['content']['content'],
+	'on_empty'=>$contents['on_empty']['content'],
 ];
 echo "[loop_block {$id}]";
