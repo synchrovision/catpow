07cf3d01a12c74137590f0b5a5833f4ab4312ca4
synchrovision <trinitybluster@yahoo.co.jp>
2020-10-09 12:06:20 +0900

    feat :+1:  enable to define default content at LoopBlock block

--- blocks/loopblock/editor_script.js
+++ blocks/loopblock/editor_script.js
@@ -9,40 +9,61 @@ registerBlockType('catpow/loopblock', {
 		    setAttributes = _ref.setAttributes,
 		    className = _ref.className;
 		var content_path = attributes.content_path,
-		    query = attributes.query;
+		    query = attributes.query,
+		    _attributes$AltMode = attributes.AltMode,
+		    AltMode = _attributes$AltMode === undefined ? false : _attributes$AltMode;
 
 
-		return [wp.element.createElement(
-			'div',
-			{ 'class': 'embedded_content' },
-			wp.element.createElement(
-				'div',
-				{ 'class': 'label' },
-				content_path
-			),
-			wp.element.createElement(InnerBlocks, null)
-		), wp.element.createElement(
-			InspectorControls,
+		return wp.element.createElement(
+			Fragment,
 			null,
 			wp.element.createElement(
-				PanelBody,
-				{ title: 'Query' },
-				wp.element.createElement(TextControl, {
-					label: 'content path',
-					value: content_path,
-					onChange: function onChange(content_path) {
-						setAttributes({ content_path: content_path });
-					}
-				}),
-				wp.element.createElement(TextareaControl, {
-					label: 'query',
-					value: query,
-					onChange: function onChange(query) {
-						setAttributes({ query: query });
-					}
+				BlockControls,
+				null,
+				wp.element.createElement(Toolbar, {
+					controls: [{
+						icon: 'welcome-comments',
+						title: 'AltMode',
+						isActive: AltMode,
+						onClick: function onClick() {
+							return setAttributes({ AltMode: !AltMode });
+						}
+					}]
 				})
+			),
+			wp.element.createElement(
+				'div',
+				{ className: "loopBlock " + (AltMode ? "alt_content altMode" : "embedded_content") },
+				wp.element.createElement(
+					'div',
+					{ 'class': 'label' },
+					AltMode ? wp.element.createElement(Icon, { icon: 'welcome-comments' }) : content_path
+				),
+				wp.element.createElement(InnerBlocks, { template: [['catpow/loopblockcontent'], ['catpow/loopblockcontent', { name: 'on_empty' }]], templateLock: 'all' })
+			),
+			wp.element.createElement(
+				InspectorControls,
+				null,
+				wp.element.createElement(
+					PanelBody,
+					{ title: 'Query' },
+					wp.element.createElement(TextControl, {
+						label: 'content path',
+						value: content_path,
+						onChange: function onChange(content_path) {
+							setAttributes({ content_path: content_path });
+						}
+					}),
+					wp.element.createElement(TextareaControl, {
+						label: 'query',
+						value: query,
+						onChange: function onChange(query) {
+							setAttributes({ query: query });
+						}
+					})
+				)
 			)
-		)];
+		);
 	},
 	save: function save(_ref2) {
 		var attributes = _ref2.attributes,
@@ -50,5 +71,47 @@ registerBlockType('catpow/loopblock', {
 		    setAttributes = _ref2.setAttributes;
 
 		return wp.element.createElement(InnerBlocks.Content, null);
+	}
+});
+
+registerBlockType('catpow/loopblockcontent', {
+	title: '🐾 LoopBlockContent',
+	icon: 'editor-code',
+	category: 'catpow',
+	parent: ['catpow/loopblock'],
+	attributes: {
+		name: { type: 'attribute', label: '名前', selector: 'loopBlockContent', attribute: 'name', default: 'content' }
+	},
+	edit: function edit(_ref3) {
+		var attributes = _ref3.attributes,
+		    className = _ref3.className,
+		    setAttributes = _ref3.setAttributes,
+		    clientId = _ref3.clientId;
+		var name = attributes.name;
+
+
+		var template = name == 'on_empty' ? [['core/paragraph', { align: 'center', content: 'Not Found' }]] : [['catpow/section']];
+
+		return wp.element.createElement(
+			'div',
+			{ className: 'loopBlockContent' },
+			wp.element.createElement(InnerBlocks, { template: template, templateLock: false })
+		);
+	},
+	save: function save(_ref4) {
+		var attributes = _ref4.attributes,
+		    className = _ref4.className,
+		    setAttributes = _ref4.setAttributes;
+		var name = attributes.name;
+
+		return wp.element.createElement(
+			Fragment,
+			null,
+			wp.element.createElement(
+				'loopBlockContent',
+				{ name: name },
+				wp.element.createElement(InnerBlocks.Content, null)
+			)
+		);
 	}
 });
--- blocks/loopblock/editor_script.jsx
+++ blocks/loopblock/editor_script.jsx
@@ -5,31 +5,81 @@
 	category: 'catpow-functional',
 	example:CP.example,
 	edit({attributes,setAttributes,className}){
-        const {content_path,query}=attributes;
+        const {content_path,query,AltMode=false}=attributes;
 		 
-        return [
-			<div class="embedded_content">
-				<div class="label">{content_path}</div>
-				<InnerBlocks/>
-			</div>,
-			<InspectorControls>
-				<PanelBody title="Query">
-					<TextControl
-						label='content path'
-						value={content_path}
-						onChange={(content_path)=>{setAttributes({content_path});}}
+        return (
+			<Fragment>
+				<BlockControls>
+					<Toolbar
+						controls={[
+							{
+								icon: 'welcome-comments',
+								title: 'AltMode',
+								isActive: AltMode,
+								onClick: () => setAttributes({AltMode:!AltMode})
+							}
+						]}
 					/>
-					<TextareaControl
-						label='query'
-						value={query}
-						onChange={(query)=>{setAttributes({query});}}
-					/>
-				</PanelBody>
-			</InspectorControls>
-        ];
+				</BlockControls>
+				<div className={"loopBlock "+(AltMode?"alt_content altMode":"embedded_content")}>
+					<div class="label">
+						{AltMode?(
+							<Icon icon="welcome-comments"/>
+						):(content_path)}
+					</div>
+					<InnerBlocks template={[['catpow/loopblockcontent'],['catpow/loopblockcontent',{name:'on_empty'}]]} templateLock="all"/>
+				</div>
+				<InspectorControls>
+					<PanelBody title="Query">
+						<TextControl
+							label='content path'
+							value={content_path}
+							onChange={(content_path)=>{setAttributes({content_path});}}
+						/>
+						<TextareaControl
+							label='query'
+							value={query}
+							onChange={(query)=>{setAttributes({query});}}
+						/>
+					</PanelBody>
+				</InspectorControls>
+			</Fragment>
+        );
     },
 
 	save({attributes,className,setAttributes}){
 		return <InnerBlocks.Content/>;
+	}
+});
+
+
+registerBlockType('catpow/loopblockcontent',{
+	title:'🐾 LoopBlockContent',
+	icon:'editor-code',
+	category:'catpow',
+    parent:['catpow/loopblock'],
+	attributes:{
+		name:{type:'attribute',label:'名前',selector:'loopBlockContent',attribute:'name',default:'content'},
+	},
+	edit({attributes,className,setAttributes,clientId}){
+		const {name}=attributes;
+		
+		const template=(name=='on_empty')?[['core/paragraph',{align:'center',content:'Not Found'}]]:[['catpow/section']];
+		
+        return (
+			<div className={'loopBlockContent'}>
+				<InnerBlocks template={template} templateLock={false}/>
+			</div>
+		);
+    },
+	save({attributes,className,setAttributes}){
+		const {name}=attributes;
+		return (
+			<Fragment>
+				<loopBlockContent name={name}>
+					<InnerBlocks.Content/>
+				</loopBlockContent>
+			</Fragment>
+		);
 	}
 });
