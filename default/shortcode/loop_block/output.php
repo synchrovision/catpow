cc5356b267265f880004d28eac43366f067058f2
synchrovision <trinitybluster@yahoo.co.jp>
2020-10-13 10:41:33 +0900

    feat :+1: enablet to render content with file at loop_block shortcode

--- default/shortcode/loop_block/output.php
+++ default/shortcode/loop_block/output.php
@@ -3,25 +3,25 @@ namespace Catpow;
 $prm=shortcode_atts([0=>false],$atts);
 $data=$GLOBALS['loop_block_data'][$prm[0]];
 
-if(empty($data['query'])){
-	$query=null;
-}
-else{
-	$data['query']=do_shortcode($data['query']);
-	if($data['query'][0]==='{'){
-		$query=json_decode($data['query'],1)?:null;
-	}
-	elseif($data['query'][0]==='[' && preg_match('/^\[([\w_\\\:]+)(\s(.+))?\]$/',$data['query'],$matches)){
-		$query=$matches[1](shortcode_parse_atts($matches[3]));
-	}
-	else{
-		$cond=new util\cond($data['query']);
-		$query=$cond->get_query($data['path']);
-	}
-}
 
 if(cp::$content){
 	$path=$data['path'];
+	if(empty($data['query'])){
+		$query=null;
+	}
+	else{
+		$data['query']=do_shortcode($data['query']);
+		if($data['query'][0]==='{'){
+			$query=json_decode($data['query'],1)?:null;
+		}
+		elseif($data['query'][0]==='[' && preg_match('/^\[([\w_\\\:]+)(\s(.+))?\]$/',$data['query'],$matches)){
+			$query=$matches[1](shortcode_parse_atts($matches[3]));
+		}
+		else{
+			$cond=new util\cond($data['query']);
+			$query=$cond->get_query($data['path']);
+		}
+	}
 	if(!empty($path)){
 		if(strpos($path,'/')===false){
 			$loop=\cp::$content->meta($path,$query);
@@ -32,15 +32,24 @@ if(cp::$content){
 		}
 	}
 	else{$loop=\cp::$content;}
-	
-	if($loop->query->is_empty()){
-		echo $data['on_empty']??'';
+
+	if(empty($loop->query)){return;}
+
+	if(empty($data['file'])){
+		if($loop->query->is_empty()){
+			echo $data['on_empty']??'';
+		}
+		else{
+			echo $data['before_loop']??'';
+			foreach($loop->loop() as $obj){
+				echo do_shortcode($data['content']);
+			}
+			echo $data['after_loop']??'';
+		}
 	}
 	else{
-		echo $data['before_loop']??'';
-		foreach($loop->loop() as $obj){
-			echo do_shortcode($data['content']);
-		}
-		echo $data['after_loop']??'';
+		\cp::enqueue_script($path.'/script.js');
+		\cp::enqueue_style($path.'/style.css');
+		$loop->render($data['file'],$data['vars']??[]);
 	}
 }
