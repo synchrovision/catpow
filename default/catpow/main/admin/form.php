908e5bedcea3218cc586e347850b5685d6b5a3f7
synchrovision <trinitybluster@yahoo.co.jp>
2020-10-19 16:21:46 +0900

    fix :bug: prevent undefined error

--- default/catpow/main/admin/form.php
+++ default/catpow/main/admin/form.php
@@ -3,7 +3,7 @@ namespace Catpow;
 ?>
 <div class="cp_form_section">
 	<ul class="functions active">
-		<?php foreach(\cp::$use_functions as $i=>$fnc):?>
+		<?php foreach(\cp::$use_functions??[] as $i=>$fnc):?>
 		<li class="item"><?php §sec('catpow/'.$fnc.'/admin/sec_item.php',1); ?></li>
 		<?php endforeach; ?>
 	</ul>
