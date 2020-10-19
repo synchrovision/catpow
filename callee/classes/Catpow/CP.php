b4cbe051c23fe71e0e6c7f7528765656969809b0
synchrovision <trinitybluster@yahoo.co.jp>
2020-10-19 15:42:15 +0900

    feat :+1: enable to use stock as original on alternate CP class

--- callee/classes/Catpow/CP.php
+++ callee/classes/Catpow/CP.php
@@ -1,3 +1,23 @@
 <?php
 namespace Catpow;
-class CP{public function __get($name){return $this->$name;}}
+class CP{
+	public static
+		$cp,$id,$extensions,$data_types,$content,$content_path,$inputs,$forms,$form,$data,
+		$core_functions=[
+			'init',
+			'basic',
+			'image',
+			'post','term','user','nav','comment','view',
+			'template',
+		],$use_functions,$use_blocks;
+	protected $stock;
+	
+	public function __get($name){return $this->$name;}
+	public function __wakeup(){
+		self::$id=&$this->stock['id'];
+		self::$inputs=&$this->stock['inputs'];
+		self::$forms=&$this->stock['forms'];
+		self::$data=&$this->stock['data'];
+	}
+}
+class_alias('Catpow\CP','cp');
