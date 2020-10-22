8094f6dd838e9de40e429ac2125a01f824097be1
synchrovision <trinitybluster@yahoo.co.jp>
2020-10-22 15:32:05 +0900

    feat :+1: enablet to output movie file

--- callee/protected_media.php
+++ callee/protected_media.php
@@ -1,34 +1,46 @@
 <?php
 include __DIR__.'/classes/autoload.php';
 session_start();
-list($id,$file)=explode('/',$_GET['path']);
-if(empty($id) || empty(\cp::$data['allowed_protected_media'][$id])){
-	header('Content-type: image/svg+xml');
-	?>
-	<svg
-		 xmlns="http://www.w3.org/2000/svg"
-		 xmlns:xlink="http://www.w3.org/1999/xlink"
-		 viewBox="0 0 300 300"
-	>
-		<defs>
-			<style>
-				.bg{
-					fill:#cccccc;
-				}
-				.text{
-					fill:#aaaaaa;
-					font-size:20px;
-					text-anchor:middle;
-					dominant-baseline:middle;
-				}
-			</style>
-		</defs>
-		<rect class="bg" x="0" y="0" width="300" height="300"></rect>
-		<text class="text" x="150" y="150">Access Denied</text>
-	</svg>
-	<?php
+$dir=dirname(__DIR__,3).'/uploads/';
+$path=$_GET['path'];
+preg_match('|(sites/(?P<blog_id>\d+)/)?catpow/p(?P<media_id>\d+)/|',$path,$matches);
+$site_id=$matches['blog_id']?:'1';
+$media_id=$matches['media_id'];
+
+if(empty(\cp::$data['allowed_protected_media'][$site_id][$media_id]) || strpos($path,'../') || strpos($path,'..¥')){
+	header('HTTP/1.0 403 Forbidden');
 	die();
 }
-$path=\cp::$data['allowed_protected_media'][$id]['path'];
-header('Content-type: '.mime_content_type($path.'/'.$file));
-readfile($path.'/'.$file);
+
+$file=$dir.'/'.$path;
+$mime=mime_content_type($file);
+$type=substr($mime,0,strpos($mime,'/'));
+/*output*/
+header('Content-type: '.$mime);
+switch($type){
+	case 'video':
+	case 'audio':
+		$size = filesize($file);
+		$fp = fopen($file,"rb");
+		$etag = md5($_SERVER["REQUEST_URI"]).$size;
+		if(@$_SERVER["HTTP_RANGE"]){
+			list($start,$end) = sscanf($_SERVER["HTTP_RANGE"],"bytes=%d-%d");
+			if(empty($end)) $end = $start + 1000000 - 1;
+			if($end>=($size-1)) $end = $size - 1;
+			header("HTTP/1.1 206 Partial Content");
+			header("Content-Range: bytes {$start}-{$end}/{$size}");
+			$size = $end - $start + 1;
+			fseek($fp,$start);
+		}
+		header("Accept-Ranges: bytes");
+		header("Content-Length: {$size}");
+		header("Etag: \"{$etag}\"");
+
+		if($size) echo fread($fp,$size);
+
+		fclose($fp);
+
+		exit;
+	default:
+		readfile($file);
+}
