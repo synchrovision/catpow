82aeda96b4bcbb684c8e5406569ca5eeee621ee0
synchrovision <trinitybluster@yahoo.co.jp>
2020-10-22 15:29:47 +0900

    feat :+1: add alternate query classes

---
+++ callee/classes/Catpow/query/catpow.php
@@ -0,0 +1,3 @@
+<?php
+namespace Catpow\query;
+class catpow{public function __get($name){return $this->$name;}}
---
+++ callee/classes/Catpow/query/cpdb.php
@@ -0,0 +1,3 @@
+<?php
+namespace Catpow\query;
+class cpdb{public function __get($name){return $this->$name;}}
