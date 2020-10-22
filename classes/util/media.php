f3c8047540e21eaf5e12bb6ec1d714f36449830c
synchrovision <trinitybluster@yahoo.co.jp>
2020-10-22 15:30:51 +0900

    feat :+1: add get_all_attachment_file_pathes method at util\media class

--- classes/util/media.php
+++ classes/util/media.php
@@ -6,6 +6,8 @@
 namespace Catpow\util;
 class media{
 	public static function get_all_attachment_files($id){
+		static $cache;
+		if(isset($cache[$id])){return $cache[$id];}
 		$basedir=wp_get_upload_dir()['basedir'];
 		$primary_file=get_post_meta($id,'_wp_attached_file',true);
 		if(0!==strpos($primary_file,$basedir)){
@@ -19,7 +21,12 @@ class media{
 				$rtn[$size]=$dir.'/'.$data['file'];
 			}
 		}
-		return $rtn;
+		return $cache[$id]=$rtn;
+	}
+	public static function get_all_attachment_file_paths($id){
+		return array_map(function($file){
+			return substr($file,strpos($file,'/uploads/')+9);
+		},self::get_all_attachment_files($id));
 	}
 }
 
