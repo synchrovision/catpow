96473793a9d609fb28b9baaf03effa3499fd0a83
synchrovision <trinitybluster@yahoo.co.jp>
2020-11-12 15:15:39 +0900

    feat :+1: enable to set text mail body at t-body block

--- blocks/t-body/editor_script.js
+++ blocks/t-body/editor_script.js
@@ -7,7 +7,8 @@ registerBlockType('catpow/t-body', {
 		classes: { source: 'attribute', selector: 'table', attribute: 'class', default: 'wp-block-catpow-t-body hasHeader hasFooter' },
 		headerText: { source: 'children', selector: 'thead th', default: ['Title'] },
 		footerText: { source: 'children', selector: 'tfoot td', default: ['caption'] },
-		body_class: { type: 'string', default: 'white' }
+		body_class: { type: 'string', default: 'white' },
+		textMail: { source: 'text', selector: 'textmail' }
 	},
 	example: CP.example,
 	edit: function edit(_ref) {
@@ -17,16 +18,134 @@ registerBlockType('catpow/t-body', {
 		var classes = attributes.classes,
 		    headerText = attributes.headerText,
 		    footerText = attributes.footerText,
-		    body_class = attributes.body_class;
+		    body_class = attributes.body_class,
+		    textMail = attributes.textMail,
+		    _attributes$TextMode = attributes.TextMode,
+		    TextMode = _attributes$TextMode === undefined ? false : _attributes$TextMode;
 
 		var primaryClass = 'wp-block-catpow-t-body';
 		var states = CP.wordsToFlags(classes);
 
 		var selectiveClasses = ['color', { label: 'ヘッダ', values: 'hasHeader' }, { label: 'フッタ', values: 'hasFooter' }, { label: '背景色', values: ['white', 'gray', 'black'], key: 'body_class' }];
 
-		return [wp.element.createElement(
-			'div',
-			{ className: "mail_body " + body_class },
+		return wp.element.createElement(
+			Fragment,
+			null,
+			TextMode ? wp.element.createElement(TextareaControl, {
+				value: textMail,
+				onChange: function onChange(textMail) {
+					return setAttributes({ textMail: textMail });
+				}
+			}) : wp.element.createElement(
+				'div',
+				{ className: "mail_body " + body_class },
+				wp.element.createElement(
+					'table',
+					{ width: '100%', align: 'center', valign: 'top', className: classes },
+					states.hasHeader && wp.element.createElement(
+						'thead',
+						null,
+						wp.element.createElement(
+							'tr',
+							null,
+							wp.element.createElement(
+								'th',
+								null,
+								wp.element.createElement(RichText, {
+									onChange: function onChange(headerText) {
+										setAttributes({ headerText: headerText });
+									},
+									value: headerText
+								})
+							)
+						)
+					),
+					wp.element.createElement(
+						'tbody',
+						null,
+						wp.element.createElement(
+							'tr',
+							null,
+							wp.element.createElement(
+								'td',
+								null,
+								wp.element.createElement(
+									'center',
+									null,
+									wp.element.createElement(InnerBlocks, null)
+								)
+							)
+						)
+					),
+					states.hasFooter && wp.element.createElement(
+						'tfoot',
+						null,
+						wp.element.createElement(
+							'tr',
+							null,
+							wp.element.createElement(
+								'td',
+								null,
+								wp.element.createElement(RichText, {
+									onChange: function onChange(footerText) {
+										setAttributes({ footerText: footerText });
+									},
+									value: footerText
+								})
+							)
+						)
+					)
+				)
+			),
+			wp.element.createElement(
+				InspectorControls,
+				null,
+				wp.element.createElement(SelectModeToolbar, {
+					set: setAttributes,
+					attr: attributes,
+					modes: ['TextMode']
+				}),
+				wp.element.createElement(SelectClassPanel, {
+					title: '\u30AF\u30E9\u30B9',
+					icon: 'art',
+					set: setAttributes,
+					attr: attributes,
+					selectiveClasses: selectiveClasses,
+					filters: CP.filters['t-body'] || {}
+				}),
+				wp.element.createElement(
+					PanelBody,
+					{ title: 'CLASS', icon: 'admin-generic', initialOpen: false },
+					wp.element.createElement(TextareaControl, {
+						label: '\u30AF\u30E9\u30B9',
+						onChange: function onChange(classes) {
+							return setAttributes({ classes: classes });
+						},
+						value: classes
+					})
+				)
+			)
+		);
+	},
+	save: function save(_ref2) {
+		var attributes = _ref2.attributes,
+		    className = _ref2.className,
+		    setAttributes = _ref2.setAttributes;
+		var classes = attributes.classes,
+		    headerText = attributes.headerText,
+		    textMail = attributes.textMail,
+		    footerText = attributes.footerText;
+
+		var primaryClass = 'wp-block-catpow-t-body';
+		var states = CP.wordsToFlags(classes);
+		return wp.element.createElement(
+			Fragment,
+			null,
+			wp.element.createElement(
+				'textmail',
+				null,
+				textMail
+			),
 			wp.element.createElement(
 				'table',
 				{ width: '100%', align: 'center', valign: 'top', className: classes },
@@ -39,12 +158,7 @@ registerBlockType('catpow/t-body', {
 						wp.element.createElement(
 							'th',
 							null,
-							wp.element.createElement(RichText, {
-								onChange: function onChange(headerText) {
-									setAttributes({ headerText: headerText });
-								},
-								value: headerText
-							})
+							wp.element.createElement(RichText.Content, { value: headerText })
 						)
 					)
 				),
@@ -60,7 +174,7 @@ registerBlockType('catpow/t-body', {
 							wp.element.createElement(
 								'center',
 								null,
-								wp.element.createElement(InnerBlocks, null)
+								wp.element.createElement(InnerBlocks.Content, null)
 							)
 						)
 					)
@@ -74,93 +188,8 @@ registerBlockType('catpow/t-body', {
 						wp.element.createElement(
 							'td',
 							null,
-							wp.element.createElement(RichText, {
-								onChange: function onChange(footerText) {
-									setAttributes({ footerText: footerText });
-								},
-								value: footerText
-							})
+							wp.element.createElement(RichText.Content, { value: footerText })
 						)
-					)
-				)
-			)
-		), wp.element.createElement(
-			InspectorControls,
-			null,
-			wp.element.createElement(SelectClassPanel, {
-				title: '\u30AF\u30E9\u30B9',
-				icon: 'art',
-				set: setAttributes,
-				attr: attributes,
-				selectiveClasses: selectiveClasses,
-				filters: CP.filters['t-body'] || {}
-			}),
-			wp.element.createElement(
-				PanelBody,
-				{ title: 'CLASS', icon: 'admin-generic', initialOpen: false },
-				wp.element.createElement(TextareaControl, {
-					label: '\u30AF\u30E9\u30B9',
-					onChange: function onChange(classes) {
-						return setAttributes({ classes: classes });
-					},
-					value: classes
-				})
-			)
-		)];
-	},
-	save: function save(_ref2) {
-		var attributes = _ref2.attributes,
-		    className = _ref2.className,
-		    setAttributes = _ref2.setAttributes;
-		var classes = attributes.classes,
-		    headerText = attributes.headerText,
-		    footerText = attributes.footerText;
-
-		var primaryClass = 'wp-block-catpow-t-body';
-		var states = CP.wordsToFlags(classes);
-		return wp.element.createElement(
-			'table',
-			{ width: '100%', align: 'center', valign: 'top', className: classes },
-			states.hasHeader && wp.element.createElement(
-				'thead',
-				null,
-				wp.element.createElement(
-					'tr',
-					null,
-					wp.element.createElement(
-						'th',
-						null,
-						wp.element.createElement(RichText.Content, { value: headerText })
-					)
-				)
-			),
-			wp.element.createElement(
-				'tbody',
-				null,
-				wp.element.createElement(
-					'tr',
-					null,
-					wp.element.createElement(
-						'td',
-						null,
-						wp.element.createElement(
-							'center',
-							null,
-							wp.element.createElement(InnerBlocks.Content, null)
-						)
-					)
-				)
-			),
-			states.hasFooter && wp.element.createElement(
-				'tfoot',
-				null,
-				wp.element.createElement(
-					'tr',
-					null,
-					wp.element.createElement(
-						'td',
-						null,
-						wp.element.createElement(RichText.Content, { value: footerText })
 					)
 				)
 			)
--- blocks/t-body/editor_script.jsx
+++ blocks/t-body/editor_script.jsx
@@ -7,11 +7,12 @@
 		classes:{source:'attribute',selector:'table',attribute:'class',default:'wp-block-catpow-t-body hasHeader hasFooter'},
 		headerText:{source:'children',selector:'thead th',default:['Title']},
 		footerText:{source:'children',selector:'tfoot td',default:['caption']},
-		body_class:{type:'string',default:'white'}
+		body_class:{type:'string',default:'white'},
+		textMail:{source:'text',selector:'textmail'}
 	},
 	example:CP.example,
 	edit({attributes,className,setAttributes}){
-        const {classes,headerText,footerText,body_class}=attributes;
+        const {classes,headerText,footerText,body_class,textMail,TextMode=false}=attributes;
 		const primaryClass='wp-block-catpow-t-body';
 		var states=CP.wordsToFlags(classes);
 		
@@ -22,18 +23,91 @@
 			{label:'背景色',values:['white','gray','black'],key:'body_class'}
 		];
 		
-        return [
-			<div className={"mail_body "+body_class}>
+        return (
+			<Fragment>
+				{TextMode?(
+					<TextareaControl
+						value={textMail}
+						onChange={(textMail)=>setAttributes({textMail})}
+					/>
+				 ):(
+					<div className={"mail_body "+body_class}>
+						<table width="100%" align="center" valign="top" className={classes}>
+							{states.hasHeader &&
+								<thead>
+									<tr>
+										<th>
+											<RichText
+												onChange={(headerText)=>{setAttributes({headerText});}}
+												value={headerText}
+											/>
+										</th>
+									</tr>
+								</thead>
+							}
+							<tbody>
+								<tr>
+									<td>
+										<center>
+											<InnerBlocks/>
+										</center>
+									</td>
+								</tr>
+							</tbody>
+							{states.hasFooter &&
+								<tfoot>
+									<tr>
+										<td>
+											<RichText
+												onChange={(footerText)=>{setAttributes({footerText});}}
+												value={footerText}
+											/>
+										</td>
+									</tr>
+								</tfoot>
+							}
+						</table>
+					</div>
+				 )}
+				<InspectorControls>
+					<SelectModeToolbar
+						set={setAttributes}
+						attr={attributes}
+						modes={['TextMode']}
+					/>	
+					<SelectClassPanel
+						title='クラス'
+						icon='art'
+						set={setAttributes}
+						attr={attributes}
+						selectiveClasses={selectiveClasses}
+						filters={CP.filters['t-body'] || {}}
+					/>
+					<PanelBody title="CLASS" icon="admin-generic" initialOpen={false}>
+						<TextareaControl
+							label='クラス'
+							onChange={(classes)=>setAttributes({classes})}
+							value={classes}
+						/>
+					</PanelBody>
+				</InspectorControls>
+        	</Fragment>
+		);
+    },
+
+
+	save({attributes,className,setAttributes}){
+        const {classes,headerText,textMail,footerText}=attributes;
+		const primaryClass='wp-block-catpow-t-body';
+		var states=CP.wordsToFlags(classes);
+		return (
+			<Fragment>
+				<textmail>{textMail}</textmail>
 				<table width="100%" align="center" valign="top" className={classes}>
 					{states.hasHeader &&
 						<thead>
 							<tr>
-								<th>
-									<RichText
-										onChange={(headerText)=>{setAttributes({headerText});}}
-										value={headerText}
-									/>
-								</th>
+								<th><RichText.Content value={headerText}/></th>
 							</tr>
 						</thead>
 					}
@@ -41,7 +115,7 @@
 						<tr>
 							<td>
 								<center>
-									<InnerBlocks/>
+									<InnerBlocks.Content/>
 								</center>
 							</td>
 						</tr>
@@ -49,68 +123,12 @@
 					{states.hasFooter &&
 						<tfoot>
 							<tr>
-								<td>
-									<RichText
-										onChange={(footerText)=>{setAttributes({footerText});}}
-										value={footerText}
-									/>
-								</td>
+								<td><RichText.Content value={footerText}/></td>
 							</tr>
 						</tfoot>
 					}
 				</table>
-			</div>,
-			<InspectorControls>
-				<SelectClassPanel
-					title='クラス'
-					icon='art'
-					set={setAttributes}
-					attr={attributes}
-					selectiveClasses={selectiveClasses}
-					filters={CP.filters['t-body'] || {}}
-				/>
-				<PanelBody title="CLASS" icon="admin-generic" initialOpen={false}>
-					<TextareaControl
-						label='クラス'
-						onChange={(classes)=>setAttributes({classes})}
-						value={classes}
-					/>
-				</PanelBody>
-			</InspectorControls>
-        ];
-    },
-
-
-	save({attributes,className,setAttributes}){
-        const {classes,headerText,footerText}=attributes;
-		const primaryClass='wp-block-catpow-t-body';
-		var states=CP.wordsToFlags(classes);
-		return (
-			<table width="100%" align="center" valign="top" className={classes}>
-				{states.hasHeader &&
-					<thead>
-						<tr>
-							<th><RichText.Content value={headerText}/></th>
-						</tr>
-					</thead>
-				}
-				<tbody>
-					<tr>
-						<td>
-							<center>
-								<InnerBlocks.Content/>
-							</center>
-						</td>
-					</tr>
-				</tbody>
-				{states.hasFooter &&
-					<tfoot>
-						<tr>
-							<td><RichText.Content value={footerText}/></td>
-						</tr>
-					</tfoot>
-				}
-			</table>
+			</Fragment>
 		);
 	}
 });
