e9b6cec9902ea3e2c325416f55577746b126046b
synchrovision <trinitybluster@yahoo.co.jp>
2020-11-24 20:02:58 +0900

    feat :+1: add Finder page to nav menu items

--- classes/template_type/finder.php
+++ classes/template_type/finder.php
@@ -6,6 +6,12 @@ namespace Catpow\template_type;
 
 class finder extends template_type{
 	public static $permalinks=['finder'];
+	
+	public static function get_nav_menu_items($conf_data){
+		return [
+			$conf_data['label'].'  Finder'=>$conf_data['name'].'/finder'
+		];
+	}
 }
 
 ?>
