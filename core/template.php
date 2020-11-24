24fdb7b8862e6677d445308f671aaadc1d80397c
synchrovision <trinitybluster@yahoo.co.jp>
2020-11-24 20:01:45 +0900

    refactor :recycle: extract get_deps method from template function

--- core/template.php
+++ core/template.php
@@ -496,26 +496,7 @@ function res($data){
 	$res=array_merge($res??[],$data);
 }
 function deps(){
-	$scripts=wp_scripts();
-	$scripts->all_deps($scripts->queue);
-	$styles=wp_styles();
-	$styles->all_deps($styles->queue);
-	$GLOBALS['res']['deps']=[
-		'scripts'=>array_values(array_filter(array_map(function($handle)use($scripts){
-			if(empty($src=$scripts->registered[$handle]->src)){return false;}
-			if(!preg_match('|^(https?:)?//|',$src) && !($scripts->content_url && 0 === strpos($src,$scripts->content_url))){
-				return $scripts->base_url.$src;
-			}
-			return apply_filters('script_loader_src',$src,$handle);
-		},$scripts->to_do))),
-		'styles'=>array_values(array_filter(array_map(function($handle)use($styles){
-			if(empty($src=$styles->registered[$handle]->src)){return false;}
-			if(!preg_match( '|^(https?:)?//|',$src) && !($styles->content_url && 0 === strpos($src,$styles->content_url))){
-				 return $styles->base_url.$src;
-			}
-			return apply_filters('style_loader_src',$src,$handle);
-		},$styles->to_do)))
-	];
+	$GLOBALS['res']['deps']=util\wp::get_deps();
 }
 
 function §message($content=''){
