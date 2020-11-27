3499395dcf5e2b4ea31b04301cc761399302bf65
synchrovision <trinitybluster@yahoo.co.jp>
2020-11-27 11:01:51 +0900

    feat :+1: inherit template of taxonomy and category parent

--- functions/inherit_template/functions.php
+++ functions/inherit_template/functions.php
@@ -12,3 +12,30 @@ add_filter('page_template_hierarchy',function($templates){
 	}
 	return $templates;
 });
+//子カテゴリのテンプレート選択候補に親カテゴリのテンプレートを加える
+add_filter('category_template_hierarchy',function($templates){
+	$term=get_queried_object();
+	if($term->parent>0){
+		$ancestors=[];
+		while(!empty($term->parent)){
+			$ancestors[]=(int)$term->parent;
+			$term=get_term($term->parent,'category');
+			if(is_wp_error($term) || in_array($term->parent,$ancestors)){break;}
+			array_splice($templates,-1,0,'category-'.$term->slug.'.php');
+		}
+	}
+	return $templates;
+});
+add_filter('taxonomy_template_hierarchy',function($templates){
+	$term=get_queried_object();
+	if($term->parent>0){
+		$ancestors=[];
+		while(!empty($term->parent)){
+			$ancestors[]=(int)$term->parent;
+			$term=get_term($term->parent,$term->taxonomy);
+			if(is_wp_error($term) || in_array($term->parent,$ancestors)){break;}
+			array_splice($templates,-1,0,'taxonomy-'.$term->taxonomy.'-'.$term->slug.'.php');
+		}
+	}
+	return $templates;
+});
