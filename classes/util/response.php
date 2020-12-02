a9b200c5a8fd8fa7b3903870a6a4d968a388280a
synchrovision <trinitybluster@yahoo.co.jp>
2020-12-02 20:06:30 +0900

    feat :+1: rename class and add output method

--- classes/util/response.php
+++ classes/util/response.php
@@ -1,10 +1,45 @@
 <?php
 /**
-* アップロードファイル関連のメソッド
+* レスポンス関連処理
 * 
 */
 namespace Catpow\util;
-class header{
+class response{
+	/*ファイル出力*/
+	public static function output($file){
+		$mime=mime_content_type($file);
+		$type=substr($mime,0,strpos($mime,'/'));
+		header('Content-type: '.$mime);
+		switch($type){
+			case 'video':
+			case 'audio':
+				$size = filesize($file);
+				$fp = fopen($file,"rb");
+				$etag = md5($_SERVER["REQUEST_URI"]).$size;
+				if(@$_SERVER["HTTP_RANGE"]){
+					list($start,$end) = sscanf($_SERVER["HTTP_RANGE"],"bytes=%d-%d");
+					if(empty($end)) $end = $start + 1000000 - 1;
+					if($end>=($size-1)) $end = $size - 1;
+					header("HTTP/1.1 206 Partial Content");
+					header("Content-Range: bytes {$start}-{$end}/{$size}");
+					$size = $end - $start + 1;
+					fseek($fp,$start);
+				}
+				header("Accept-Ranges: bytes");
+				header("Content-Length: {$size}");
+				header("Etag: \"{$etag}\"");
+
+				if($size) echo fread($fp,$size);
+
+				fclose($fp);
+
+				exit;
+			default:
+				readfile($file);
+		}
+		exit;
+	}
+	
 	/*ダウンロード*/
 	public static function download($fname,$content){
 		if(headers_sent()){die('headers already sent');}
@@ -18,7 +53,7 @@ class header{
 		elseif(file_exists($content)){include $content;}
 		elseif($f=self::get_file_path($content)){include ($f);}
 		else{echo $content;}
-		die();
+		exit;
 	}
 	
 	/*ストリーム*/
