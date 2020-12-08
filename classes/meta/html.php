fc2ff3f073a33196beb953e907523e4c100551c4
synchrovision <trinitybluster@yahoo.co.jp>
2020-12-08 10:30:20 +0900

    feat :+1: wrap output of html meta_type with div.cp_html

--- classes/meta/html.php
+++ classes/meta/html.php
@@ -7,11 +7,20 @@ class html extends meta{
 	
 	public static function input($meta,$prm){
 		$path=$meta->the_data_path;
-		$setting=array('textarea_name'=>\cp::get_input_name($path));
+		add_editor_style('content.css');
+		$setting=[
+			'textarea_name'=>\cp::get_input_name($path),
+			'tinymce'=>['body_class'=>'cp_html']
+		];
 		ob_start();
 		wp_editor($meta->value,\cp::get_input_id($path).'_editor',$setting);
 		$rtn=ob_get_clean();
 		return $rtn;
+	}
+	public static function output($meta,$prm){
+		$val=$meta->value;
+		if(empty($val)){return $val;}
+		return sprintf('<div class="cp_html">%s</div>',$val);
 	}
 }
 ?>
