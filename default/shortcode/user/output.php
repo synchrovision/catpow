ada411d19730a017d57570e74b4a236f7ef80d9c
synchrovision <trinitybluster@yahoo.co.jp>
2020-12-19 22:11:16 +0900

    refactor :+1: get reset_password_url with util/user class

--- default/shortcode/user/output.php
+++ default/shortcode/user/output.php
@@ -9,14 +9,7 @@ switch($prm[0]){
 	case 'email':echo $user->user_email;break;
 	case 'role':echo $GLOBALS['user_datas'][reset($user->roles)]['label'];break;
 	case 'lost_password_url':echo wp_lostpassword_url();break;
-	case 'reset_password_url':
-		$key=get_password_reset_key($user);
-		if(is_wp_error($key)){
-			error_log($key->get_error_message());
-			return '';
-		}
-		echo network_site_url("wp-login.php?action=rp&key=$key&login=".rawurlencode($user->user_login));
-		break;
+	case 'reset_password_url':echo util\user::get_reset_password_url($user);break;
 	case 'loop':
 		if(cp::$content && !empty($content)){
 			foreach(cp::$content->user($user->ID)->loop() as $obj){
