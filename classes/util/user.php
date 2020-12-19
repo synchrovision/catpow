b3dc7786cc01570893bc1ea9dbc7b2fe29d22e9e
synchrovision <trinitybluster@yahoo.co.jp>
2020-12-19 22:09:30 +0900

    feat :+1: add util/user class

---
+++ classes/util/user.php
@@ -0,0 +1,28 @@
+<?php
+/**
+* WPのAjaxのメソッド
+*/
+namespace Catpow\util;
+class user{
+	/**
+	* パスワードリセットのURLを発行する
+	* 結果をキャッシュして同プロセス中に複数回パスワードリセットのURLを出力しても
+	* キーが上書きされないようにする
+	* @param WP_USER|int $user パスワードをリセットするユーザー
+	* @return string パスワードリセットのURL　失敗した場合は空の文字列
+	**/
+	public static function get_reset_password_url($user=null){
+		static $urls;
+		if(empty($user)){$user=get_user();}
+		if(!is_numeric($user)){$user=get_user($user);}
+		if(isset($urls[$user->ID])){return $urls[$user->ID];}
+		$key=get_password_reset_key($user);
+		if(is_wp_error($key)){
+			error_log($key->get_error_message());
+			return $urls[$user->ID]='';
+		}
+		return $urls[$user->ID]=network_site_url("wp-login.php?action=rp&key=$key&login=".rawurlencode($user->user_login));
+	}
+}
+
+?>
