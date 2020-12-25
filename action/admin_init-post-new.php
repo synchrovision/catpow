1fba5118e0f668516b0ffdaa18f56ba23c68d815
synchrovision <trinitybluster@yahoo.co.jp>
2020-12-25 13:12:43 +0900

    chore :up: remove useless hook
    default_content is set as template at register_post_type

--- action/admin_init-post-new.php
+++ action/admin_init-post-new.php
@@ -10,16 +10,6 @@ add_filter('default_title',function($title){
 	}
 	return $title;
 });
-add_filter('default_content',function($content){
-	global $current_screen,$post_types;
-	$default_content=$post_types[$current_screen->post_type]['default_content']??null;
-	if(isset($default_content)){
-		if(is_callable($default_content)){return $default_content();}
-		return $default_title;
-	}
-	if($default_content=CP::get_template_contents('post/'.$current_screen->post_type.'/admin/default.php')){return $default_content;}
-	return $content;
-});
 add_filter('user_can_richedit',function($can_richedit){
 	if(isset($GLOBALS['post_types'][get_post_type()]['richedit'])){
 		return $GLOBALS['post_types'][get_post_type()]['richedit'];
