4f8b800cf6ce0809751abd03c6a1071928a4de96
synchrovision <trinitybluster@yahoo.co.jp>
2020-12-25 19:23:15 +0900

    change :wrench: add menu as sub_menu if data_type is post at config template_type

--- classes/template_type/config.php
+++ classes/template_type/config.php
@@ -7,6 +7,13 @@ namespace Catpow\template_type;
 
 class config extends template_type{
 	public static function get_menus($conf_data){
+		if($conf_data['data_type']==='post'){
+			return [
+				'sub'=>[
+					'設定'=>'admin.php'
+				]
+			];
+		}
 		return [
 			'top'=>[
 				$conf_data['label']=>'admin.php'
