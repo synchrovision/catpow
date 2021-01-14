5a657a8aa014062a44eda8000332944ae7fece8a
synchrovision <trinitybluster@yahoo.co.jp>
2021-01-14 20:07:49 +0900

    chore :up: update post_data template of mailconf

--- default/post_data/mailconf/thanks.php
+++ default/post_data/mailconf/thanks.php
@@ -7,13 +7,19 @@ $post_data['meta']=[
 ob_start();
 ?>
 <!-- wp:catpow/t-body -->
-<table width="100%" align="center" valign="top" class="wp-block-catpow-t-body hasHeader hasFooter"><thead><tr><th><img class="wp-image-43" src="<?=cp::get_logo_url()?>" alt="" style="width: 250px;"/></th></tr></thead><tbody><tr><td><center>
+<textmail>
 <?php foreach($conf_data['inputs'] as $name=>$conf):?>
-<!-- wp:paragraph -->
-<p class="has-regular-font-size"><?=$conf['label']?>：[output <?=$name?>]</p>
-<!-- /wp:paragraph -->
+<?=$conf['label']?>：[output <?=$name?>]
 <?php endforeach; ?>
-</center></td></tr></tbody><tfoot><tr><td><?php bloginfo('name'); ?><br/><?=home_url()?></td></tr></tfoot></table>
+</textmail><table width="100%" align="center" class="wp-block-catpow-t-body hasHeader hasFooter"><thead class="wp-block-catpow-t-body__header"><tr><th><img class="wp-image" src="<?=get_theme_file_uri('images/logo.svg')?>" alt="<?=bloginfo('title')?>" style="width: 250px;"/></th></tr></thead><tbody class="wp-block-catpow-t-body__body"><tr><td><div class="wp-block-catpow-t-body__body__contents"><!-- wp:catpow/t-box -->
+<table class="wp-block-catpow-t-box medium"><tbody><tr><td>
+<?php foreach($conf_data['inputs'] as $name=>$conf):?>
+<!-- wp:catpow/t-paragraph -->
+<table width="100%" class="wp-block-catpow-t-paragraph medium left"><tbody><tr><td><?=$conf['label']?>：[output <?=$name?>]</td></tr></tbody></table>
+<!-- /wp:catpow/t-paragraph -->
+<?php endforeach; ?>
+<</td></tr></tbody></table>
+<!-- /wp:catpow/t-box --></div></td></tr></tbody><tfoot class="wp-block-catpow-t-body__footer"><tr><td><?php bloginfo('name'); ?><br/><?=home_url()?></td></tr></tfoot></table>
 <!-- /wp:catpow/t-body -->
 <?php
 $post_data['post_content']=ob_get_clean(); ?>
