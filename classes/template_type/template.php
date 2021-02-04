9fd1e35d99bc59aa4433871595435d14373bfb80
synchrovision <trinitybluster@yahoo.co.jp>
2021-02-04 11:50:58 +0900

    feat :+1: enable to define page title and slug as default at template

--- classes/template_type/template.php
+++ classes/template_type/template.php
@@ -22,7 +22,7 @@ class template extends template_type{
 				'article_type'=>'template',
 				'public'=>false,
 				'show_in_menu'=>false,
-				'meta'=>$conf_data['template_meta']??['name'=>['type'=>'post_name'],'image'=>['type'=>'picture']],
+				'meta'=>$conf_data['template_meta']??['name'=>['type'=>'post_name'],'title'=>['type'=>'text'],'slug'=>['type'=>'text'],'image'=>['type'=>'picture']],
 				'template'=>['admin']
 			];
 			\cp::fill_conf_data('post',$template_post_type,$GLOBALS['post_types'][$template_post_type]);
