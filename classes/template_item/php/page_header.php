1dc257ed42486578e6b0a5bd4bfd712670f49ef8
synchrovision <trinitybluster@yahoo.co.jp>
2021-02-04 11:50:33 +0900

    feat :+1: enable to define page title and slug as default at template

--- classes/template_item/php/page_header.php
+++ classes/template_item/php/page_header.php
@@ -19,16 +19,21 @@ class page_header extends \Catpow\template_item\php{
 			if($has_template){
 				$template_conf_data=\cp::get_conf_data("post/{$path_data['data_name']}_tmpl");
 				$rtn['template_start']="<?php foreach(loop('post/{$path_data['data_name']}_tmpl',['name'=>'{$type}']) as \$post): ?>";
-				$rtn['title']=['div.title',['div.text',['h1','@page_title',['small','@page_slug']]]];
-				if(isset($template_conf_data['meta']['title_image'])){
-					$rtn['title'][]=['div.image',"<?php output('title_image'); ?>"];
+				$parts=['title'=>'@page_title','slug'=>'@page_slug','image'=>['img[src="<?=header_image();?>"]']];
+				$prepared_meta_names=[
+					'title'=>['page_title','title'],
+					'slug'=>['page_slug','slug'],
+					'image'=>['title_image','image']
+				];
+				foreach($prepared_meta_names as $part=>$meta_names){
+					foreach($meta_names as $meta_name){
+						if(isset($template_conf_data['meta'][$meta_name])){
+							$parts[$part]="<?php output('{$meta_name}'); ?>";
+							break;
+						}
+					}
 				}
-				elseif(isset($template_conf_data['meta']['image'])){
-					$rtn['title'][]=['div.image',"<?php output('image'); ?>"];
-				}
-				else{
-					$rtn['title'][]=['div.image',['img[src="<?=header_image();?>"]']];
-				}
+				$rtn['title']=['div.title',['div.text',['h1',$parts['title'],['small',$parts['slug']]]],['div.image',$parts['image']]];
 				$rtn['template_end']="<?php endforeach; ?>";
 			}
 			else{
