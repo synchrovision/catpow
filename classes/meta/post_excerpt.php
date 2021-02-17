33ce9bd846de52821e68addf7dfa59ff3d92459e
synchrovision <trinitybluster@yahoo.co.jp>
2021-02-17 11:20:39 +0900

    feat :+1: define default rows and cols attr of post_excerpt meta_type

--- classes/meta/post_excerpt.php
+++ classes/meta/post_excerpt.php
@@ -2,5 +2,9 @@
 namespace Catpow\meta;
 
 class post_excerpt extends post_content{
+	public static function fill_conf(&$conf){
+		$conf['rows']=$conf['rows']??3;
+		$conf['cols']=$conf['cols']??50;
+	}
 }
 ?>
