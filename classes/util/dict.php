e647c5b47bf439c21d81c2c24a98782994eaed1e
synchrovision <trinitybluster@yahoo.co.jp>
2021-02-18 15:52:05 +0900

    feat :+1: add util\dict class

---
+++ classes/util/dict.php
@@ -0,0 +1,26 @@
+<?php
+/**
+* 簡易な連想配列を生成するためのクラス
+*/
+namespace Catpow\util;
+class dict{
+	public static
+		$dict_line_regex='/^(?P<key>.+?)\s*=\s*(?P<value>.+)$/';
+	public $data;
+	public function __construct($data){
+		if(is_string($data)){$data=self::parse($data);}
+		$this->data=$data;
+	}
+	public function parse($str){
+		$data=[];
+		$lines=explode("\n",$str);
+		foreach($lines as $line){
+			if(preg_match(self::$dict_line_regex,$line,$matches)){
+				$data[$matches['key']]=$matches['value'];
+			}
+		}
+		return $data;
+	}
+}
+
+?>
