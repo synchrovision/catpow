048fa02fa1f545aa2c8ae9673b29b46ba63a6489
synchrovision <trinitybluster@yahoo.co.jp>
2021-02-18 15:59:40 +0900

    feat :+1:  enable to select mail is plain or html at T-Body block

--- default/classes/article_type/mailconf.php
+++ default/classes/article_type/mailconf.php
@@ -9,10 +9,9 @@ class mailconf extends article_type{
 		'core/paragraph',
 		'core/spacer',
 		
-		'catpow/loopblock',
-		
 		'catpow/t-body',
 		'catpow/t-box',
+		'catpow/t-loop',
 		
 		'catpow/t-image',
 		'catpow/t-media-text',
@@ -35,10 +34,10 @@ class mailconf extends article_type{
 				'to'=>['type'=>'text','label'=>'送信先','size'=>30],
 				'from'=>['type'=>'text','label'=>'送信元','size'=>30]
 			],
-			'default_content'=>[
-				['catpow/t-body']
-			]
+			'supports'=>['title','editor','custom-fields'],
+			'default_content'=>[['catpow/t-body']]
 		],$conf_data);
+		register_post_meta($conf_data['data_name'],'type',['type'=>'string','show_in_rest'=>true,'single'=>true]);
 	}
 	public static function get_default_post_datas($conf_data){
 		return [
