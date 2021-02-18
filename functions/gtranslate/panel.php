4eee7b2687afaf686ba03f515aad55ae852faf47
synchrovision <trinitybluster@yahoo.co.jp>
2021-02-18 16:04:36 +0900

    change :wrench: add texts at gtranslate panel

--- functions/gtranslate/panel.php
+++ functions/gtranslate/panel.php
@@ -1,15 +1,17 @@
-<?php
-namespace Catpow;
-?>
+<?php namespace Catpow; ?>
 <ul>
 	<li>
+		<h3>ウィジェット表示</h3>
+		<p>翻訳ウィジェットの表示・非表示を設定</p>
 		<?php input('show_widget'); ?>
 	</li>
 	<li>
+		<h3>対応言語</h3>
+		<p>翻訳対応する言語を選択</p>
 		<?php input('locale'); ?>
+		<ul class="buttons">
+			<li class="edit"><?php button('更新','action','message'); ?></li>
+		</ul>
 	</li>
-</ul>
-<ul class="buttons">
-	<li class="edit"><?php button('更新','action','message'); ?></li>
 </ul>
 <?php §message(); ?>
