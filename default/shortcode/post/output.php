d9ce75c73ddea12c244497f8eb1d076587db9215
synchrovision <trinitybluster@yahoo.co.jp>
2021-03-02 14:22:19 +0900

    feat :+1: add tags output

--- default/shortcode/post/output.php
+++ default/shortcode/post/output.php
@@ -14,5 +14,21 @@ switch($prm[0]){
 	case 'href':echo substr(get_the_permalink(),strpos(get_the_permalink(),'://')+3);break;
 	case 'class':echo get_post_class();break;
 	case 'thumbnail':the_post_thumbnail();break;
+	case 'tags':
+		global $post;
+		$confs=CP::get_conf_data('post/'.$post->post_type)['taxonomies']??null;
+		if(empty($confs)){break;}
+		$tax_names=$atts[1]??array_keys($confs);
+		foreach($tax_names as $tax_name){
+			foreach(get_the_terms($post,$tax_name)?:[] as $term){
+				printf(
+					'<a class="tag %s" href="%s">%s</a>',
+					implode(' ',get_term_meta($term->term_id,'term_class')?:['color0']),
+					get_term_link($term),
+					$term->name
+				);
+			}
+		}
+		break;
 	default: echo $GLOBALS['post']->{$prm[0]};
 }
