e6641ad3e60744b015f9ca4f6fa23dc1d381c788
synchrovision <trinitybluster@yahoo.co.jp>
2021-03-02 14:19:52 +0900

    feat :+1: add SelectTermColor meta

---
+++ classes/meta/SelectTermColor.php
@@ -0,0 +1,31 @@
+<?php
+namespace Catpow\meta;
+
+class SelectTermColor extends SelectPostColor{
+	static $ui='SelectColor';
+	public static function get($data_type,$data_name,$id,$meta_name,$conf){
+		global $wpdb;
+		if($color=$wpdb->get_var($wpdb->prepare(
+			"SELECT meta_value FROM {$wpdb->termmeta} ".
+			"WHERE term_id = %d ".
+			"AND meta_key = 'term_class' ".
+			"AND meta_value LIKE 'color%'",
+			$id
+		))){return [$color];}
+		return ['color0'];
+	}
+	public static function set($data_type,$data_name,$id,$meta_name,$vals,$conf){
+		global $wpdb;
+		$wpdb->query($wpdb->prepare(
+			"DELETE FROM {$wpdb->termmeta} ".
+			"WHERE term_id = %d ".
+			"AND meta_key = 'term_class' ".
+			"AND meta_value LIKE 'color%'",
+			$id
+		));
+		foreach($vals as $val){
+			add_term_meta($id,'term_class',$val);
+		}
+	}
+}
+?>
