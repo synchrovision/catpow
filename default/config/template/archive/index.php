c1d78625671fcfac172b33de743dae07319b5f7f
synchrovision <trinitybluster@yahoo.co.jp>
2021-03-02 14:23:39 +0900

    change :wrench: use @page_content

--- default/config/template/archive/index.php
+++ default/config/template/archive/index.php
@@ -1,14 +1,2 @@
 <?php namespace Catpow; ?>
-<ul class="wp-block-catpow-listed menu hasImage hasTitle hasText hasLink">
-	<?php foreach(loop() as $obj): ?>
-	<li class="item">
-		<!--@image vga-->
-		<div class="text">
-			<h3><!--@title--></h3>
-			<p><!--@desc--></p>
-		</div>
-		<!--@link-->
-	</li>
-	<?php endforeach; ?>
-</ul>
-
+<!--@page_content-->
