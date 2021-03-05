032fa3dad1779275f7cbc699e411675f92987777
synchrovision <trinitybluster@yahoo.co.jp>
2021-03-06 01:52:28 +0900

    chore :up: use get_font_roles method instead of font_roles property

--- functions/adobe_fonts/conf.php
+++ functions/adobe_fonts/conf.php
@@ -6,6 +6,6 @@ $conf=[
 		'cp_adobe_fonts_pid'=>['type'=>'options','option'=>'cp_adobe_fonts_pid','placeholder'=>'プロジェクトID','size'=>15]
 	]
 ];
-foreach(style_config::$font_roles as $role=>$font_conf){
+foreach(style_config::get_font_roles() as $role=>$font_conf){
 	$conf['meta']["{$role}_font"]=['type'=>'options','option'=>"cp_adobe_fonts_{$role}_font",'placeholder'=>$font_conf['label'],'size'=>50];
 }
