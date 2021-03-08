78f71e86af86f89c77f27d287dd46f86f2298d5b
synchrovision <trinitybluster@yahoo.co.jp>
2021-03-08 20:34:49 +0900

    feat :+1: enable to define size or position with file name

--- classes/api/images/pattern.php
+++ classes/api/images/pattern.php
@@ -5,6 +5,9 @@ namespace Catpow\api\images;
 */
 
 class pattern extends icon{
+	public static
+		$param_pattern='(s(?P<size>[fci]))?(w(?P<width>\d+))?(h(?P<height>\d+))?(r(?P<repeat>x|y|n))?(x(?P<x>\d+))?(y(?P<y>\d+))?',
+		$param_default=['size'=>null,'width'=>null,'height'=>null,'repeat'=>null,'x'=>null,'y'=>null];
 }
 
 ?>
