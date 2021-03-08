7afead83e65092f2c6d215a85a93da9e3533edbb
synchrovision <trinitybluster@yahoo.co.jp>
2021-03-08 20:34:18 +0900

    change :wrench: use uri instead of file path

--- callee/svg.php
+++ callee/svg.php
@@ -1,9 +1,10 @@
 <?php
-if(substr($_GET['file'],-4)!=='.svg'){
+if(substr($_GET['uri'],-4)!=='.svg'){
 	header('HTTP/1.0 403 Forbidden');
 	die();
 }
-$svg=file_get_contents($_GET['file']);
+$url=($_SERVER['HTTPS']?'https://':'http://').$_SERVER['HTTP_HOST'].$_GET['uri'];
+$svg=file_get_contents($url);
 
 if(!empty($_GET['color'])){
 	if(preg_match('/^#([a-fA-f0-9]{3}){1,2}$/',$_GET['color'])){
@@ -13,16 +14,15 @@ if(!empty($_GET['color'])){
 elseif(!empty($_GET['c'])){
 	if(preg_match('/^\w+$/',$_GET['c'])){
 		if(!empty($_GET['theme']) && preg_match('/^[\w\-]+$/',$_GET['theme'])){
-			$json=preg_replace('@(/wp-content/).+$@',"$1themes/{$_GET['theme']}/json/colors.json",$_GET['file']);
+			$json=preg_replace('@(/wp-content/).+$@',"$1themes/{$_GET['theme']}/json/colors.json",$url);
 		}
 		else{
-			$json=preg_replace('@(/wp-content/(themes/[\w\-]+|plugins/catpow/default)/).+$@','$1json/colors.json',$_GET['file']);
+			$json=preg_replace('@(/wp-content/(themes/[\w\-]+|plugins/catpow/default)/).+$@','$1json/colors.json',$url);
 		}
-		if(file_exists($json) && ($colors=json_decode(file_get_contents($json),true)) && $c=$colors[$_GET['c']]??null){
+		if(($colors=json_decode(file_get_contents($json),true)) && $c=$colors[$_GET['c']]??null){
 			if(isset($c))$svg=str_replace('<svg ',"<svg color='{$c}' fill='currentcolor' ",$svg);
 		}
 	}
 }
-
 header('Content-type: image/svg+xml');
 echo $svg;
