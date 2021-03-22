4e376520bc0f63a4b530bc3bf3d407413605d6c5
synchrovision <trinitybluster@yahoo.co.jp>
2021-03-22 20:04:55 +0900

    feat :+1: register remarkable

--- action/init.php
+++ action/init.php
@@ -23,6 +23,7 @@ while($fname = $cpjs_dir->read()){
 wp_enqueue_style('font_awesome','https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.1/css/all.min.css');
 wp_register_script('urljs','https://cdnjs.cloudflare.com/ajax/libs/URI.js/1.19.2/URI.min.js');
 wp_register_script('axios','https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.0/axios.min.js');
+wp_register_script('remarkable','https://cdnjs.cloudflare.com/ajax/libs/remarkable/2.0.1/remarkable.min.js');
 
 wp_enqueue_script('catpow');
 wp_localize_script('catpow','cp',array(
