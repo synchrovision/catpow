2fa82d23b5cdab02f8440bc75c619f0f4eb4c345
synchrovision <trinitybluster@yahoo.co.jp>
2021-04-28 13:49:50 +0900

    feat :+1: compile po to jde

--- jsx_compiler/compiler.php
+++ jsx_compiler/compiler.php
@@ -8,7 +8,12 @@ if(!file_exists(__DIR__.'/node_modules')){passthru('npm install');}
 
 while(true){
 	$jsx_files=get_jsx_files();
-	for($i=0;$i<20;$i++){cp_jsx_compile($jsx_files);sleep(3);}
+	$po_files=get_po_files();
+	for($i=0;$i<20;$i++){
+		cp_jsx_compile($jsx_files);
+		cp_po_compile($po_files);
+		sleep(3);
+	}
 }
 
 function get_jsx_files(){
@@ -29,6 +34,30 @@ function cp_jsx_compile($jsx_files){
 			passthru('babel '.$jsx_file.' -o '.$js_file.' > '.__DIR__.'/logs/result.txt');
 			echo "build {$js_file}\n";
 			touch($js_file);
+		}
+	}
+}
+
+function get_po_files(){
+	$po_files=[];
+    $wp_content_dir=dirname(__DIR__,3).'/';
+	
+	foreach(glob($wp_content_dir.'plugins/catpow{,-*}/blocks/*/languages/*.po',GLOB_BRACE) as $po_file){
+		$handle='cp_blocks_editor_script_'.basename(dirname(dirname($po_file)));
+		$jed_file=substr($po_file,0,-3).'-'.$handle.'.json';
+		$po_files[$po_file]=$jed_file;
+	}
+	return $po_files;
+}
+
+function cp_po_compile($po_files){
+	foreach($po_files as $po_file=>$jed_file){
+		if(!file_exists($po_file)){continue;}
+		if(!file_exists($jed_file) or filemtime($jed_file) < filemtime($po_file)){
+			if(!is_dir(dirname($jed_file))){mkdir(dirname($jed_file),0777,true);}
+			passthru('po2json '.$po_file.' '.$jed_file.' -f jed1.x -d catpow > '.__DIR__.'/logs/result.txt');
+			echo "build {$jed_file}\n";
+			touch($jed_file);
 		}
 	}
 }
