a127a68aecbd02e072bb52cd58804693f527c2ce
synchrovision <trinitybluster@yahoo.co.jp>
2021-05-05 19:53:46 +0900

    feat :+1: add Finder Pagenate component

---
+++ components/Finder/Pagenate/component.jsx
@@ -0,0 +1,47 @@
+﻿/**
+* Finderのページネーション
+*/
+
+Catpow.Finder.Pagenate=(props)=>{
+	const {useContext}=wp.element;
+	const {state,dispatch}=useContext(Catpow.FinderContext);
+	
+	if(!state.maxNumPages || state.maxNumPages<2){return false;}
+	return (
+		<div className="FinderPagenate">
+			<div
+				className={"button button_prev"+(state.page<=1?' disable':'')}
+				onClick={()=>{
+					if(state.page<=1){return;}
+					dispatch({type:'setPage',page:state.page-1});
+				}}
+			>
+				<div class={"icon dashicons dashicons-arrow-left-alt2"}></div>
+			</div>
+			<ul className="items">
+				{[...Array(state.maxNumPages).keys()].map((i)=>{
+					const page=i+1;
+					const p=page-state.page;
+					const stateClass=p===0?'active':(p<0?'before':'after')
+					return (
+						<li
+							className={'item item'+p+' '+stateClass}
+							onClick={()=>{
+								dispatch({type:'setPage',page});
+							}}
+						>{page}</li>
+					);
+				})}
+			</ul>
+			<div
+				className={"button button_next"+(state.page>=state.maxNumPages?' disable':'')}
+				onClick={()=>{
+					if(state.page>=state.maxNumPages){return;}
+					dispatch({type:'setPage',page:state.page+1});
+				}}
+			>
+				<div class={"icon dashicons dashicons-arrow-right-alt2"}></div>
+			</div>
+		</div>
+	);
+}
