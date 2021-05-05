d08765162b324d0bba38cccb461445acd42f9307
synchrovision <trinitybluster@yahoo.co.jp>
2021-05-05 19:46:24 +0900

    feat :+1: add SelectBox component

---
+++ components/SelectBox/component.jsx
@@ -0,0 +1,16 @@
+﻿Catpow.SelectBox=(props)=>{
+	const {label,value,options,onChange}=props;
+	
+	return (
+		<select className={"SelectBox"} value={value} onChange={(e)=>onChange(event.target.value)}>
+			{label && (
+				<option value={false}>{label}</option>
+			)}
+			{typeof options === 'object' && (Array.isArray(options)?(
+				options.map((val)=><option value={val}>{val}</option>)
+			):(
+				Object.keys(options).map((label)=><option value={options[label]}>{label}</option>)
+			))}
+		</select>
+	);
+}
