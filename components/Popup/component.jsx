ac0f482116cd85e0291ee3a697a84d52ffd3880d
synchrovision <trinitybluster@yahoo.co.jp>
2021-05-07 11:24:03 +0900

    feat :+1: add onClosed, closeOnClickAway props

--- components/Popup/component.jsx
+++ components/Popup/component.jsx
@@ -1,17 +1,21 @@
-﻿Catpow.Popup=function({children,open,onClose,closeButton}){
-	var [state,setPopupState]=wp.element.useState('closed');
-	if(open && state==='closed'){setPopupState('open');}
-	if(!open && state==='open'){setPopupState('close');}
+﻿Catpow.Popup=(props)=>{
+	const {children,open,onClose,onClosed,closeButton=false,closeOnClickAway=true}=props;
+	const {useState,useEffect}=wp.element;
+	var [state,setPopupState]=useState('closed');
+	
+	useEffect(()=>setPopupState(open?'open':'close'),[open]);
+	
 	return (
 		<div
 			className={'Popup '+state}
 			onAnimationEnd={()=>{
 				if(state==='close'){
 					setPopupState('closed');
+					onClosed();
 				}
 			}}
 		>
-			<div class="PopupBG" onClick={onClose}></div>
+			<div class="PopupBG" onClick={()=>{if(closeOnClickAway){onClose()}}}></div>
 			<div class="PopupBody">
 				<div className="PopupContents">{children}</div>
 				{closeButton && (
