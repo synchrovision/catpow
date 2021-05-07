5c9d20def861e17e5964db245821c7a1f5683c41
synchrovision <trinitybluster@yahoo.co.jp>
2021-05-07 11:58:48 +0900

    feat :+1: add Finder BulkControl component

---
+++ components/Finder/BulkControl/component.js
@@ -0,0 +1,249 @@
+var _this = this;
+
+/**
+* Finderで選択された項目をbulk APIで処理するコンポーネント
+*/
+
+Catpow.Finder.BulkControl = function (props) {
+	var callback = props.callback;
+	var _wp$element = wp.element,
+	    useState = _wp$element.useState,
+	    useCallback = _wp$element.useCallback,
+	    useMemo = _wp$element.useMemo,
+	    useEffect = _wp$element.useEffect,
+	    useContext = _wp$element.useContext;
+	var __ = wp.i18n.__;
+
+	var _useContext = useContext(Catpow.FinderContext),
+	    state = _useContext.state,
+	    dispatch = _useContext.dispatch;
+
+	var _useState = useState(false),
+	    _useState2 = babelHelpers.slicedToArray(_useState, 2),
+	    value = _useState2[0],
+	    setValue = _useState2[1];
+
+	var _useState3 = useState(false),
+	    _useState4 = babelHelpers.slicedToArray(_useState3, 2),
+	    modal = _useState4[0],
+	    setModal = _useState4[1];
+
+	var cols = state.index.cols;
+
+
+	useEffect(function () {
+		wp.apiFetch({
+			path: state.apiPath + '/bulk/index'
+		}).then(function (bulk) {
+			dispatch({ type: 'update', data: { bulk: bulk } });
+		});
+	}, []);
+	var options = useMemo(function () {
+		if (!state.bulk) {
+			return {};
+		}
+		var options = {};
+		Object.keys(state.bulk).map(function (name) {
+			options[state.bulk[name].label] = name;
+		});
+		return options;
+	}, [state.bulk]);
+
+	var exec_bulk = useCallback(function () {
+		var _ref = babelHelpers.asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(action) {
+			var conf, rows, _vals;
+
+			return regeneratorRuntime.wrap(function _callee$(_context) {
+				while (1) {
+					switch (_context.prev = _context.next) {
+						case 0:
+							conf = state.bulk[action];
+							rows = state.items.filter(function (item) {
+								return item._selected;
+							}).map(function (item) {
+								return item._id;
+							});
+							_context.prev = 2;
+							_context.next = 5;
+							return show_modal(conf);
+
+						case 5:
+							_vals = _context.sent;
+							_context.next = 11;
+							break;
+
+						case 8:
+							_context.prev = 8;
+							_context.t0 = _context['catch'](2);
+							return _context.abrupt('return', false);
+
+						case 11:
+
+							wp.apiFetch({
+								path: state.apiPath + '/bulk/exec/' + action,
+								method: 'POST',
+								data: { rows: rows, vals: vals }
+							}).then(function (res) {
+								console.log(res);
+								if (callback) {
+									callback({ action: action, res: res, state: state, dispatch: dispatch });
+								}
+								if (res.items) {
+									res.items.map(function (newItem) {
+										var oldItem = state.items.find(function (oldItem) {
+											return oldItem._id === newItem._id;
+										});
+										if (oldItem) {
+											babelHelpers.extends(oldItem, newItem);
+										}
+									});
+								}
+								if (res.message) {
+									dispatch(babelHelpers.extends({ type: 'showMessage' }, res.message));
+								}
+							});
+
+						case 12:
+						case 'end':
+							return _context.stop();
+					}
+				}
+			}, _callee, _this, [[2, 8]]);
+		}));
+
+		return function (_x) {
+			return _ref.apply(this, arguments);
+		};
+	}(), [state, dispatch]);
+	var show_modal = useCallback(function (conf) {
+		var _Catpow = Catpow,
+		    ModalForm = _Catpow.ModalForm;
+		var Input = ModalForm.Input,
+		    Button = ModalForm.Button;
+
+		return new Promise(function (resolve, reject) {
+			if (!conf.inputs) {
+				resolve(false);return;
+			}
+			setModal(wp.element.createElement(
+				ModalForm,
+				{
+					onComplete: function onComplete(values) {
+						setModal(false);
+						if (values.accept) {
+							resolve(values);
+						} else {
+							reject(false);
+						}
+					}
+				},
+				wp.element.createElement(
+					'div',
+					{ className: 'FinderBulkControlForm' },
+					wp.element.createElement(
+						'h3',
+						{ className: 'title' },
+						conf.label
+					),
+					wp.element.createElement(
+						'ul',
+						{ className: 'inputs' },
+						conf.inputs.map(function (props) {
+							var label = props.label,
+							    desc = props.desc,
+							    caption = props.caption,
+							    otherPorps = babelHelpers.objectWithoutProperties(props, ['label', 'desc', 'caption']);
+
+							return wp.element.createElement(
+								'li',
+								{ className: 'item' },
+								label && wp.element.createElement(
+									'h4',
+									{ className: 'label' },
+									label
+								),
+								desc && wp.element.createElement(
+									'p',
+									{ className: 'desc' },
+									desc
+								),
+								wp.element.createElement(
+									'div',
+									{ className: 'input' },
+									wp.element.createElement(Input, otherPorps)
+								),
+								caption && wp.element.createElement(
+									'p',
+									{ className: 'caption' },
+									caption
+								)
+							);
+						})
+					),
+					wp.element.createElement(
+						'ul',
+						{ className: 'buttons s' },
+						wp.element.createElement(
+							'li',
+							{ className: 'item negative' },
+							wp.element.createElement(Button, {
+								label: __('キャンセル', 'catpow'),
+								name: 'accept',
+								value: false
+							})
+						),
+						wp.element.createElement(
+							'li',
+							{ className: 'item primary' },
+							wp.element.createElement(Button, {
+								label: __('実行', 'catpow'),
+								name: 'accept',
+								value: true
+							})
+						)
+					)
+				)
+			));
+		});
+	}, [setModal]);
+
+	return wp.element.createElement(
+		'div',
+		{ className: 'FinderControl FinderBulkControl' },
+		wp.element.createElement(
+			'ul',
+			{ className: 'items' },
+			wp.element.createElement(
+				'li',
+				{ className: 'item' + (open ? ' active' : '') },
+				wp.element.createElement(
+					'div',
+					{ className: 'inputs' },
+					wp.element.createElement(Catpow.SelectBox, {
+						label: __('一括処理', 'catpow'),
+						value: value,
+						options: options,
+						onChange: function onChange(val) {
+							setValue(val);
+						}
+					}),
+					wp.element.createElement(
+						'button',
+						{
+							className: 'button',
+							onClick: function onClick(e) {
+								exec_bulk(value);
+							}
+						},
+						__('適用', 'catpow')
+					)
+				)
+			)
+		),
+		wp.element.createElement(
+			Catpow.OuterContents,
+			{ name: 'modal' },
+			modal
+		)
+	);
+};
---
+++ components/Finder/BulkControl/component.jsx
@@ -0,0 +1,130 @@
+﻿/**
+* Finderで選択された項目をbulk APIで処理するコンポーネント
+*/
+
+Catpow.Finder.BulkControl=(props)=>{
+	const {callback}=props;
+	const {useState,useCallback,useMemo,useEffect,useContext}=wp.element;
+	const {__}=wp.i18n;
+	const {state,dispatch}=useContext(Catpow.FinderContext);
+	const [value,setValue]=useState(false);
+	const [modal,setModal]=useState(false);
+	const {cols}=state.index;
+	
+	useEffect(()=>{
+		wp.apiFetch({
+			path:state.apiPath+'/bulk/index'
+		}).then((bulk)=>{
+			dispatch({type:'update',data:{bulk}});
+		});
+	},[]);
+	const options=useMemo(()=>{
+		if(!state.bulk){return {};}
+		const options={};
+		Object.keys(state.bulk).map((name)=>{
+			options[state.bulk[name].label]=name;
+		});
+		return options;
+	},[state.bulk]);
+	
+	const exec_bulk=useCallback(async(action)=>{
+		const conf=state.bulk[action];
+		const rows=state.items.filter((item)=>item._selected).map((item)=>item._id);
+		
+		try{
+			const vals=await show_modal(conf);
+		}
+		catch(err){return false;}
+		
+		wp.apiFetch({
+			path:state.apiPath+'/bulk/exec/'+action,
+			method: 'POST',
+			data:{rows,vals}
+		}).then((res)=>{
+			console.log(res);
+			if(callback){
+				callback({action,res,state,dispatch});
+			}
+			if(res.items){
+				res.items.map((newItem)=>{
+					const oldItem=state.items.find((oldItem)=>oldItem._id===newItem._id)
+					if(oldItem){Object.assign(oldItem,newItem);}
+				});
+			}
+			if(res.message){
+				dispatch({type:'showMessage',...res.message});
+			}
+		});
+	},[state,dispatch]);
+	const show_modal=useCallback((conf)=>{
+		const {ModalForm}=Catpow;
+		const {Input,Button}=ModalForm;
+		return new Promise((resolve,reject)=>{
+			if(!conf.inputs){resolve(false);return;}
+			setModal(
+				<ModalForm
+					onComplete={(values)=>{
+						setModal(false);
+						if(values.accept){resolve(values);}
+						else{reject(false);}
+					}}
+				>
+					<div className="FinderBulkControlForm">
+						<h3 className="title">{conf.label}</h3>
+						<ul className="inputs">
+							{conf.inputs.map((props)=>{
+								const {label,desc,caption,...otherPorps}=props;
+								return (
+									<li className="item">
+										{label && <h4 className="label">{label}</h4>}
+										{desc && <p className="desc">{desc}</p>}
+										<div className="input"><Input {...otherPorps}/></div>
+										{caption && <p className="caption">{caption}</p>}
+									</li>
+								)
+							})}
+						</ul>
+						<ul className="buttons s">
+							<li className="item negative">
+								<Button
+									label={__('キャンセル','catpow')}
+									name="accept"
+									value={false}
+								/>
+							</li>
+							<li className="item primary">
+								<Button
+									label={__('実行','catpow')}
+									name="accept"
+									value={true}
+								/>
+							</li>
+						</ul>
+					</div>
+				</ModalForm>
+			);
+		});
+	},[setModal]);
+	
+	return (
+		<div className="FinderControl FinderBulkControl">
+			<ul className="items">
+				<li className={'item'+(open?' active':'')}>
+					<div className="inputs">
+						<Catpow.SelectBox
+							label={__('一括処理','catpow')}
+							value={value}
+							options={options}
+							onChange={(val)=>{setValue(val);}}
+						/>
+						<button
+							className="button"
+							onClick={(e)=>{exec_bulk(value)}}
+						>{__('適用','catpow')}</button>
+					</div>
+				</li>
+			</ul>
+			<Catpow.OuterContents name="modal">{modal}</Catpow.OuterContents>
+		</div>
+	);
+}
