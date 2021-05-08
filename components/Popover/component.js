79b2747cd38b4a14de463af88732f25227c81c34
synchrovision <trinitybluster@yahoo.co.jp>
2021-05-08 12:48:58 +0900

    feat :+1: use Exeternal component

--- components/Popover/component.js
+++ components/Popover/component.js
@@ -1,45 +1,59 @@
-Catpow.Popover = function (_ref) {
-	var children = _ref.children,
-	    open = _ref.open,
-	    onClose = _ref.onClose,
-	    closeButton = _ref.closeButton,
-	    _ref$position = _ref.position,
-	    position = _ref$position === undefined ? 'top' : _ref$position;
+Catpow.Popover = function (props) {
+	var children = props.children,
+	    open = props.open,
+	    onClose = props.onClose,
+	    closeButton = props.closeButton,
+	    _props$position = props.position,
+	    position = _props$position === undefined ? 'top' : _props$position;
+	var _wp$element = wp.element,
+	    Fragment = _wp$element.Fragment,
+	    useEffect = _wp$element.useEffect,
+	    useState = _wp$element.useState,
+	    useRef = _wp$element.useRef;
 
-	var _wp$element$useState = wp.element.useState('closed'),
-	    _wp$element$useState2 = babelHelpers.slicedToArray(_wp$element$useState, 2),
-	    state = _wp$element$useState2[0],
-	    setPopoverState = _wp$element$useState2[1];
+	var _useState = useState('closed'),
+	    _useState2 = babelHelpers.slicedToArray(_useState, 2),
+	    state = _useState2[0],
+	    setPopoverState = _useState2[1];
 
-	if (open && state === 'closed') {
-		setPopoverState('open');
-	}
-	if (!open && state === 'open') {
-		setPopoverState('close');
-	}
+	useEffect(function () {
+		return setPopoverState(open ? 'open' : state === 'closed' ? 'closed' : 'close');
+	}, [open]);
+
+	var ref = useRef({});
+
 	return wp.element.createElement(
-		'div',
-		{
-			className: 'Popover ' + state + ' ' + position,
-			onAnimationEnd: function onAnimationEnd() {
-				if (state === 'close') {
-					setPopoverState('closed');
-				}
-			}
-		},
+		Fragment,
+		null,
+		wp.element.createElement('div', { className: 'PopoverAnchor', ref: ref }),
 		wp.element.createElement(
-			'div',
-			{ 'class': 'PopoverBody' },
-			wp.element.createElement('div', { 'class': 'PopoverArrow' }),
+			Catpow.External,
+			{ className: 'PopoverContainer', trace: ref.current },
 			wp.element.createElement(
 				'div',
-				{ className: 'PopoverContents' },
-				children
-			),
-			closeButton && wp.element.createElement(
-				'div',
-				{ className: 'PopoverControl' },
-				wp.element.createElement('div', { className: 'close', onClick: onClose })
+				{
+					className: 'Popover ' + state + ' ' + position,
+					onAnimationEnd: function onAnimationEnd() {
+						if (state === 'close') {
+							setPopoverState('closed');
+						}
+					}
+				},
+				wp.element.createElement(
+					'div',
+					{ 'class': 'PopoverBody' },
+					wp.element.createElement('div', { 'class': 'PopoverArrow' }),
+					wp.element.createElement(
+						'div',
+						{ className: 'PopoverContents' },
+						children
+					),
+					closeButton && wp.element.createElement(
+						'div',
+						{ className: 'PopoverControl' },
+						wp.element.createElement('div', { className: 'close', onClick: onClose })
+					)
+				)
 			)
 		)
 	);
--- components/Popover/component.jsx
+++ components/Popover/component.jsx
@@ -1,25 +1,35 @@
-﻿Catpow.Popover=function({children,open,onClose,closeButton,position='top'}){
-	var [state,setPopoverState]=wp.element.useState('closed');
-	if(open && state==='closed'){setPopoverState('open');}
-	if(!open && state==='open'){setPopoverState('close');}
+﻿Catpow.Popover=function(props){
+	const {children,open,onClose,closeButton,position='top'}=props;
+	const {Fragment,useEffect,useState,useRef}=wp.element;
+	const [state,setPopoverState]=useState('closed');
+	
+	useEffect(()=>setPopoverState(open?'open':(state==='closed'?'closed':'close')),[open]);
+	
+	const ref=useRef({});
+	
 	return (
-		<div
-			className={`Popover ${state} ${position}`}
-			onAnimationEnd={()=>{
-				if(state==='close'){
-					setPopoverState('closed');
-				}
-			}}
-		>
-			<div class="PopoverBody">
-				<div class="PopoverArrow"></div>
-				<div className="PopoverContents">{children}</div>
-				{closeButton && (
-					<div className="PopoverControl">
-						<div className="close" onClick={onClose}></div>
+		<Fragment>
+			<div className="PopoverAnchor" ref={ref}></div>
+			<Catpow.External className="PopoverContainer" trace={ref.current}>
+				<div
+					className={`Popover ${state} ${position}`}
+					onAnimationEnd={()=>{
+						if(state==='close'){
+							setPopoverState('closed');
+						}
+					}}
+				>
+					<div class="PopoverBody">
+						<div class="PopoverArrow"></div>
+						<div className="PopoverContents">{children}</div>
+						{closeButton && (
+							<div className="PopoverControl">
+								<div className="close" onClick={onClose}></div>
+							</div>
+						)}
 					</div>
-				)}
-			</div>
-		</div>
+				</div>
+			</Catpow.External>
+		</Fragment>
 	);
 }
