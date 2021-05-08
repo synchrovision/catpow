f50711be9668e030a91b0feab44f545d7172f0a8
synchrovision <trinitybluster@yahoo.co.jp>
2021-05-08 12:23:23 +0900

    feat :+1: add External component

---
+++ components/External/component.jsx
@@ -0,0 +1,42 @@
+﻿Catpow.External=(props)=>{
+	const {children,trace}=props;
+	const {render,useState,useMemo,useCallback,useEffect,useRef}=wp.element;
+	
+	const ref=useRef({contents:false,setContents:()=>{}});
+	
+	const el=useMemo(()=>{
+		const el=document.createElement('div');
+		el.className=props.className;
+		document.body.appendChild(el);
+		return el;
+	},[]);
+	
+	const ExternalContents=useCallback((props)=>{
+		[ref.current.contents,ref.current.setContents]=useState(children);
+		return ref.current.contents;
+	},[]);
+	
+	useEffect(()=>{
+		render(<ExternalContents/>,el);
+		return ()=>el.remove();
+	},[]);
+	useEffect(()=>{ref.current.setContents(children);},[children]);
+	
+	if(trace){
+		useEffect(()=>{
+			el.style.position='absolute';
+			const timer=setInterval(()=>{
+				if(trace.getBoundingClientRect){
+					const bnd=trace.getBoundingClientRect();
+					el.style.left=(window.scrollX+bnd.left)+'px';
+					el.style.top=(window.scrollY+bnd.top)+'px';
+					el.style.width=bnd.width+'px';
+					el.style.height=bnd.height+'px';
+				}
+			},50);
+			return ()=>clearInterval(timer);
+		},[trace]);
+	}
+	
+	return false;
+}
