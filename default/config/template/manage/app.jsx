d6739457513ae3b92a39f49e04c115bb51c2693f
synchrovision <trinitybluster@yahoo.co.jp>
2021-05-11 20:29:47 +0900

    feat :+1: output forcused item with Focused component

--- default/config/template/manage/app.jsx
+++ default/config/template/manage/app.jsx
@@ -3,18 +3,19 @@ const app=(props)=>{
 	
 	const App=useCallback((props)=>{
 		const {useContext,useCallback,Fragment}=wp.element;
-		const {Nav,Spacer,Main,SelectLayout,SelectColumns,BulkControl,FilterControl,Download,PerPage,Status,Contents,Pagenate}=Catpow.Finder;
-		const {state,dispatch}=useContext(Catpow.FinderContext);
+		const {Nav,Spacer,Main,SelectLayout,SelectColumns,BulkControl,FilterControl,Download,PerPage,Status,SearchResults,Focused,Pagenate}=Catpow.Finder;
+		const {state,dispatch,info}=useContext(Catpow.FinderContext);
 		
 		return(
 			<Catpow.Transition>
-				{state.focused?(
+				{state.wait?(
+					<Fragment depth={0}>
+						<Catpow.Spinner/>
+					</Fragment>
+				):state.focused?(
 					<Fragment depth={2}>
 						<div className={"icon dashicons dashicons-arrow-left-alt"} onClick={()=>dispatch({type:'blurItem'})}></div>
-						<div>
-							{state.focused.simei}
-							吾輩は猫である。名前はまだない。どこで生れたか頓と見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。吾輩はここで始めて人間というものを見た。しかもあとで聞くとそれは書生という人間中で一番獰悪な種族であったそうだ。この書生というのは時々我々を捕えて煮て食うという話である。しかしその当時は何という考もなかったから別段恐しいとも思わなかった。ただ彼の掌に載せられてスーと持ち上げられた時何だかフワフワした感じがあったばかりである。掌の上で少し落ち付いて書生の顔を見たのがいわゆる人間というものの見始であろう。この時妙なものだと思った感じが今でも残っている。第一毛を以て装飾されべきはずの顔がつるつるしてまるで薬缶だ。その後猫にも大分逢ったがこんな片輪には一度も出会わした事がない。のみならず顔の真中が余りに突起している。そうしてその穴の中から時々ぷうぷうと烟を吹く。どうも咽せぽくて実に弱った。これが人間の飲む烟草というものである事は漸くこの頃知った。
-						</div>
+						<Focused/>
 					</Fragment>
 				):(
 					<Fragment depth={1}>
@@ -29,7 +30,7 @@ const app=(props)=>{
 							<Status/>
 						</Nav>
 						<Main>
-							<Contents/>
+							<SearchResults/>
 						</Main>
 						<Pagenate/>
 					</Fragment>
