88011f85fa1cfe443255d27226cd9ea6e0db425e
synchrovision <trinitybluster@yahoo.co.jp>
2021-05-11 12:21:00 +0900

    feat :+1: add Spinner component

---
+++ components/Spinner/component.jsx
@@ -0,0 +1,13 @@
+﻿Catpow.Spinner=(props)=>{
+	const {type='circle'}=props;
+	
+	return (
+		<div className={`Spinner Spinner-${type}`}>
+			<div className="graphics">
+				<div className="graphic graphic1"></div>
+				<div className="graphic graphic2"></div>
+				<div className="graphic graphic3"></div>
+			</div>
+		</div>
+	);
+}
