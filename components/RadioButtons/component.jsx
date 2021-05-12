b6eecb5b98c791b65dc30ca686fbf9a186422ba5
synchrovision <trinitybluster@yahoo.co.jp>
2021-05-12 20:42:39 +0900

    feat :+1: add RadioButtons component

---
+++ components/RadioButtons/component.jsx
@@ -0,0 +1,30 @@
+﻿Catpow.RadioButtons=(props)=>{
+	const {useMemo}=wp.element;
+	const {value,onChange}=props;
+	
+	const options=useMemo(()=>{
+		if(Array.isArray(props.options)){
+			return props.options.map((option)=>{
+				if(typeof option === 'object'){return option;}
+				return {label:option,value:option};
+			});
+		}
+		return Object.keys(props.options).map((label)=>{
+			return {label,value:props.options[label]};
+		});
+	},[props.options]);
+	
+	return (
+		<div className={"RadioButtons"}>
+			{options.map((option)=>{
+				const selected=option.value===value;
+				return (
+					<div className={"RadioButton"+(selected?' selected':'')} onClick={(e)=>{onChange(option.value);}}>
+						<div className={"RadioButtonIcon"+(selected?' selected':'')} > </div>
+						{option.label}
+					</div>
+				);
+			})}
+		</div>
+	);
+}
