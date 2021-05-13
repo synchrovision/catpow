2028572fe8078af86f9b25ab9bd0ffbd29a91544
synchrovision <trinitybluster@yahoo.co.jp>
2021-05-13 12:10:25 +0900

    fix :bug: resolve conf with parent class

--- classes/meta/todouhuken.php
+++ classes/meta/todouhuken.php
@@ -2,13 +2,14 @@
 namespace Catpow\meta;
 
 class todouhuken extends select_json{
+	public static $output_type='text';
 	
 	public static function fill_conf(&$conf){
 		$conf['value']='todouhuken';
 	}
 	public static function resolve_conf($conf){
 		if(empty($conf['role'])){$conf['role']='prefecture';}
-		return $conf;
+		return parent::resolve_conf($conf);
 	}
 }
 ?>
