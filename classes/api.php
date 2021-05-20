b2adfe81d8e81887aa31e7be50bbf901a5c21964
synchrovision <trinitybluster@yahoo.co.jp>
2021-05-20 20:54:28 +0900

    change :wrench: share nonce through same class api

--- classes/api.php
+++ classes/api.php
@@ -13,7 +13,7 @@ abstract class api{
 	}
 	public static function permission($req){
 		if(!empty(static::$method) && $req->get_method()!==static::$method){return false;}
-		if(!empty(static::$check_nonce) && !(wp_verify_nonce($_SERVER['HTTP_X_WP_NONCE'], 'wp_rest') && wp_verify_nonce($_SERVER['HTTP_X_CP_NONCE'],$req['content_path']))){return false;}
+		if(!empty(static::$check_nonce) && !(wp_verify_nonce($_SERVER['HTTP_X_WP_NONCE'], 'wp_rest') && wp_verify_nonce($_SERVER['HTTP_X_CP_NONCE'],$req['data_type'].'/'.$req['data_name']))){return false;}
 		if(!empty(static::$capability) && !current_user_can(static::$capability)){return false;}
 		return true;
 	}
