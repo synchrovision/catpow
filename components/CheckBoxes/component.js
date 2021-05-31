1c8eda0151f4e7fd3962846083d1b94d49cce181
synchrovision <trinitybluster@yahoo.co.jp>
2021-05-31 17:20:58 +0900

    feat :+1: add CheckBoxes component

---
+++ components/CheckBoxes/component.js
@@ -0,0 +1,41 @@
+Catpow.CheckBoxes = function (props) {
+  var useMemo = wp.element.useMemo;
+  var value = props.value,
+      _onChange = props.onChange;
+  var _Catpow = Catpow,
+      CheckBox = _Catpow.CheckBox;
+  var options = useMemo(function () {
+    if (Array.isArray(props.options)) {
+      return props.options.map(function (option) {
+        if (babelHelpers.typeof(option) === 'object') {
+          return option;
+        }
+
+        return {
+          label: option,
+          value: option
+        };
+      });
+    }
+
+    return Object.keys(props.options).map(function (label) {
+      return {
+        label: label,
+        value: props.options[label]
+      };
+    });
+  }, [props.options]);
+  return wp.element.createElement("div", {
+    className: "CheckBoxes"
+  }, options.map(function (option) {
+    return wp.element.createElement(CheckBox, {
+      label: option.label,
+      onChange: function onChange(selected) {
+        _onChange(selected ? value.concat(value, [option.value]) : value.filter(function (val) {
+          return val !== option.value;
+        }));
+      },
+      selected: value.indexOf(option.value) !== -1
+    });
+  }));
+};
---
+++ components/CheckBoxes/component.jsx
@@ -0,0 +1,35 @@
+﻿Catpow.CheckBoxes=(props)=>{
+	const {useMemo}=wp.element;
+	const {value,onChange}=props;
+	const {CheckBox}=Catpow;
+	
+	const options=useMemo(()=>{
+		if(Array.isArray(props.options)){
+			return props.options.map((option)=>{
+				if(typeof option === 'object'){return option;}
+				return {label:option,value:option};
+			});
+		}
+		return Object.keys(props.options).map((label)=>{
+			return {label,value:props.options[label]};
+		});
+	},[props.options]);
+	
+	return (
+		<div className={"CheckBoxes"}>
+			{options.map((option)=>(
+				<CheckBox
+					label={option.label}
+					onChange={(selected)=>{
+						onChange(
+							selected?
+							value.concat(value,[option.value]):
+							value.filter((val)=>val!==option.value)
+						)
+					}}
+					selected={value.indexOf(option.value)!==-1}
+				/>
+			))}
+		</div>
+	);
+}
