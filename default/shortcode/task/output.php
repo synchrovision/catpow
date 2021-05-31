dea1e222631ad7c94752adb492064211979ad3a9
synchrovision <trinitybluster@yahoo.co.jp>
2021-05-31 17:08:25 +0900

    feat :+1: enable to define flags at beacon shortcode

--- default/shortcode/task/output.php
+++ default/shortcode/task/output.php
@@ -6,6 +6,6 @@ if(empty($task)){return;}
 switch($prm[0]){
 	case 'url':echo $task->get_url();break;
 	case 'id':echo $task->get_task_id();break;
-	case 'beacon':$task->beacon();break;
+	case 'beacon':$task->beacon($atts['name']??'beacon',(empty($atts['count'])?0:1)&(empty($atts['unlimited'])?0:2));break;
 }
 $task->save();
