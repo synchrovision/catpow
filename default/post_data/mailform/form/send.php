5e3c4d5a0925569a469bacbaf074c5ee542b2d82
synchrovision <trinitybluster@yahoo.co.jp>
2021-06-01 12:14:33 +0900

    refactor :recycle: use get_block_code

--- default/post_data/mailform/form/send.php
+++ default/post_data/mailform/form/send.php
@@ -4,21 +4,14 @@ $post_data['meta']=[
 	'clear'=>[1],
 	'receive'=>[-1],
 	'push'=>[1],
-	'send_mail'=>[$mail_conf_post_type.'/notice',$mail_conf_post_type.'/thanks'],
-	'check_task'=>[-1]
+	'send_mail'=>[$mail_conf_post_type.'/notice',$mail_conf_post_type.'/thanks']
 ];
-ob_start();
-?>
-<!-- wp:catpow/simpletable -->
-<table class="wp-block-catpow-simpletable inputs"><tbody><?php
-	foreach($conf_data['meta']['mail']['meta'] as $name=>$conf){
-		$classes='item';
-		if($conf['required']??false){$classes.=' required';}
-		else{$classes.=' optional';}
-		echo "<tr class=\"{$classes}\"><th>{$conf['label']}</th><td>[output {$name}]</td></tr>";
-	}
-?></tbody></table>
-<!-- /wp:catpow/simpletable -->
-
-<?php
-$post_data['post_content']=ob_get_clean();
+$post_data['post_content']=cp::get_block_code('simpletable',[
+	'classes'=>'inputs',
+	'rows'=>array_map(function($conf,$name){
+		return ['classes'=>empty($conf['required'])?'optional':'required','cells'=>[
+			['text'=>$conf['label']],
+			['text'=>"[output {$name}]"]
+		]];
+	},$conf_data['inputs'],array_keys($conf_data['inputs']))
+]);
