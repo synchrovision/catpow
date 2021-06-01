362925fde84c9d43d561ded36bb75a7118db2c14
synchrovision <trinitybluster@yahoo.co.jp>
2021-06-01 17:35:17 +0900

    feat :+1: add block code generator

---
+++ blocks/t-heading/gen.php
@@ -0,0 +1,3 @@
+<!-- wp:catpow/t-heading -->
+<table width="100%" class="wp-block-catpow-t-heading <?=$attr['classes']??'left medium'?>"><tbody><tr><td><?=$attr['title']??''?></td></tr></tbody></table>
+<!-- /wp:catpow/t-heading -->
