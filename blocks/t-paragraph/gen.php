2a32b51aee475a7530a29d0e247f684c4740a2ab
synchrovision <trinitybluster@yahoo.co.jp>
2021-06-01 17:34:20 +0900

    fix :bug: fix typo

--- blocks/t-paragraph/gen.php
+++ blocks/t-paragraph/gen.php
@@ -1,3 +1,3 @@
 <!-- wp:catpow/t-paragraph -->
-<table width="100%" class="wp-block-catpow-t-paragraph <?=$attr['classses']??'left medium'?>"><tbody><tr><td><?=$attr['text']??''?></td></tr></tbody></table>
+<table width="100%" class="wp-block-catpow-t-paragraph <?=$attr['classes']??'left medium'?>"><tbody><tr><td><?=$attr['text']??''?></td></tr></tbody></table>
 <!-- /wp:catpow/t-paragraph -->
