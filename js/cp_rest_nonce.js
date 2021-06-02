67ebe0b4a7dda7d44cd044db37f31bde66916bde
synchrovision <trinitybluster@yahoo.co.jp>
2021-06-02 13:20:28 +0900

    feat :+1: send nonce by class_path if exists

--- js/cp_rest_nonce.js
+++ js/cp_rest_nonce.js
@@ -1,10 +1,18 @@
 /* global wp console cp_rest_nonces*/
 wp.apiFetch.use(function(options,next){
 	if(cp_rest_nonces && options.path.substr(0,7)==='/cp/v1/'){
-		const content_path=options.path.substr(7).split('/').slice(0,3).join('/');
+		const chunks=options.path.substr(7).split('/');
+		const class_path=chunks.slice(0,2).join('/');
+		if(cp_rest_nonces[class_path]){
+			if(!options.headers){options.headers={};}
+			options.headers['X-CP-Nonce']=cp_rest_nonces[class_path];
+			return next(options);
+		}
+		const content_path=chunks.slice(0,3).join('/');
 		if(cp_rest_nonces[content_path]){
 			if(!options.headers){options.headers={};}
 			options.headers['X-CP-Nonce']=cp_rest_nonces[content_path];
+			return next(options);
 		}
 	}
 	return next(options);
