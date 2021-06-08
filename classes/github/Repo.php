70390c3af8106a250c057d7c3a06d2286ab32218
synchrovision <trinitybluster@yahoo.co.jp>
2021-06-08 12:30:02 +0900

    feat :+1: enable to bind to plugin

--- classes/github/Repo.php
+++ classes/github/Repo.php
@@ -1,14 +1,26 @@
 <?php
 namespace Catpow\github;
+use Michelf\MarkdownExtra;
 
 class Repo{
 	protected $repo,$access_token;
+	public $plugin,$plugin_data;
 	const DEFAULT_REPO='synchrovision/catpow';
 	
 	public function __construct($repo=null,$access_token=null){
 		$this->repo=$repo??static::DEFAULT_REPO;
 		$this->access_token=$access_token;
 	}
+	public static function of_plugin($plugin){
+		$plugin_data=get_plugin_data(\WP_PLUGIN_DIR.'/'.$plugin);
+		if(empty($plugin_data['GitHub Repository'])){return false;}
+		$rtn=new self($plugin_data['GitHub Repository']);
+		$rtn->plugin=$plugin;
+		$rtn->plugin_data=$plugin_data;
+		return $rtn;
+	}
+	
+	
 	function query($path){
 		$url='https://api.github.com/repos/'.$this->repo.'/'.$path;
 		if ($this->token){
@@ -33,6 +45,43 @@ class Repo{
 				return $this->latestRelease['tag_name'];
 			case 'latestZipUrl':
 				return sprintf('https://github.com/%s/archive/%s.zip',$this->repo,$this->latestVersion);
+			case 'hasNewerRelease':
+				if(empty($this->plugin) || empty($this->plugin_data)){return false;}
+				return version_compare($this->latestVersion,$this->plugin_data['Version'],'>');
+			case 'dataForTransientUpdatePlugins':
+					$res=[
+						'slug'=>dirname($this->plugin),
+						'plugin'=>$this->plugin,
+						'new_version'=>$this->latestVersion,
+						'url'=>$this->html_url,
+						'package'=>$this->latestZipUrl
+					];
+				return (object)$res;
+			case 'dataForPluginsApi':
+				if(empty($this->plugin) || empty($this->plugin_data)){return false;}
+				$slug=dirname($this->plugin);
+				if(is_file($f=\WP_PLUGIN_DIR.'/'.$slug.'/README.md')){
+					$readme=MarkdownExtra::defaultTransform(file_get_contents($f));
+				}
+				$res=[
+					'slug'=>$slug,
+					'name'=>$this->plugin_data['Name'],
+					'plugin_name'=>$this->plugin_data['Name'],
+					'author'=>sprintf(
+						'<a href="%1$s" target="_blank">%2$s</a>',
+						esc_url($this->author['html_url']),
+						esc_html($this->author['login'])
+					),
+					'homepage'=>$this->homepage,
+					'version'=>$this->tag_name,
+					'last_updated'=>$this->published_at,
+					'download_link'=>esc_url($this->latestZipUrl),
+					'sections'=>[
+						'readme'=>$readme??'',
+						'changelog'=>MarkdownExtra::defaultTransform($this->body)
+					]
+				];
+				return (object)$res;
 			default:
 				return $this->latestRelease[$name]??null;
 		}
