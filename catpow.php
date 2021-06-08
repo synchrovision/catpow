6045a6e36ea5fc142cc3285f0aa63151b0216282
synchrovision <trinitybluster@yahoo.co.jp>
2021-06-08 12:30:59 +0900

    chore :up: update doc comment

--- catpow.php
+++ catpow.php
@@ -1,16 +1,15 @@
 <?php
-/**
- * @package Catpow
- * @version 1.0.0-alpha
- */
 /*
 Plugin Name: Catpow
 Description: オリジナルCMS制作支援フレームワーク。オリジナルCMSの開発のためのWordPress中級者以上向けのプラグインです。PHP7.2〜　Wordpress5.0〜
 Author: synchro_vision
-Version: 1.1
+Version: 0.2.0-alpha
 Author URI: https://twitter.com/synchro_vision
 Text Domain: catpow
 Domain Path: /languages
+GitHub Repository: synchrovision/catpow
+Requires at least: 5.6
+Requires PHP: 7.4
 */
 include __DIR__.'/vendor/autoload.php';
 include __DIR__.'/classes/CP.php';
