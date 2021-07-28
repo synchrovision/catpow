7da4f0a261b05d44913240686c4d3800e3add45d
synchrovision <trinitybluster@yahoo.co.jp>
2021-07-28 15:24:29 +0900

    feat :+1: replace url in options on change site url

--- action/admin_init-options.php
+++ action/admin_init-options.php
@@ -9,6 +9,10 @@ add_filter('pre_update_option_siteurl',function($new,$old){
 			post_content = REPLACE( post_content, \"$old\", \"$new\" ),
 			guid = REPLACE( guid, \"$old\", \"$new\" )
 		");
+		$wpdb->query("
+			UPDATE $wpdb->options SET
+			option_value = REPLACE( option_value, \"$old\", \"$new\" )
+		");
 	}
 	return $new;
 },10,2);
