50d27f7c06d8e69302f4436c15b995b19507dc86
synchrovision <trinitybluster@yahoo.co.jp>
2021-09-24 20:20:23 +0900

    fix :bug: prevent undefined error

--- default/post_data/mailconf/thanks.php
+++ default/post_data/mailconf/thanks.php
@@ -8,16 +8,16 @@ $post_data['meta']=[
 $post_data['post_content']=cp::get_block_code('t-body',[
 	'headerText'=>sprintf('<img class="wp-image" src="%s" alt="%s" style="width: 250px;"/>',get_theme_file_uri('images/logo.svg'),get_bloginfo('title')),
 	'footerText'=>get_bloginfo('name').'<br/>'.home_url(),
-	'textMail'=>implode("\n",array_map(function($conf){
-		return "{$conf['label']}：[output {$conf['name']}]";
-	},$conf_data['inputs']))
+	'textMail'=>implode("\n",array_map(function($conf,$name){
+		return "{$conf['label']}：[output {$name}]";
+	},$conf_data['inputs'],array_keys($conf_data['inputs'])))
 ],[
 	cp::get_block_code('t-box',[],[
-		implode("\n",array_map(function($conf){
+		implode("\n",array_map(function($conf,$name){
 			return cp::get_block_code('t-paragraph',[
 				'classes'=>'medium left',
-				'text'=>"{$conf['label']}：[output {$conf['name']}]"
+				'text'=>"{$conf['label']}：[output {$name}]"
 			]);
-		},$conf_data['inputs']))
+		},$conf_data['inputs'],array_keys($conf_data['inputs'])))
 	])
 ]);
