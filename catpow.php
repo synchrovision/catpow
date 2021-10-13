06d593300c85659e0a8058af959a5a1df6557b3a
synchrovision <trinitybluster@yahoo.co.jp>
2021-10-13 20:51:04 +0900

    chore :up: update version

--- catpow.php
+++ catpow.php
@@ -3,7 +3,7 @@
 Plugin Name: Catpow
 Description: サイト固有の機能を実装したオリジナルデザインのテーマを開発するためのプラグインです。
 Author: synchro_vision
-Version: 0.3.11-alpha
+Version: 0.3.12-alpha
 Author URI: https://catpow.info
 Text Domain: catpow
 Domain Path: /languages
