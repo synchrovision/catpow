5a6c742d22b15295839674a906ba494db2f57b87
synchrovision <trinitybluster@yahoo.co.jp>
2021-10-15 15:18:42 +0900

    fix :bug: use MYSQL_ATTR_USE_BUFFERED_QUERY

--- classes/cpdb.php
+++ classes/cpdb.php
@@ -23,8 +23,7 @@ class cpdb{
 			array(
 				\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
 				\PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
-				\PDO::ATTR_EMULATE_PREPARES => false,
-				\PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => false
+				\PDO::ATTR_EMULATE_PREPARES => false
 			)
 		);
 		$this->init();
