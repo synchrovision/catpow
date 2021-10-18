1ae8bd306a82d16961bc2a80731c1ffcec8958c5
synchrovision <trinitybluster@yahoo.co.jp>
2021-10-18 20:49:23 +0900

    feat :+1: recreate PhoneNumber UI with functional component

--- ui/PhoneNumber/input.js
+++ ui/PhoneNumber/input.js
@@ -1,97 +1,143 @@
-Catpow.UI.PhoneNumber = function (_wp$element$Component) {
-	babelHelpers.inherits(_class, _wp$element$Component);
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) { symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); } keys.push.apply(keys, symbols); } return keys; }
 
-	function _class(props) {
-		babelHelpers.classCallCheck(this, _class);
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { babelHelpers.defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
 
-		var _this = babelHelpers.possibleConstructorReturn(this, (_class.__proto__ || Object.getPrototypeOf(_class)).call(this, props));
+Catpow.UI.PhoneNumber = function (props) {
+  var _wp$element = wp.element,
+      useState = _wp$element.useState,
+      useMemo = _wp$element.useMemo,
+      useRef = _wp$element.useRef,
+      useCallback = _wp$element.useCallback,
+      useReducer = _wp$element.useReducer;
+  var init = useCallback(function (state) {
+    var value = state.value;
+    var secs = [];
+    return {
+      value: value,
+      secs: value.split('-'),
+      isComposing: false
+    };
+  }, []);
+  var reducer = useCallback(function (state, action) {
+    switch (action.type) {
+      case 'SET_SEC':
+        {
+          var i = action.i,
+              val = action.val;
 
-		_this.secs0 = React.createRef();
-		_this.secs1 = React.createRef();
-		_this.secs2 = React.createRef();
-		_this.state = { value: props.value, isComposing: false };
-		return _this;
-	}
+          if (!val.match(/^\d*$/)) {
+            return state;
+          }
 
-	babelHelpers.createClass(_class, [{
-		key: 'render',
-		value: function render() {
-			var _this2 = this;
+          var secs = state.value.split('-');
 
-			var _state = this.state,
-			    value = _state.value,
-			    isComposing = _state.isComposing;
+          if (val.length === 10) {
+            secs[0] = val.substring(0, 2);
+            secs[1] = val.substring(2, 6);
+            secs[2] = val.substring(6);
+          } else if (val.length === 11) {
+            secs[0] = val.substring(0, 3);
+            secs[1] = val.substring(3, 7);
+            secs[2] = val.substring(7);
+          } else {
+            secs[i] = val;
 
-			var secs = value.split('-');
+            if (val.length > 3 && i < 2 || i == 0 && val.match(/^0\d0$/)) {
+              if (!state.isComposing) {
+                refs[i + 1].current.focus();
+              }
+            }
+          }
 
-			var setValue = function setValue(i, val) {
-				if (!val.match(/^\d+$/)) {
-					val = '';
-				}
-				if (val.length === 10) {
-					secs[0] = val.substring(0, 2);
-					secs[1] = val.substring(2, 6);
-					secs[2] = val.substring(6);
-				} else if (val.length === 11) {
-					secs[0] = val.substring(0, 3);
-					secs[1] = val.substring(3, 7);
-					secs[2] = val.substring(7);
-				} else {
-					secs[i] = val;
-					if (val.length > 3 && i < 2 || i == 0 && val.match(/^0\d0$/)) {
-						if (!isComposing) {
-							_this2['secs' + (i + 1)].current.focus();
-						}
-					}
-				}
-				_this2.setState({ value: secs.join('-') });
-			};
+          return _objectSpread(_objectSpread({}, state), {}, {
+            secs: secs,
+            value: secs.join('-')
+          });
+        }
 
-			var input = function input(i) {
-				return wp.element.createElement('input', {
-					type: 'text',
-					className: "sec" + i,
-					size: '4',
-					autocomplete: ['tel-area-code', 'tel-local-prefix', 'tel-local-suffix'][i],
-					onChange: function onChange(e) {
-						var val = e.target.value;
-						setValue(i, e.target.value);
-					},
-					onCompositionStart: function onCompositionStart(e) {
-						_this2.setState({ isComposing: true });
-					},
-					onCompositionEnd: function onCompositionEnd(e) {
-						isComposing = false;
-						setValue(i, e.target.value);
-						_this2.setState({ isComposing: isComposing });
-					},
-					ref: _this2['secs' + i],
-					value: secs[i]
-				});
-			};
+      case 'START_COMPOSE':
+        return _objectSpread(_objectSpread({}, state), {}, {
+          isComposing: true
+        });
 
-			return wp.element.createElement(
-				'div',
-				{ className: 'PhoneNumber' },
-				input(0),
-				wp.element.createElement(
-					'span',
-					{ 'class': 'sep' },
-					'-'
-				),
-				input(1),
-				wp.element.createElement(
-					'span',
-					{ 'class': 'sep' },
-					'-'
-				),
-				input(2),
-				wp.element.createElement(Catpow.UI.HiddenValues, {
-					name: this.props.name,
-					value: value
-				})
-			);
-		}
-	}]);
-	return _class;
-}(wp.element.Component);
+      case 'END_COMPOSE':
+        return _objectSpread(_objectSpread({}, state), {}, {
+          isComposing: false
+        });
+    }
+
+    return state;
+  }, []);
+
+  var _useReducer = useReducer(reducer, {
+    value: props.value
+  }, init),
+      _useReducer2 = babelHelpers.slicedToArray(_useReducer, 2),
+      state = _useReducer2[0],
+      dispatch = _useReducer2[1];
+
+  var Input = useCallback(function (_ref) {
+    var i = _ref.i,
+        refs = _ref.refs,
+        state = _ref.state,
+        dispatch = _ref.dispatch;
+    return wp.element.createElement("input", {
+      type: "text",
+      className: "sec" + i,
+      size: "4",
+      autocomplete: ['tel-area-code', 'tel-local-prefix', 'tel-local-suffix'][i],
+      onChange: function onChange(e) {
+        var val = e.target.value;
+        dispatch({
+          type: 'SET_SEC',
+          i: i,
+          val: val
+        });
+      },
+      onCompositionStart: function onCompositionStart(e) {
+        dispatch({
+          type: 'START_COMPOSE'
+        });
+      },
+      onCompositionEnd: function onCompositionEnd(e) {
+        var val = e.target.value;
+        dispatch({
+          type: 'SET_SEC',
+          i: i,
+          val: val
+        });
+        dispatch({
+          type: 'END_COMPOSE'
+        });
+      },
+      ref: refs[i],
+      value: state.secs[i]
+    });
+  }, []);
+  var refs = [useRef({}), useRef({}), useRef({})];
+  return wp.element.createElement("div", {
+    className: 'PhoneNumber'
+  }, wp.element.createElement(Input, {
+    i: 0,
+    refs: refs,
+    state: state,
+    dispatch: dispatch
+  }), wp.element.createElement("span", {
+    class: "sep"
+  }, "-"), wp.element.createElement(Input, {
+    i: 1,
+    refs: refs,
+    state: state,
+    dispatch: dispatch
+  }), wp.element.createElement("span", {
+    class: "sep"
+  }, "-"), wp.element.createElement(Input, {
+    i: 2,
+    refs: refs,
+    state: state,
+    dispatch: dispatch
+  }), wp.element.createElement(Catpow.UI.HiddenValues, {
+    name: props.name,
+    value: state.value
+  }));
+};
--- ui/PhoneNumber/input.jsx
+++ ui/PhoneNumber/input.jsx
@@ -1,74 +1,79 @@
-﻿Catpow.UI.PhoneNumber=class extends wp.element.Component{
-	constructor(props) {
-		super(props);
-		this.secs0=React.createRef();
-		this.secs1=React.createRef();
-		this.secs2=React.createRef();
-		this.state={value:props.value,isComposing:false};
-	}
-	render(){
-		var {value,isComposing}=this.state;
-		var secs=value.split('-');
-		
-		const setValue=(i,val)=>{
-			if(!val.match(/^\d+$/)){val='';}
-			if(val.length===10){
-				secs[0]=val.substring(0,2);
-				secs[1]=val.substring(2,6);
-				secs[2]=val.substring(6);
-			}
-			else if(val.length===11){
-				secs[0]=val.substring(0,3);
-				secs[1]=val.substring(3,7);
-				secs[2]=val.substring(7);
-			}
-			else{
-				secs[i]=val;
-				if((val.length>3 && i<2) || (i==0 && val.match(/^0\d0$/))){
-					if(!isComposing){
-						this['secs'+(i+1)].current.focus();
+﻿Catpow.UI.PhoneNumber=(props)=>{
+	const {useState,useMemo,useRef,useCallback,useReducer}=wp.element;
+	
+	const init=useCallback((state)=>{
+		const {value}=state;
+		const secs=[];
+		return {value,secs:value.split('-'),isComposing:false};
+	},[]);
+	const reducer=useCallback((state,action)=>{
+		switch(action.type){
+			case 'SET_SEC':{
+				const {i,val}=action;
+				if(!val.match(/^\d*$/)){return state;}
+				const secs=state.value.split('-');
+				if(val.length===10){
+					secs[0]=val.substring(0,2);
+					secs[1]=val.substring(2,6);
+					secs[2]=val.substring(6);
+				}
+				else if(val.length===11){
+					secs[0]=val.substring(0,3);
+					secs[1]=val.substring(3,7);
+					secs[2]=val.substring(7);
+				}
+				else{
+					secs[i]=val;
+					if((val.length>3 && i<2) || (i==0 && val.match(/^0\d0$/))){
+						if(!state.isComposing){
+							refs[i+1].current.focus();
+						}
 					}
 				}
+				return {...state,secs,value:secs.join('-')};
 			}
-			this.setState({value:secs.join('-')});
-		};
-		
-		const input=(i)=>(
-			<input
-				type="text"
-				className={"sec"+i}
-				size="4"
-				autocomplete={['tel-area-code','tel-local-prefix','tel-local-suffix'][i]}
-				onChange={(e)=>{
-					var val=e.target.value;
-					setValue(i,e.target.value);
-				}}
-				onCompositionStart={(e)=>{
-					this.setState({isComposing:true});
-				}}
-				onCompositionEnd={(e)=>{
-					isComposing=false;
-					setValue(i,e.target.value);
-					this.setState({isComposing});
-				}}
-				ref={this['secs'+i]}
-				value={secs[i]}
-			/>
-		);
-		
-		return (
-			<div className={'PhoneNumber'}>
-				{input(0)}
-				<span class="sep">-</span>
-				{input(1)}
-				<span class="sep">-</span>
-				{input(2)}
+			case 'START_COMPOSE':return {...state,isComposing:true};
+			case 'END_COMPOSE':return {...state,isComposing:false};
+		}
+		return state;
+	},[]);
+	const [state,dispatch]=useReducer(reducer,{value:props.value},init);
+	const Input=useCallback(({i,refs,state,dispatch})=>(
+		<input
+			type="text"
+			className={"sec"+i}
+			size="4"
+			autocomplete={['tel-area-code','tel-local-prefix','tel-local-suffix'][i]}
+			onChange={(e)=>{
+				const val=e.target.value;
+				dispatch({type:'SET_SEC',i,val});
+			}}
+			onCompositionStart={(e)=>{
+				dispatch({type:'START_COMPOSE'});
+			}}
+			onCompositionEnd={(e)=>{
+				const val=e.target.value;
+				dispatch({type:'SET_SEC',i,val});
+				dispatch({type:'END_COMPOSE'});
+			}}
+			ref={refs[i]}
+			value={state.secs[i]}
+		/>
+	),[]);
+	const refs=[useRef({}),useRef({}),useRef({})];
+	
+	return (
+		<div className={'PhoneNumber'}>
+			<Input i={0} refs={refs} state={state} dispatch={dispatch}/>
+			<span class="sep">-</span>
+			<Input i={1} refs={refs} state={state} dispatch={dispatch}/>
+			<span class="sep">-</span>
+			<Input i={2} refs={refs} state={state} dispatch={dispatch}/>
 
-				<Catpow.UI.HiddenValues
-					name={this.props.name}
-					value={value}
-				/>
-			</div>
-		);
-	}
+			<Catpow.UI.HiddenValues
+				name={props.name}
+				value={state.value}
+			/>
+		</div>
+	);
 }
