254c516feb5bfe78792a8312fe3e4af1b07b41e5
synchrovision <trinitybluster@yahoo.co.jp>
2022-02-03 20:03:25 +0900

    refactor :recycle: use createPotal

--- components/External/component.js
+++ components/External/component.js
@@ -1,62 +1,55 @@
 Catpow.External = function (props) {
-	var children = props.children,
-	    trace = props.trace;
-	var _wp$element = wp.element,
-	    render = _wp$element.render,
-	    useState = _wp$element.useState,
-	    useMemo = _wp$element.useMemo,
-	    useCallback = _wp$element.useCallback,
-	    useEffect = _wp$element.useEffect,
-	    useRef = _wp$element.useRef;
+  var children = props.children,
+      trace = props.trace;
+  var _wp$element = wp.element,
+      render = _wp$element.render,
+      useState = _wp$element.useState,
+      useMemo = _wp$element.useMemo,
+      useCallback = _wp$element.useCallback,
+      useEffect = _wp$element.useEffect,
+      useRef = _wp$element.useRef,
+      createPortal = _wp$element.createPortal;
+  var ref = useRef({
+    contents: false,
+    setContents: function setContents() {}
+  });
+  var el = useMemo(function () {
+    if (props.id) {
+      var exEl = document.getElementById(props.id);
 
+      if (exEl) {
+        return exEl;
+      }
+    }
 
-	var ref = useRef({ contents: false, setContents: function setContents() {} });
+    var el = document.createElement('div');
 
-	var el = useMemo(function () {
-		var el = document.createElement('div');
-		el.className = props.className;
-		document.body.appendChild(el);
-		return el;
-	}, []);
+    if (props.id) {
+      el.id = props.id;
+    }
 
-	var ExternalContents = useCallback(function (props) {
-		var _useState = useState(children);
+    el.className = props.className;
+    document.body.appendChild(el);
+    return el;
+  }, []);
 
-		var _useState2 = babelHelpers.slicedToArray(_useState, 2);
+  if (trace) {
+    useEffect(function () {
+      el.style.position = 'absolute';
+      var timer = setInterval(function () {
+        if (trace.getBoundingClientRect) {
+          var bnd = trace.getBoundingClientRect();
+          el.style.left = window.scrollX + bnd.left + 'px';
+          el.style.top = window.scrollY + bnd.top + 'px';
+          el.style.width = bnd.width + 'px';
+          el.style.height = bnd.height + 'px';
+        }
+      }, 50);
+      return function () {
+        return clearInterval(timer);
+      };
+    }, [trace]);
+  }
 
-		ref.current.contents = _useState2[0];
-		ref.current.setContents = _useState2[1];
-
-		return ref.current.contents;
-	}, []);
-
-	useEffect(function () {
-		render(wp.element.createElement(ExternalContents, null), el);
-		return function () {
-			return el.remove();
-		};
-	}, []);
-	useEffect(function () {
-		ref.current.setContents(children);
-	}, [children]);
-
-	if (trace) {
-		useEffect(function () {
-			el.style.position = 'absolute';
-			var timer = setInterval(function () {
-				if (trace.getBoundingClientRect) {
-					var bnd = trace.getBoundingClientRect();
-					el.style.left = window.scrollX + bnd.left + 'px';
-					el.style.top = window.scrollY + bnd.top + 'px';
-					el.style.width = bnd.width + 'px';
-					el.style.height = bnd.height + 'px';
-				}
-			}, 50);
-			return function () {
-				return clearInterval(timer);
-			};
-		}, [trace]);
-	}
-
-	return false;
+  return createPortal(children, el);
 };
--- components/External/component.jsx
+++ components/External/component.jsx
@@ -1,26 +1,20 @@
 ﻿Catpow.External=(props)=>{
 	const {children,trace}=props;
-	const {render,useState,useMemo,useCallback,useEffect,useRef}=wp.element;
+	const {render,useState,useMemo,useCallback,useEffect,useRef,createPortal}=wp.element;
 	
 	const ref=useRef({contents:false,setContents:()=>{}});
 	
 	const el=useMemo(()=>{
+		if(props.id){
+			const exEl=document.getElementById(props.id);
+			if(exEl){return exEl;}
+		}
 		const el=document.createElement('div');
+		if(props.id){el.id=props.id;}
 		el.className=props.className;
 		document.body.appendChild(el);
 		return el;
 	},[]);
-	
-	const ExternalContents=useCallback((props)=>{
-		[ref.current.contents,ref.current.setContents]=useState(children);
-		return ref.current.contents;
-	},[]);
-	
-	useEffect(()=>{
-		render(<ExternalContents/>,el);
-		return ()=>el.remove();
-	},[]);
-	useEffect(()=>{ref.current.setContents(children);},[children]);
 	
 	if(trace){
 		useEffect(()=>{
@@ -38,5 +32,5 @@
 		},[trace]);
 	}
 	
-	return false;
+	return createPortal(children,el);
 }
