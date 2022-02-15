bbe1a7bc122e04fa4aad9870568f8cc92bfb3b0d
synchrovision <trinitybluster@yahoo.co.jp>
2022-02-15 18:04:25 +0900

    refactor :recycle: remove fonts settings from AdobeFonts function panel

--- functions/adobe_fonts/conf.php
+++ functions/adobe_fonts/conf.php
@@ -1,12 +1,7 @@
 <?php
-use Catpow\util\style_config;
 $conf=[
 	'cat'=>'adobe',
 	'meta'=>[
-		'cp_adobe_fonts_pid'=>['type'=>'options','option'=>'cp_adobe_fonts_pid','placeholder'=>'プロジェクトID','size'=>15],
-		'fonts'=>['type'=>'jsondata']
+		'cp_adobe_fonts_pid'=>['type'=>'options','option'=>'cp_adobe_fonts_pid','placeholder'=>'プロジェクトID','size'=>15]
 	]
 ];
-foreach(style_config::get_font_roles() as $role=>$font_conf){
-	$conf['meta']['fonts']['meta'][$role.'_font']=['type'=>'text','label'=>$font_conf['label'],'size'=>50];
-}
