8467a0b150800dc9d05d90afcc13c95492381d17
synchrovision <trinitybluster@yahoo.co.jp>
2022-02-15 17:57:07 +0900

    feat :+1: add fonts section to customizer

--- action/customize_register.php
+++ action/customize_register.php
@@ -1,17 +1,34 @@
 <?php
 use Catpow\util\style_config;
 
-foreach(style_config::get_color_roles() as $setting_name=>$color_setting){
-	$wp_customize->add_setting($setting_name,['default'=>$color_setting['default']]);
-	$wp_customize->add_control(new WP_Customize_Color_Control( $wp_customize, $setting_name, [
+$wp_customize->remove_control('background_color');
+$wp_customize->remove_control('header_textcolor');
+foreach(style_config::get_color_roles() as $role=>$color_setting){
+	$setting_id="colors[{$role}]";
+	$wp_customize->add_setting($setting_id,['default'=>$color_setting['default']]);
+	$wp_customize->add_control(new WP_Customize_Color_Control($wp_customize,$setting_id,[
 		'label'=> $color_setting['label'],
 		'section'=>'colors',
-		'settings'=>$setting_name,
+		'settings'=>$setting_id,
 	]));
-	add_action("customize_save_{$setting_name}",["Catpow\\util\\style_config",'update']);
+	
 }
+add_action("customize_save_colors",["Catpow\\util\\style_config",'update']);
 
-add_action("customize_save_background_color",["Catpow\\util\\style_config",'update']);
-$wp_customize->remove_control('header_textcolor');
+$wp_customize->add_section('fonts',[
+	'title'=>__('フォント','catpow'),
+	'priority'=>40
+]);
+	
 
-
+foreach(style_config::get_font_roles() as $role=>$font_setting){
+	$setting_id="fonts[{$role}]";
+	$wp_customize->add_setting($setting_id,['default'=>$font_setting['default']]);
+	$wp_customize->add_control($setting_id,[
+		'type'=>'text',
+		'label'=> $font_setting['label'],
+		'section'=>'fonts'
+	]);
+	
+}
+add_action("customize_save_fonts",["Catpow\\util\\style_config",'update']);
