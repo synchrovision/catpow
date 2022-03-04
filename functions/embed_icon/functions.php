76527b8c5fb8c86b82b7329a07d4aedb187cdf92
synchrovision <trinitybluster@yahoo.co.jp>
2022-03-04 19:05:48 +0900

    chore :up: remover useless code

--- functions/embed_icon/functions.php
+++ functions/embed_icon/functions.php
@@ -2,6 +2,5 @@
 add_filter('cp_block_items_attributes_iconHolder',function($items,$args){
 	$items['query']['embedIconSrc']=array_merge(["source"=>'attribute',"selector"=>'.embedIcon',"attribute"=>'data-src'],$args);
 	$items['query']['embedIconCode']=array_merge(["source"=>'html',"selector"=>'.embedIcon'],$args);
-	$items['iconComponent']='EmbedIcon';
 	return $items;
 },10,2);
