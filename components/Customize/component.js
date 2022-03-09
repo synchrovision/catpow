6cb929c3d89c9153bc745981bb72aadb713aa13b
synchrovision <trinitybluster@yahoo.co.jp>
2022-03-09 17:40:01 +0900

    feat :+1: add Customize component

---
+++ components/Customize/component.js
@@ -0,0 +1,38 @@
+/**
+* CatpowのWordPressのテーマカスタマイザで用いる
+* コンポーネントのベースとなるコンポーネント
+*/
+Catpow.Customize = function (props) {
+  var _wp$element = wp.element,
+      useState = _wp$element.useState,
+      useCallback = _wp$element.useCallback,
+      useMemo = _wp$element.useMemo,
+      useEffect = _wp$element.useEffect,
+      useRef = _wp$element.useRef,
+      useReducer = _wp$element.useReducer;
+  var id = props.id,
+      _props$type = props.type,
+      type = _props$type === void 0 ? 'Text' : _props$type,
+      param = props.param;
+
+  var _useState = useState(null),
+      _useState2 = babelHelpers.slicedToArray(_useState, 2),
+      value = _useState2[0],
+      setValue = _useState2[1];
+
+  useEffect(function () {
+    wp.customize(id, function (setting) {
+      setValue(setting.get());
+    });
+  }, [id]);
+  var onChange = useCallback(function (value) {
+    setValue(value);
+    wp.customize.control(id).setting.set(value);
+  }, [id]);
+  return wp.element.createElement(Catpow.Customize[type], {
+    id: id,
+    value: value,
+    onChange: onChange,
+    param: param
+  });
+};
---
+++ components/Customize/component.jsx
@@ -0,0 +1,21 @@
+﻿/**
+* CatpowのWordPressのテーマカスタマイザで用いる
+* コンポーネントのベースとなるコンポーネント
+*/
+
+
+Catpow.Customize=(props)=>{
+	const {useState,useCallback,useMemo,useEffect,useRef,useReducer}=wp.element;
+	const {id,type='Text',param}=props;
+	const [value,setValue]=useState(null);
+	
+	useEffect(()=>{
+		wp.customize(id,(setting)=>{setValue(setting.get());})
+	},[id]);
+	const onChange=useCallback((value)=>{
+		setValue(value);
+		wp.customize.control(id).setting.set(value);
+	},[id]);
+	
+	return wp.element.createElement(Catpow.Customize[type],{id,value,onChange,param});
+}
