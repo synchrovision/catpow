700d6aa5001c4c2351fff0c13499431934a77d9a
synchrovision <trinitybluster@yahoo.co.jp>
2022-03-09 17:41:41 +0900

    feat :+1: add customize control class

---
+++ classes/customize/control.php
@@ -0,0 +1,30 @@
+<?php
+namespace Catpow\customize;
+
+class control extends \WP_Customize_Control{
+	public $param=[];
+	public function get_component_param(){
+		return apply_filters('cp_customize_component_param_'.$this->type,$this->param);
+	}
+	public function enqueue(){
+		\cp::use_component('Customize/'.$this->type);
+	}
+	protected function render_content(){
+		$id='cp_customize_component_'.$this->id;
+		?>
+		<div id="<?=$id?>"></div>
+		<script>
+			wp.element.render(
+				wp.element.createElement(
+					Catpow.Customize,{
+						id:'<?=$this->id?>',
+						type:'<?=$this->type?>',
+						param:<?=json_encode($this->get_component_param(),0500)?>
+					}
+				),
+				document.getElementById('<?=$id?>')
+			);
+		</script>
+		<?php
+	}
+}
