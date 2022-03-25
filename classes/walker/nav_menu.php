483578af8356b53509d632c378b713ff30524d5e
synchrovision <trinitybluster@yahoo.co.jp>
2022-03-25 17:04:55 +0900

    feat :+1: enqueue style and script in template dir

--- classes/walker/nav_menu.php
+++ classes/walker/nav_menu.php
@@ -2,17 +2,20 @@
 namespace Catpow\walker;
 class nav_menu extends \Walker_Nav_Menu {
 	public function walk($menu_items,$max_depth,...$args){
+		$path="nav/{$args[0]->theme_location}/nav_menu";
 		$childrens=[];
 		foreach($menu_items as $menu_item){
 			if(!isset($childrens[$menu_item->ID])){$childrens[$menu_item->ID]=[];}
 			$childrens[$menu_item->menu_item_parent][]=$menu_item;
 		}
 		foreach($childrens as $id=>$children){
-			$childrens[$id]=new \Catpow\content\loop(['path'=>"nav/{$args[0]->theme_location}/nav_menu",'objects'=>$children]);
+			$childrens[$id]=new \Catpow\content\loop(['path'=>$path,'objects'=>$children]);
 		}
 		foreach($childrens as $id=>$children){
 			$childrens[$id]->childrens=$childrens;
 		}
+		\cp::enqueue_script($path.'/script.js');
+		\cp::enqueue_style($path.'/style.css');
 		$childrens[0]->render('loop.php');
 	}
 }
