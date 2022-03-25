c8c33b00636fdb068411fb860b456743be12b9e2
synchrovision <trinitybluster@yahoo.co.jp>
2022-03-25 17:20:47 +0900

    feat :+1: update type input

--- blocks/formbuttons/editor_script.js
+++ blocks/formbuttons/editor_script.js
@@ -32,9 +32,10 @@ registerBlockType('catpow/formbuttons', {
       values: 'i'
     }];
     var itemClasses = ['color', {
+      type: 'gridbuttons',
       label: '属性',
       filter: 'rank',
-      values: ['default', 'primary', 'negative', 'danger', 'secure']
+      values: ['default', 'primary', 'secondary', 'negative', 'danger', 'secure']
     }, {
       label: 'アイコン',
       values: 'hasIcon',
