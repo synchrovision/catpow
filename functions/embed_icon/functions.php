552e9fbafc98eb9ff2c488581fcc324f4da7d341
synchrovision <trinitybluster@yahoo.co.jp>
2022-04-06 16:01:43 +0900

    feat :+1: apply filters to block attributes

--- functions/embed_icon/functions.php
+++ functions/embed_icon/functions.php
@@ -1,4 +1,9 @@
 <?php
+add_filter('cp_block_attributes_iconHolder',function($atts,$args){
+	$atts['embedIconSrc']=array_merge(["source"=>'attribute',"selector"=>'.embedIcon',"attribute"=>'data-src'],$args);
+	$atts['embedIconCode']=array_merge(["source"=>'html',"selector"=>'.embedIcon'],$args);
+	return $atts;
+},10,2);
 add_filter('cp_block_items_attributes_iconHolder',function($items,$args){
 	$items['query']['embedIconSrc']=array_merge(["source"=>'attribute',"selector"=>'.embedIcon',"attribute"=>'data-src'],$args);
 	$items['query']['embedIconCode']=array_merge(["source"=>'html',"selector"=>'.embedIcon'],$args);
