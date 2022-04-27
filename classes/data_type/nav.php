648311ef103a838617571d0728bccde09ef57726
synchrovision <trinitybluster@yahoo.co.jp>
2022-04-27 12:21:36 +0900

    chore :up: update default template

--- classes/data_type/nav.php
+++ classes/data_type/nav.php
@@ -3,8 +3,8 @@ namespace Catpow\data_type;
 
 class nav extends post{
 	public static function get_default_templates($conf){
-		if(empty($conf['meta'])){return ['menu'];}
-		return ['menu','admin'];
+		if(empty($conf['meta'])){return ['menu_item'];}
+		return ['menu_item','admin'];
 	}
 }
 
