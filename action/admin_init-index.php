d7c0f267fcb56ec1e2ff3c20b8e7046cd7645208
synchrovision <trinitybluster@yahoo.co.jp>
2022-04-27 12:04:16 +0900

    feat :+1: enable to add control form

--- action/admin_init-index.php
+++ action/admin_init-index.php
@@ -1,7 +1,7 @@
 <?php
 namespace Catpow;
 
-add_action( 'wp_dashboard_setup',function(){
+add_action('wp_dashboard_setup',function(){
 	foreach(cp::get_file_urls('dashboard/widgets') as $widgets_dir=>$widgets_url){
 		if(!empty($files=glob($widgets_dir.'/*/contents.php'))){
 			array_walk($files,function($contents_file)use($widgets_url){
@@ -10,9 +10,21 @@ add_action( 'wp_dashboard_setup',function(){
 				if(file_exists($conf_file=$dir.'/conf.php')){include $conf_file;}
 				\cp::enqueue_style("dashboard/widgets/{$id}/style.css");
 				\cp::enqueue_script("dashboard/widgets/{$id}/script.js");
-				wp_add_dashboard_widget($id,$conf['label']??$id,function()use($contents_file){
-					include $contents_file;
-				});
+				if(file_exists($form_file=$dir.'/form.php')){
+					$control_callback=function()use($form_file){
+						include $form_file;
+					};
+				}
+				wp_add_dashboard_widget(
+					$id,
+					$conf['label']??$id,function($object,$args)use($contents_file){
+						include $contents_file;
+					},
+					$control_callback??null,
+					$conf['callback_args']??null,
+					$conf['context']??'normal'
+					,$conf['priority']??'core'
+				);
 			});
 		}
 	}
