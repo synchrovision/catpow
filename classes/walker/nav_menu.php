1073d695f4c692b57c1faa482f758dd43ea58ed6
synchrovision <trinitybluster@yahoo.co.jp>
2022-06-01 11:53:49 +0900

    feat :+1: use loop function to render menu

--- classes/walker/nav_menu.php
+++ classes/walker/nav_menu.php
@@ -10,24 +10,8 @@ class nav_menu extends \Walker_Nav_Menu {
 		switch($template_class_name::$menu_template_type){
 			case 'menu_item':
 				return call_user_func_array('parent::walk',func_get_args());
-				break;
 			case 'menu':
-				$path="nav/{$args[0]->theme_location}/{$this->template}";
-				$childrens=[];
-				foreach($menu_items as $menu_item){
-					if(!isset($childrens[$menu_item->ID])){$childrens[$menu_item->ID]=[];}
-					$childrens[$menu_item->menu_item_parent][]=$menu_item;
-				}
-				foreach($childrens as $id=>$children){
-					$childrens[$id]=new \Catpow\content\loop(['path'=>$path,'objects'=>$children]);
-				}
-				foreach($childrens as $id=>$children){
-					$childrens[$id]->childrens=$childrens;
-				}
-				\cp::enqueue_script($path.'/script.js');
-				\cp::enqueue_style($path.'/style.css');
-				$childrens[0]->render('loop.php');
-				break;
+				return \Catpow\loop("nav/{$args[0]->theme_location}/{$this->template}/loop.php");
 			case 'component':
 				
 		}
