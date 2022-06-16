e8600d1d12956a969bb3de03211c0553960fe9e9
synchrovision <trinitybluster@yahoo.co.jp>
2022-06-16 15:34:01 +0900

    feat :+1: get app name from config.json

--- classes/template_type/app.php
+++ classes/template_type/app.php
@@ -5,9 +5,11 @@ namespace Catpow\template_type;
 */
 
 class app extends template_type{
-	public static $app_name='App';
 	public static function get_embeddables($path,$conf_data){
-		return ['app'=>[static::$app_name=>'app']];
+		$config_json=\cp::get_file_path($path.'/config.json',\cp::FROM_THEME|\cp::FROM_CONFIG);
+		$config=json_decode(file_get_contents($config_json),true);
+		$app_name=$config['AppName']??'App';
+		return ['app'=>[$app_name=>'app.js']];
 	}
 }
 
