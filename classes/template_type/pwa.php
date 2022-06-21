be113218dc6c6012f8d9f80cca94102a8950ae87
synchrovision <trinitybluster@yahoo.co.jp>
2022-06-21 10:20:02 +0900

    feat :+1: use name in manifest.json

--- classes/template_type/pwa.php
+++ classes/template_type/pwa.php
@@ -7,9 +7,11 @@ namespace Catpow\template_type;
 class pwa extends template_type{
 	public static $permalinks=['file'];
 	
-	public static function get_nav_menu_items($conf_data){
+	public static function get_nav_menu_items($path,$conf_data){
+		$manifest_json=\cp::get_file_path($path.'/manifest.json',\cp::FROM_THEME|\cp::FROM_CONFIG);
+		$manifest=json_decode(file_get_contents($manifest_json),true);
 		return [
-			$conf_data['label'].' PWA'=>$conf_data['name'].'/pwa/index.html'
+			$manifest['name']=>$conf_data['name'].'/pwa/index.html'
 		];
 	}
 }
