9a7dbacd7e44c0e2c05a49bb6ff0be836e6e2601
synchrovision <trinitybluster@yahoo.co.jp>
2022-06-21 10:19:15 +0900

    change :wrench: change key name

--- classes/template_type/app.php
+++ classes/template_type/app.php
@@ -8,7 +8,7 @@ class app extends template_type{
 	public static function get_embeddables($path,$conf_data){
 		$config_json=\cp::get_file_path($path.'/config.json',\cp::FROM_THEME|\cp::FROM_CONFIG);
 		$config=json_decode(file_get_contents($config_json),true);
-		$app_name=$config['AppName']??'App';
+		$app_name=$config['name']??'App';
 		return ['app'=>[$app_name=>'app.js']];
 	}
 }
