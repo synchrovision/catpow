b2cc6918d3a23ae210bc512fe909e62a836a50f1
synchrovision <trinitybluster@yahoo.co.jp>
2022-07-26 16:35:15 +0900

    feat :+1: print css vars at login page

--- action/login_init.php
+++ action/login_init.php
@@ -3,4 +3,5 @@ add_action('login_enqueue_scripts',function(){
 	add_filter('login_headerurl',function($url){return home_url();});
 	add_filter('login_headertext',function($url){return get_bloginfo('name');});
 	cp::enqueue_style('admin.css');
+	Catpow\util\style_config::print_css_vars();
 });
