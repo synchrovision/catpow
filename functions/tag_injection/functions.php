eea6d5e8aa68853789961e16cd9fca45f50e6f6e
synchrovision <trinitybluster@yahoo.co.jp>
2022-08-08 13:05:24 +0900

    fix :bug: strip slashes before output code

--- functions/tag_injection/functions.php
+++ functions/tag_injection/functions.php
@@ -1,16 +1,16 @@
 <?php
 add_action('wp_head',function(){
 	if($tag=get_option('cp_tag_injection_wp_head')){
-		echo $tag;
+		echo stripslashes($tag);
 	}
 });
 add_action('wp_body_open',function(){
 	if($tag=get_option('cp_tag_injection_wp_body_open')){
-		echo $tag;
+		echo stripslashes($tag);
 	}
 });
 add_action('wp_footer',function(){
 	if($tag=get_option('cp_tag_injection_wp_footer')){
-		echo $tag;
+		echo stripslashes($tag);
 	}
 });
