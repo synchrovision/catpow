96c76b25b25efb70cbce72af73e006b2522125ae
synchrovision <trinitybluster@yahoo.co.jp>
2022-10-17 17:31:02 +0900

    feat :+1: enable to protect file if is future post

--- classes/meta/pdf.php
+++ classes/meta/pdf.php
@@ -1,8 +1,25 @@
 <?php
 namespace Catpow\meta;
+use Catpow\util\media_protect;
 
 class pdf extends media{
 	
+	public static function set($data_type,$data_name,$id,$meta_name,$vals,$conf){
+		self::protectMediaIfShould($data_type,$data_name,$id,$meta_name,$vals,$conf);
+		parent::set($data_type,$data_name,$id,$meta_name,$vals,$conf);
+	}
+	public static function add($data_type,$data_name,$id,$meta_name,$vals,$conf){
+		self::protectMediaIfShould($data_type,$data_name,$id,$meta_name,$vals,$conf);
+		parent::add($data_type,$data_name,$id,$meta_name,$vals,$conf);
+	}
+	public static function protectMediaIfShould($data_type,$data_name,$id,$meta_name,$vals,$conf){
+		if(!empty($conf['protect']) && $data_type==='post' && get_post_status($id)==='future'){
+			$time=get_post_timestamp($id);
+			foreach((array)$vals as $val){
+				media_protect::set_cond($val,['time'=>['from'=>$time]]);
+			}
+		}
+	}
 	public static function output($meta,$prm){
 		$val=$meta->value;
 		$rtn=array();
