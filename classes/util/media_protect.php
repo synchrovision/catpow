51c93974e5053fe3c74bb4beb7161a66fb1fbd58
synchrovision <trinitybluster@yahoo.co.jp>
2022-10-17 17:34:47 +0900

    refactor :recycle: use const to define dir name

--- classes/util/media_protect.php
+++ classes/util/media_protect.php
@@ -5,12 +5,14 @@
 */
 namespace Catpow\util;
 class media_protect{
+	const DIR_NAME='protected';
 	public static function protect($id){
 		$metadata=get_post_meta($id,'_wp_attachment_metadata',true);
 		if(!empty($metadata['protected'])){return;}
+		$dirname=self::DIR_NAME;
 		
 		$metadata['protected']=true;
-		$metadata['file']="catpow/p{$id}/".$metadata['file'];
+		$metadata['file']="{$dirname}/p{$id}/".($metadata['file']??get_post_meta($id,'_wp_attached_file',true));
 		
 		$basedir=wp_get_upload_dir()['basedir'];
 		$dir=$basedir.'/'.dirname($metadata['file']);
@@ -19,13 +21,13 @@ class media_protect{
 		$files=media::get_all_attachment_files($id);
 		
 		if(!is_dir($dir)){mkdir($dir,0777,true);}
-		if(!file_exists($htaccess_file=$basedir.'/catpow/.htaccess')){
+		if(!file_exists($htaccess_file="{$basedir}/{$dirname}/.htaccess")){
 			$subdir=substr($basedir,strpos($basedir,'/uploads')+9);
 			if(!empty($subdir)){$subdir.='/';}
 			file_put_contents(
 				$htaccess_file,
 				"RewriteEngine on\n".
-				"RewriteRule (.+) ".str_repeat('../',$depth+1)."plugins/catpow/callee/protected_media.php?path={$subdir}catpow/$1 [L,QSA]"
+				"RewriteRule (.+) ".str_repeat('../',$depth+1)."plugins/catpow/callee/protected_media.php?path={$subdir}{$dirname}/$1 [L,QSA]"
 			);
 		}
 		foreach($files as $file){
@@ -37,9 +39,10 @@ class media_protect{
 	public static function unprotect($id){
 		$metadata=get_post_meta($id,'_wp_attachment_metadata',true);
 		if(empty($metadata['protected'])){return;}
+		$dirname=self::DIR_NAME;
 		
 		unset($metadata['protected']);
-		$metadata['file']=preg_replace('|^catpow/p\d+/|','',$metadata['file']);
+		$metadata['file']=preg_replace("|^{$dirname}/p\d+/|",'',$metadata['file']);
 		
 		$basedir=wp_get_upload_dir()['basedir'];
 		$dir=$basedir.'/'.dirname($metadata['file']);
@@ -49,7 +52,7 @@ class media_protect{
 		foreach($files as $file){
 			if(file_exists($file)){rename($file,$dir.'/'.basename($file));}
 		}
-		dir::delete($basedir."/catpow/p{$id}");
+		dir::delete($basedir."/{$dirname}/p{$id}");
 		update_post_meta($id,'_wp_attachment_metadata',$metadata);
 		update_post_meta($id,'_wp_attached_file',$metadata['file']);
 	}
