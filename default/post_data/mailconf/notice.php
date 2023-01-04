f28cbad569b2195d3e0161d51d97f3c268faf2f6
synchrovision <trinitybluster@yahoo.co.jp>
2023-01-04 10:44:10 +0900

    feat :+1: use get_block_code

--- default/post_data/mailconf/notice.php
+++ default/post_data/mailconf/notice.php
@@ -3,23 +3,24 @@ $post_data['post_title']='【お問い合わせ】[output name]様より';
 $post_data['meta']=[
 	'to'=>get_option('admin_email'),
 	'from'=>get_option('admin_email'),
+	'type'=>'html',
 ];
-ob_start();
-?>
-<!-- wp:catpow/t-body -->
-<textmail>
-<?php foreach($conf_data['inputs'] as $name=>$conf):?>
-<?=$conf['label']?>：[output <?=$name?>]
-<?php endforeach; ?>
-</textmail><table width="100%" align="center" class="wp-block-catpow-t-body hasHeader hasFooter"><thead class="wp-block-catpow-t-body__header"><tr><th><img class="wp-image" src="<?=get_theme_file_uri('images/logo.svg')?>" alt="<?=bloginfo('title')?>" style="width: 250px;"/></th></tr></thead><tbody class="wp-block-catpow-t-body__body"><tr><td><div class="wp-block-catpow-t-body__body__contents"><!-- wp:catpow/t-box -->
-<table class="wp-block-catpow-t-box medium"><tbody><tr><td>
-<?php foreach($conf_data['inputs'] as $name=>$conf):?>
-<!-- wp:catpow/t-paragraph -->
-<table width="100%" class="wp-block-catpow-t-paragraph medium left"><tbody><tr><td><?=$conf['label']?>：[output <?=$name?>]</td></tr></tbody></table>
-<!-- /wp:catpow/t-paragraph -->
-<?php endforeach; ?>
-<</td></tr></tbody></table>
-<!-- /wp:catpow/t-box --></div></td></tr></tbody><tfoot class="wp-block-catpow-t-body__footer"><tr><td><?php bloginfo('name'); ?><br/><?=home_url()?></td></tr></tfoot></table>
-<!-- /wp:catpow/t-body -->
-<?php
-$post_data['post_content']=ob_get_clean(); ?>
+$post_data['post_content']=cp::get_block_code('t-body',[
+	'headerText'=>sprintf(
+		'<img class="wp-image" src="%s" alt="%s" style="width: 250px;"/>',
+		wp_get_attachment_image_src(get_theme_mod('custom_logo'),'full')[0]??'',get_bloginfo('title')
+	),
+	'footerText'=>get_bloginfo('name').'<br/>'.home_url(),
+	'textMail'=>implode("\n",array_map(function($conf,$name){
+		return "{$conf['label']}：[output {$name}]";
+	},$conf_data['inputs'],array_keys($conf_data['inputs'])))
+],[
+	cp::get_block_code('t-box',[],[
+		implode("\n",array_map(function($conf,$name){
+			return cp::get_block_code('t-paragraph',[
+				'classes'=>'medium left',
+				'text'=>"{$conf['label']}：[output {$name}]"
+			]);
+		},$conf_data['inputs'],array_keys($conf_data['inputs'])))
+	])
+]);
