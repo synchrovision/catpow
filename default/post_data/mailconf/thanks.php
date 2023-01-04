056d58c4ccbd9fe0700a124a1593db8e0636f7ec
synchrovision <trinitybluster@yahoo.co.jp>
2023-01-04 10:43:36 +0900

    feat :+1: use custom logo for mail

--- default/post_data/mailconf/thanks.php
+++ default/post_data/mailconf/thanks.php
@@ -6,7 +6,10 @@ $post_data['meta']=[
 	'type'=>'html',
 ];
 $post_data['post_content']=cp::get_block_code('t-body',[
-	'headerText'=>sprintf('<img class="wp-image" src="%s" alt="%s" style="width: 250px;"/>',get_theme_file_uri('images/logo.svg'),get_bloginfo('title')),
+	'headerText'=>sprintf(
+		'<img class="wp-image" src="%s" alt="%s" style="width: 250px;"/>',
+		wp_get_attachment_image_src(get_theme_mod('custom_logo'),'full')[0]??'',get_bloginfo('title')
+	),
 	'footerText'=>get_bloginfo('name').'<br/>'.home_url(),
 	'textMail'=>implode("\n",array_map(function($conf,$name){
 		return "{$conf['label']}：[output {$name}]";
