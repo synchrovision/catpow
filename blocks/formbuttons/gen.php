ff527c7b1a8cd68bd62225f60cb42cb19d9e9379
synchrovision <trinitybluster@yahoo.co.jp>
2023-01-04 12:58:52 +0900

    chore :up: update default post data

--- blocks/formbuttons/gen.php
+++ blocks/formbuttons/gen.php
@@ -2,7 +2,7 @@
 <ul class="wp-block-catpow-formbuttons buttons <?=$attr['classes']??''?>">
 <?php foreach((array)$attr['items'] as $item): ?>
 	<li class="item <?=$item['classes']??''?>">
-		<div class="button" data-action="<?=$item['action']??''?>" data-callback="<?=$item['callback']??''?>" data-target="<?=$item['target']??''?>" ignore-message="<?=$item['ignoreMessage']??''?>" data-event="<?=$item['event']??''?>">
+		<div class="button" data-action="<?=$item['action']??''?>" data-callback="<?=$item['callback']??''?>" data-target="<?=$item['target']??''?>" ignore-message="<?=$item['ignoreMessage']??''?>"<?=empty($item['event'])?'':sprintf(' data-event="%s"',$item['event'])?>>
 			<?php if(preg_match('/\bhasIcon\b/',$item['classes']??'')):?>
 				<span class="icon">
 					<img src="<?=$item['iconSrc']??''?>" alt="<?=$item['iconAlt']??''?>"/>
