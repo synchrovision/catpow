9c6ea050f2f923d814f07961ea89ed27b9054d6a
synchrovision <trinitybluster@yahoo.co.jp>
2023-01-06 19:18:35 +0900

    feat :+1: add hsbToHex function

--- js/catpow_util.js
+++ js/catpow_util.js
@@ -190,6 +190,17 @@ Catpow.util={
 		};
 		return '#'+f(0)+f(8)+f(4);
 	},
+	hsbToHex:(hsb)=>{
+		const {h,s,b}=hsb;
+		const hs=h/60;
+		const d=b*s/10000;
+		const min=(b/100)-d;
+		const f=function(n){
+			var c=min+d*Math.min(1,Math.max(0,Math.abs((hs+n)%6-3)-1));
+			return Math.round(255*c).toString(16).padStart(2,'0');
+		};
+		return '#'+f(0)+f(4)+f(2);
+	},
 	hexToHsl:function(hex){
 		var h=0,s=0;
 		var rgb=hex.match(/#?(\w{2})(\w{2})(\w{2})/).slice(1).map(function(c){return parseInt(c,16)/0xff;});
