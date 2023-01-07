09c59b114d6e79f9d2575e6d3800393903f09990
synchrovision <trinitybluster@yahoo.co.jp>
2023-01-08 02:48:55 +0900

    feat :+1: update cpform.nonce

--- js/cpform.nonce.js
+++ js/cpform.nonce.js
@@ -1,17 +1,20 @@
-/* global wp console cp_rest_nonces*/
+/* global wp console*/
+window.Catpow.nonces=window.Catpow.nonces || {};
+
 wp.apiFetch.use(function(options,next){
-	if(cp_rest_nonces && options.path && options.path.substr(0,7)==='/cp/v1/'){
+	const nonces=window.Catpow.nonces;
+	if(options.path && options.path.substr(0,7)==='/cp/v1/'){
 		const chunks=options.path.substr(7).split('/');
 		const class_path=chunks.slice(0,2).join('/');
-		if(cp_rest_nonces[class_path]){
+		if(nonces[class_path]){
 			if(!options.headers){options.headers={};}
-			options.headers['X-CP-Nonce']=cp_rest_nonces[class_path];
+			options.headers['X-CP-Nonce']=nonces[class_path];
 			return next(options);
 		}
 		const content_path=chunks.slice(0,3).join('/');
-		if(cp_rest_nonces[content_path]){
+		if(nonces[content_path]){
 			if(!options.headers){options.headers={};}
-			options.headers['X-CP-Nonce']=cp_rest_nonces[content_path];
+			options.headers['X-CP-Nonce']=nonces[content_path];
 			return next(options);
 		}
 	}
