68a9b3bf09b52ae85caa5b68b54ba16ea3b47f75
synchrovision <trinitybluster@yahoo.co.jp>
2023-01-19 20:37:40 +0900

    feat :+1: update SelectThemeColor component

--- blocks/_init/init/SelectThemeColor.jsx
+++ blocks/_init/init/SelectThemeColor.jsx
@@ -2,21 +2,35 @@
 
 CP.SelectThemeColor=(props)=>{
 	const {selected,onChange}=props;
+	const {useCallback,useMemo}=wp.element;
 	const {bem}=Catpow.util;
-	const classes=bem('fillColor');
-
-	var items=Array.from(Array(13),(v,i)=>{
-		const value='color'+i;
+	const classes=bem('CP-SelectThemeColor');
+	
+	const data=useMemo(()=>CP.parseColorClass(selected),[selected]);
+	
+	const Selections=useCallback((props)=>{
+		const {absolute=false,relative=false,selected}=props;
 		return (
-			<li
-				className={classes(value,{active:value==selected})}
-				onClick={()=>onChange(value)}
-				key={value}
-			> </li>
+			<ul className={classes.items({absolute})}>
+			{Array.from(Array(13),(v,value)=>{
+				const colorClass=CP.generateColorClass({absolute,relative,value})
+				return (
+					<li
+						className={classes.items.item(colorClass,{active:colorClass==selected,absolute,relative})}
+						onClick={()=>onChange(colorClass)}
+						key={colorClass}
+					> </li>
+				);
+			})}
+			</ul>
 		);
-	});
+	},[]);
 
 	return (
-		<ul className="selectColor">{items}</ul>
+		<div className={classes()}>
+			<Selections selected={selected} absolute={true}/>
+			<Selections selected={selected}/>
+			<Selections selected={selected} relative={true}/>
+		</div>
 	);
 };
