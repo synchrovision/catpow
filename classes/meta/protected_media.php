31368a411df69a0391964e000ad66bb5edefdac0
synchrovision <16245284+synchrovision@users.noreply.github.com>
2023-02-05 18:50:57 +0900

    feat :+1: add should_allow method

--- classes/meta/protected_media.php
+++ classes/meta/protected_media.php
@@ -12,18 +12,17 @@ class protected_media extends media{
 		parent::add($data_type,$data_name,$id,$meta_name,$vals,$conf);
 	}
 	public static function output($meta,$prm){
-		do{
-			if(!empty($meta->conf['capability']) && !current_user_can($meta->conf['capability'])){break;}
-			media_protect::allow($meta->value);
-		}while(false);
+		if(self::should_allow($meta,$prm)){media_protect::allow($meta->value);}
 		return parent::output($meta,$prm);
 	}
 	public static function input($meta,$prm){
-		do{
-			if(!empty($meta->conf['capability']) && !current_user_can($meta->conf['capability'])){break;}
-			media_protect::allow($meta->value);
-		}while(false);
+		if(self::should_allow($meta,$prm)){media_protect::allow($meta->value);}
 		return parent::input($meta,$prm);
+	}
+	public static function should_allow($meta,$prm){
+		if(is_user_logged_in() && get_post($meta->value)->post_author == get_current_user_id()){return true;}
+		if(!empty($meta->conf['capability']) && !current_user_can($meta->conf['capability'])){return false;}
+		return true;
 	}
 }
 
