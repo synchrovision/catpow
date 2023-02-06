eb6df1e89666b9cd1f91e0e1d4ccb415a749b11b
synchrovision <16245284+synchrovision@users.noreply.github.com>
2023-02-06 23:15:56 +0900

    feat :+1: update SelectColor UI

--- ui/SelectColor/input.js
+++ ui/SelectColor/input.js
@@ -1,20 +1,42 @@
 (() => {
-  // ../ui/SelectColor/input.jsx
+  // ui/SelectColor/input.jsx
   Catpow.UI.SelectColor = (props) => {
-    const { useState, useEffect } = wp.element;
+    const { useMemo, useState, useCallback } = wp.element;
     const { name } = props;
     const [value, setValue] = useState(props.value);
-    const color = value.substr(5) || 0;
-    var items = Array.from(Array(13), (v, i) => {
-      var classes = "fillColor" + i;
-      if (color == i) {
-        classes += " active";
+    const { bem } = Catpow.util;
+    const classes = bem("SelectColor");
+    const parseColorClass = useCallback((colorClass) => {
+      if (colorClass) {
+        const matches = colorClass.match(/^color((|_|\-\-)(\-?\d+))$/);
+        if (matches) {
+          return {
+            fixed: matches[2] === "--",
+            absolute: matches[2] === "",
+            relative: matches[2] === "_",
+            value: matches[3]
+          };
+        }
       }
-      return /* @__PURE__ */ React.createElement("li", { className: classes, onClick: () => {
-        setValue("color" + i);
-      } }, " ");
+      return { fixed: false, absolute: false, relative: false, value: 0 };
     });
-    ;
-    return /* @__PURE__ */ React.createElement("div", { className: "SelectColor" }, /* @__PURE__ */ React.createElement("ul", null, items), /* @__PURE__ */ React.createElement(Catpow.UI.HiddenValues, { name, value }));
+    const generateColorClass = useCallback((data2) => "color" + (data2.fixed ? "--" : data2.relative ? "_" : "") + data2.value, []);
+    const data = useMemo(() => parseColorClass(value), [value]);
+    const Selections = useCallback((props2) => {
+      const { fixed = false, absolute = false, relative = false, active = false, selected } = props2;
+      return /* @__PURE__ */ wp.element.createElement("ul", { className: classes.items({ fixed, absolute, relative, active }) }, Array.from(Array(13), (v, value2) => {
+        const colorClass = generateColorClass({ fixed, absolute, relative, value: value2 });
+        return /* @__PURE__ */ wp.element.createElement(
+          "li",
+          {
+            className: classes.items.item(colorClass, { active: colorClass == selected, fixed, absolute, relative }),
+            onClick: () => setValue(colorClass),
+            key: colorClass
+          },
+          " "
+        );
+      }));
+    }, []);
+    return /* @__PURE__ */ wp.element.createElement("div", { className: classes() }, /* @__PURE__ */ wp.element.createElement(Selections, { selected: value, fixed: true, active: data.fixed }), /* @__PURE__ */ wp.element.createElement(Selections, { selected: value, absolute: true, active: data.absolute }), /* @__PURE__ */ wp.element.createElement(Selections, { selected: value, relative: true, active: data.relative }), /* @__PURE__ */ wp.element.createElement(Catpow.UI.HiddenValues, { name, value }));
   };
 })();
--- ui/SelectColor/input.jsx
+++ ui/SelectColor/input.jsx
@@ -1,20 +1,50 @@
 ﻿Catpow.UI.SelectColor=(props)=>{
-	const {useState,useEffect}=wp.element;
+	const {useMemo,useState,useCallback}=wp.element;
 	const {name}=props;
 	const [value,setValue]=useState(props.value);
+	const {bem}=Catpow.util;
+	const classes=bem('SelectColor');
 	
-	const color=value.substr(5) || 0;
-	var items=Array.from(Array(13),(v,i)=>{
-		var classes='fillColor'+i;
-		if(color==i){classes+=' active';}
+	const parseColorClass=useCallback((colorClass)=>{
+		if(colorClass){
+			const matches=colorClass.match(/^color((|_|\-\-)(\-?\d+))$/);
+			if(matches){
+				return {
+					fixed:matches[2]==='--',
+					absolute:matches[2]==='',
+					relative:matches[2]==='_',
+					value:matches[3]
+				};
+			}
+		}
+		return {fixed:false,absolute:false,relative:false,value:0};
+	});
+	const generateColorClass=useCallback((data)=>'color'+(data.fixed?'--':(data.relative?'_':''))+data.value,[]);
+	const data=useMemo(()=>parseColorClass(value),[value]);
+	
+	const Selections=useCallback((props)=>{
+		const {fixed=false,absolute=false,relative=false,active=false,selected}=props;
 		return (
-			<li className={classes} onClick={()=>{setValue('color'+i);}}> </li>
+			<ul className={classes.items({fixed,absolute,relative,active})}>
+				{Array.from(Array(13),(v,value)=>{
+					const colorClass=generateColorClass({fixed,absolute,relative,value})
+					return (
+						<li
+							className={classes.items.item(colorClass,{active:colorClass==selected,fixed,absolute,relative})}
+							onClick={()=>setValue(colorClass)}
+							key={colorClass}
+						> </li>
+					);
+				})}
+			</ul>
 		);
-	});;
+	},[]);
 	
 	return (
-		<div className="SelectColor">
-			<ul>{items}</ul>
+		<div className={classes()}>
+			<Selections selected={value} fixed={true} active={data.fixed}/>
+			<Selections selected={value} absolute={true} active={data.absolute}/>
+			<Selections selected={value} relative={true} active={data.relative}/>
 			<Catpow.UI.HiddenValues name={name} value={value}/>
 		</div>
 	);
