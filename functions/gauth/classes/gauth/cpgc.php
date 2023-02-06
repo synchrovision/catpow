dd1ba2a4abc2ea8d7855dbe65871ba3ee71f0928
synchrovision <16245284+synchrovision@users.noreply.github.com>
2023-02-06 12:04:35 +0900

    feat :+1: login as user who has same email address if exists

--- functions/gauth/classes/gauth/cpgc.php
+++ functions/gauth/classes/gauth/cpgc.php
@@ -77,13 +77,18 @@ class cpgc{
 				$uid=get_current_user_id();
 			}
 			else{
-				$uid=wp_insert_user([
-					'user_pass'=>wp_generate_password(),
-					'user_login'=>$user_data['name'],
-					'user_email'=>$user_data['email'],
-					'first_name'=>$user_data['given_name'],
-					'last_name'=>$user_data['family_name']
-				]);
+				if($user=get_user_by('email',$user_data['email'])){
+					$uid=$user->ID;
+				}
+				else{
+					$uid=wp_insert_user([
+						'user_pass'=>wp_generate_password(),
+						'user_login'=>$user_data['name'],
+						'user_email'=>$user_data['email'],
+						'first_name'=>$user_data['given_name'],
+						'last_name'=>$user_data['family_name']
+					]);
+				}
 				wp_set_auth_cookie($uid);
 			}
 			add_user_meta($uid,'_google_id',$gid,true);
