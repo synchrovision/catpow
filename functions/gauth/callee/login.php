bf6086110cac2e4325c7bd357302c9d0549fa1b4
synchrovision <16245284+synchrovision@users.noreply.github.com>
2023-02-06 11:59:47 +0900

    fix :bug: prevent undefined error

--- functions/gauth/callee/login.php
+++ functions/gauth/callee/login.php
@@ -1,6 +1,8 @@
 <?php
 use Catpow\gauth\cpgc;
-cpgc::authenticate($_GET['code']);
+if(!empty($_GET['code'])){
+	cpgc::authenticate($_GET['code']);
+}
 cpgc::login();
 wp_redirect(home_url());
 exit;
