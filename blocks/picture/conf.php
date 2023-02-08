cfa921f94c01b9a5c69ed66b90ae9fe0960f7741
synchrovision <16245284+synchrovision@users.noreply.github.com>
2023-02-08 15:48:51 +0900

    feat :+1: update picture block

--- blocks/picture/conf.php
+++ blocks/picture/conf.php
@@ -3,7 +3,7 @@ use Catpow\util\BlockConfig;
 $attributes=[
 	"classes"=>["source"=>'attribute',"selector"=>'div',"attribute"=>'class',"default"=>'wp-block-catpow-picture'],
 
-	"sources"=>BlockConfig::getPictureSoucesAttributesForDevices(['sp','tb'],'dummy_bg.jpg'),
+	"sources"=>BlockConfig::getPictureSoucesAttributesForDevices(['sp','tb'],'picture','dummy.jpg'),
 	"mime"=>["source"=>'attribute',"selector"=>'[src]',"attribute"=>'data-mime'],
 	"src"=>["source"=>'attribute',"selector"=>'[src]',"attribute"=>'src',"default"=>cp::get_file_url('images/dummy.jpg')],
 	"alt"=>["source"=>'attribute',"selector"=>'[src]',"attribute"=>'alt'],
