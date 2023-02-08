f2ec137f59f08ec99c2883881385d26cf5849da4
synchrovision <16245284+synchrovision@users.noreply.github.com>
2023-02-08 12:37:34 +0900

    feat :+1: export current theme for sites

--- classes/util/site_config.php
+++ classes/util/site_config.php
@@ -9,8 +9,20 @@ class site_config{
 		$json=WP_CONTENT_DIR.'/config/sites.json';
 		if(!is_dir(dirname($json))){mkdir(dirname($json,0755,true));}
 		$sites=[];
-		foreach(get_sites() as $site){
-			$sites[$site->domain.$site->path]=$site->id;
+		if(is_multisite()){
+			foreach(get_sites() as $site){
+				switch_to_blog($site->id);
+				$sites[$site->domain.$site->path]=[
+					'id'=>$site->id,
+					'theme'=>get_stylesheet()
+				];
+				restore_current_blog();
+			}
+		}
+		else{
+			$sites[get_site_url()]=[
+				'theme'=>get_stylesheet()
+			];
 		}
 		file_put_contents($json,json_encode($sites,0700));
 	}
