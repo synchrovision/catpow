7b186c4eea8a082ec731079abd9ab9d5ded91a27
synchrovision <16245284+synchrovision@users.noreply.github.com>
2023-02-21 12:07:06 +0900

    fix :bug: apply key for list items, fix table structure

--- components/Finder/FilterControl/component.js
+++ components/Finder/FilterControl/component.js
@@ -6,22 +6,23 @@
     const { state, dispatch } = useContext(Catpow.FinderContext);
     const [open, setOpen] = useState(false);
     const { cols } = state.index;
-    return /* @__PURE__ */ React.createElement("div", { className: "FinderControl FinderFilterControl" }, /* @__PURE__ */ React.createElement("ul", { className: "items" }, /* @__PURE__ */ React.createElement("li", { className: "item" + (Object.keys(state.query).length ? " active" : "") }, /* @__PURE__ */ React.createElement("div", { className: "icon dashicons dashicons-filter", onClick: () => setOpen(!open) }), /* @__PURE__ */ React.createElement(Catpow.Popover, { open }, /* @__PURE__ */ React.createElement("table", { className: "FinderFilterControl__table" }, Object.keys(state.index.cols).map((key) => {
+    return /* @__PURE__ */ wp.element.createElement("div", { className: "FinderControl FinderFilterControl" }, /* @__PURE__ */ wp.element.createElement("ul", { className: "items" }, /* @__PURE__ */ wp.element.createElement("li", { className: "item" + (Object.keys(state.query).length ? " active" : "") }, /* @__PURE__ */ wp.element.createElement("div", { className: "icon dashicons dashicons-filter", onClick: () => setOpen(!open) }), /* @__PURE__ */ wp.element.createElement(Catpow.Popover, { open }, /* @__PURE__ */ wp.element.createElement("table", { className: "FinderFilterControl__table" }, /* @__PURE__ */ wp.element.createElement("tbody", { className: "FinderFilterControl__table-tbody" }, Object.keys(state.index.cols).map((key) => {
       const col = state.index.cols[key];
       if (col.role === "group") {
-        return /* @__PURE__ */ React.createElement("tr", { className: "row" }, /* @__PURE__ */ React.createElement("th", { className: "label" }, col.label), /* @__PURE__ */ React.createElement("td", { className: "inputs" }, Object.keys(col.dict).map((val) => {
+        return /* @__PURE__ */ wp.element.createElement("tr", { className: "row", key }, /* @__PURE__ */ wp.element.createElement("th", { className: "label" }, col.label), /* @__PURE__ */ wp.element.createElement("td", { className: "inputs" }, Object.keys(col.dict).map((val) => {
           const isActive = state.query[key] && state.query[key].indexOf(val) !== -1;
-          return /* @__PURE__ */ React.createElement(
+          return /* @__PURE__ */ wp.element.createElement(
             Catpow.CheckBox,
             {
               label: col.dict[val],
               selected: isActive,
-              onChange: (selected) => dispatch({ type: (selected ? "add" : "remove") + "Query", key, val })
+              onChange: (selected) => dispatch({ type: (selected ? "add" : "remove") + "Query", key, val }),
+              key: val
             }
           );
         })));
       }
       return false;
-    }))))));
+    })))))));
   };
 })();
--- components/Finder/FilterControl/component.jsx
+++ components/Finder/FilterControl/component.jsx
@@ -16,29 +16,32 @@ Catpow.Finder.FilterControl=(props)=>{
 					<div className={"icon dashicons dashicons-filter"} onClick={()=>setOpen(!open)}></div>
 					<Catpow.Popover　open={open}>
 						<table className="FinderFilterControl__table">
-						{Object.keys(state.index.cols).map((key)=>{
-							const col=state.index.cols[key];
-							if(col.role==='group'){
-								return (
-									<tr className="row">
-										<th className="label">{col.label}</th>
-										<td className="inputs">
-											{Object.keys(col.dict).map((val)=>{
-												const isActive=state.query[key] && state.query[key].indexOf(val)!==-1;
-												return (
-													<Catpow.CheckBox
-														label={col.dict[val]}
-														selected={isActive}
-														onChange={(selected)=>dispatch({type:(selected?'add':'remove')+'Query',key,val})}
-													/>
-												);
-											})}
-										</td>
-									</tr>
-								);
-							}
-							return false;
-						})}
+							<tbody className="FinderFilterControl__table-tbody">
+							{Object.keys(state.index.cols).map((key)=>{
+								const col=state.index.cols[key];
+								if(col.role==='group'){
+									return (
+										<tr className="row" key={key}>
+											<th className="label">{col.label}</th>
+											<td className="inputs">
+												{Object.keys(col.dict).map((val)=>{
+													const isActive=state.query[key] && state.query[key].indexOf(val)!==-1;
+													return (
+														<Catpow.CheckBox
+															label={col.dict[val]}
+															selected={isActive}
+															onChange={(selected)=>dispatch({type:(selected?'add':'remove')+'Query',key,val})}
+															key={val}
+														/>
+													);
+												})}
+											</td>
+										</tr>
+									);
+								}
+								return false;
+							})}
+							</tbody>
 						</table>
 					</Catpow.Popover>
 				</li>
