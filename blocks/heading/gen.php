c6920e54f418d7b8e67d1d5a474c14b647069193
synchrovision <16245284+synchrovision@users.noreply.github.com>
2023-03-09 17:23:38 +0900

    feat :+1: update gen.php for heading block

--- blocks/heading/gen.php
+++ blocks/heading/gen.php
@@ -1,3 +1,3 @@
-<!-- wp:heading -->
+<!-- wp:heading {"level":<?=$attr['level']??'3'?>} -->
 <h<?=$attr['level']??'3'?> class="<?=$attr['className']??'is-style-headline'?>"><?=$attr['content']?></h<?=$attr['level']??'3'?>>
 <!-- /wp:heading -->
