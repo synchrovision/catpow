f6e0f4c7ce2d6dad17cae3464662d3ef5ea7ba32
synchrovision <16245284+synchrovision@users.noreply.github.com>
2023-03-16 18:18:38 +0900

    feat :+1: add SearchSelect component

---
+++ components/SearchSelect/component.js
@@ -0,0 +1,82 @@
+(() => {
+  // ../components/SearchSelect/component.jsx
+  Catpow.SearchSelect = (props) => {
+    const { defaultLabel, onChange, col = 5, placeholder = "Search" } = props;
+    const { useState, useReducer, useCallback, useMemo, useRef, useEffect } = wp.element;
+    const { bem } = Catpow.util;
+    const classes = bem("SearchSelect");
+    const [open, setOpen] = useState(false);
+    const [search, setSearch] = useState("");
+    const cache = useRef({});
+    const { labelValueMap, valueLabelMap } = useMemo(() => {
+      const labelValueMap2 = {};
+      const valueLabelMap2 = {};
+      const cb = (options) => {
+        if (Array.isArray(options)) {
+          options.forEach((val) => {
+            if (typeof val === "object") {
+              cb(val);
+            } else {
+              labelValueMap2[val] = val;
+              valueLabelMap2[val] = val;
+            }
+          });
+        } else {
+          Object.keys(options).forEach((key) => {
+            if (typeof options[key] === "object") {
+              cb(options[key]);
+            } else {
+              labelValueMap2[key] = options[key];
+              valueLabelMap2[options[key]] = key;
+            }
+          });
+        }
+      };
+      cb(props.options);
+      return { labelValueMap: labelValueMap2, valueLabelMap: valueLabelMap2 };
+    }, [props.options]);
+    const toggleLabelReducer = useCallback((selectedLabels2, labelToToggle) => {
+      if (!selectedLabels2) {
+        return [labelToToggle];
+      }
+      if (selectedLabels2.includes(labelToToggle)) {
+        return selectedLabels2.filter((val) => val !== labelToToggle);
+      }
+      return [...selectedLabels2, labelToToggle];
+    }, []);
+    const [selectedLabels, toggleLabel] = useReducer(
+      toggleLabelReducer,
+      useMemo(() => props.value ? props.value.map((value) => valueLabelMap[value]) : [], [])
+    );
+    useEffect(() => {
+      onChange(selectedLabels.map((label) => labelValueMap[label]));
+    }, [selectedLabels]);
+    const getLabelsForSearch = useCallback((search2) => {
+      if (cache.current[search2]) {
+        return cache.current[search2];
+      }
+      const labels = search2.length > 2 ? getLabelsForSearch(search2.slice(0, -1)) : Object.keys(labelValueMap);
+      return cache.current[search2] = search2 ? labels.filter((label) => label.indexOf(search2) >= 0) : labels;
+    }, [cache, labelValueMap]);
+    const currentLabels = useMemo(() => getLabelsForSearch(search), [getLabelsForSearch, search]);
+    return /* @__PURE__ */ wp.element.createElement("div", { className: classes() }, /* @__PURE__ */ wp.element.createElement("div", { className: classes.selected(), onClick: () => setOpen(true) }, /* @__PURE__ */ wp.element.createElement("ul", { className: classes.selected.items() }, selectedLabels && selectedLabels.length ? selectedLabels.map((label) => /* @__PURE__ */ wp.element.createElement("li", { className: classes.selected.items.item(), key: label }, label)) : /* @__PURE__ */ wp.element.createElement("li", { className: classes.selected.items.item() }, defaultLabel))), /* @__PURE__ */ wp.element.createElement(Catpow.Popup, { open, onClose: () => open && setOpen(false) }, /* @__PURE__ */ wp.element.createElement("div", { className: classes.search() }, /* @__PURE__ */ wp.element.createElement(
+      "input",
+      {
+        type: "text",
+        className: classes.search.input(),
+        value: search,
+        placeholder,
+        onChange: (e) => setSearch(e.currentTarget.value)
+      }
+    )), /* @__PURE__ */ wp.element.createElement("div", { className: classes.selected() }, /* @__PURE__ */ wp.element.createElement("ul", { className: classes.selected.items() }, selectedLabels && selectedLabels.length > 0 && selectedLabels.map((label) => /* @__PURE__ */ wp.element.createElement("li", { className: classes.selected.items.item(), key: label }, label, /* @__PURE__ */ wp.element.createElement("span", { className: classes.selected.items.item.button(), onClick: () => toggleLabel(label) }))))), /* @__PURE__ */ wp.element.createElement("div", { className: classes.selections() }, /* @__PURE__ */ wp.element.createElement(
+      Catpow.SelectTable,
+      {
+        selections: currentLabels,
+        value: selectedLabels,
+        multiple: true,
+        col,
+        onChange: toggleLabel
+      }
+    ))));
+  };
+})();
---
+++ components/SearchSelect/component.jsx
@@ -0,0 +1,106 @@
+﻿Catpow.SearchSelect=(props)=>{
+	const {defaultLabel,onChange,col=5,placeholder='Search'}=props;
+	const {useState,useReducer,useCallback,useMemo,useRef,useEffect}=wp.element;
+	const {bem}=Catpow.util;
+	const classes=bem('SearchSelect');
+	
+	const [open,setOpen]=useState(false);
+	const [search,setSearch]=useState('');
+	const cache=useRef({});
+	
+	const {labelValueMap,valueLabelMap}=useMemo(()=>{
+		const labelValueMap={};
+		const valueLabelMap={};
+		const cb=(options)=>{
+			if(Array.isArray(options)){
+				options.forEach((val)=>{
+					if(typeof val === 'object'){
+						cb(val);
+					}
+					else{
+						labelValueMap[val]=val;
+						valueLabelMap[val]=val;
+					}
+				});
+			}
+			else{
+				Object.keys(options).forEach((key)=>{
+					if(typeof options[key] === 'object'){
+						cb(options[key]);
+					}
+					else{
+						labelValueMap[key]=options[key];
+						valueLabelMap[options[key]]=key;
+					}
+				});
+			}
+		};
+		cb(props.options);
+		return {labelValueMap,valueLabelMap};
+	},[props.options]);
+	
+	const toggleLabelReducer=useCallback((selectedLabels,labelToToggle)=>{
+		if(!selectedLabels){return [labelToToggle];}
+		if(selectedLabels.includes(labelToToggle)){return selectedLabels.filter((val)=>val!==labelToToggle);}
+		return [...selectedLabels,labelToToggle];
+	},[]);
+	const [selectedLabels,toggleLabel]=useReducer(
+		toggleLabelReducer,
+		useMemo(()=>props.value?props.value.map((value)=>valueLabelMap[value]):[],[])
+	);
+	useEffect(()=>{
+		onChange(selectedLabels.map((label)=>labelValueMap[label]));
+	},[selectedLabels]);
+	const getLabelsForSearch=useCallback((search)=>{
+		if(cache.current[search]){return cache.current[search];}
+		const labels=(search.length>2)?getLabelsForSearch(search.slice(0,-1)):Object.keys(labelValueMap);
+		return cache.current[search]=search?labels.filter((label)=>label.indexOf(search)>=0):labels;
+	},[cache,labelValueMap]);
+	const currentLabels=useMemo(()=>getLabelsForSearch(search),[getLabelsForSearch,search]);
+	
+	return (
+		<div className={classes()}>
+			<div className={classes.selected()} onClick={()=>setOpen(true)}>
+				<ul className={classes.selected.items()}>
+					{(selectedLabels && selectedLabels.length)?(
+						selectedLabels.map((label)=><li className={classes.selected.items.item()} key={label}>{label}</li>)
+					):(
+						<li className={classes.selected.items.item()}>{defaultLabel}</li>
+					)}
+				</ul>
+			</div>
+			<Catpow.Popup open={open} onClose={()=>open && setOpen(false)}>
+				<div className={classes.search()}>
+					<input
+						type="text"
+						className={classes.search.input()}
+						value={search}
+						placeholder={placeholder}
+						onChange={(e)=>setSearch(e.currentTarget.value)}
+					/>
+				</div>
+				<div className={classes.selected()}>
+					<ul className={classes.selected.items()}>
+						{selectedLabels && selectedLabels.length>0 && (
+							selectedLabels.map((label)=>(
+								<li className={classes.selected.items.item()} key={label}>
+									{label}
+									<span className={classes.selected.items.item.button()} onClick={()=>toggleLabel(label)}></span>
+								</li>
+							))
+						)}
+					</ul>
+				</div>
+				<div className={classes.selections()}>
+					<Catpow.SelectTable
+						selections={currentLabels}
+						value={selectedLabels}
+						multiple={true}
+						col={col}
+						onChange={toggleLabel}
+					/>
+				</div>
+			</Catpow.Popup>
+		</div>
+	);
+}
