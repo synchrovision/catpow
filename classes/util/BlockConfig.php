4315ecb743ba1aa13b648d3f2e182cf4529cb976
synchrovision <16245284+synchrovision@users.noreply.github.com>
2023-03-17 22:10:06 +0900

    feat :+1: add PlacedPictures attributes generator method

--- classes/util/BlockConfig.php
+++ classes/util/BlockConfig.php
@@ -1,13 +1,15 @@
 <?php
 namespace Catpow\util;
 class BlockConfig{
+	//picture
 	public static function getPictureSoucesAttributes($selector='picture'){
 		return [
+			'type'=>'array',
 			'source'=>'query',
-			'selector'=>$selector.' source',
+			'selector'=>(empty($selector)?'':' ').'source',
 			'query'=>[
-				'srcset'=>['source'=>'attribute','attribute'=>'srcset'],
-				'device'=>['source'=>'attribute','attribute'=>'data-device']
+				'srcset'=>['type'=>'string','source'=>'attribute','attribute'=>'srcset'],
+				'device'=>['type'=>'string','source'=>'attribute','attribute'=>'data-device']
 			]
 		];
 	}
@@ -20,6 +22,39 @@ class BlockConfig{
 		return array_map(function($device)use($image){
 			return ['srcset'=>\cp::get_file_url('images/'.$image),'device'=>$device];
 		},$devices);
+	}
+	//placed picture
+	public static function getPlacedPicturesAttributes($selector='.placedPictures'){
+		return [
+			'type'=>'array',
+			'source'=>'query',
+			'selector'=>$selector.' > .item',
+			'query'=>[
+				'style'=>['type'=>'object','source'=>'attribute','attribute'=>'style'],
+				'sources'=>self::getPictureSoucesAttributes(),
+				
+				'src'=>['type'=>'string','source'=>'attribute','selector'=>'img','attribute'=>'src'],
+				'alt'=>['type'=>'string','source'=>'attribute','selector'=>'img','attribute'=>'alt'],
+				'code'=>['type'=>'string','source'=>'text'],
+			]
+		];
+	}
+	public static function getPlacedPicturesAttributesForDevices($devices,$selector='.placedPictures',$image='dummy_deco.png'){
+		$attr=self::getPlacedPicturesAttributes($selector);
+		$attr['default']=self::getPlacedPicturesAttributesDefaultValueForDevices($devices,$image);
+		return $attr;
+	}
+	public static function getPlacedPicturesAttributesDefaultValueForDevices($devices,$image='dummy_deco.png'){
+		return [
+			[
+				'style'=>['position'=>'absolute','top'=>'0%','right'=>'0%','width'=>'10rem','height'=>'auto'],
+				'sources'=>self::getPictureSoucesAttributesDefaultValueForDevices($devices,$image),
+				'src'=>\cp::get_file_url('images/'.$image),
+				'srcset'=>\cp::get_file_url('images/'.$image),
+				'alt'=>'',
+				'code'=>''
+			]
+		];
 	}
 }
 
