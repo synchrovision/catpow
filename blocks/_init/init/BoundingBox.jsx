3009f34311929421a5ec20bae873be04abe6c4a5
synchrovision <16245284+synchrovision@users.noreply.github.com>
2023-03-17 22:08:18 +0900

    feat :+1: use transform

--- blocks/_init/init/BoundingBox.jsx
+++ blocks/_init/init/BoundingBox.jsx
@@ -11,41 +11,36 @@ CP.BoundingBox=(props)=>{
 	const container=useMemo(()=>props.container || document,[props.container]);
 	
 	const tracePosition=useCallback((targets)=>{
-		if(targets.length===1){
-			setStyle(extractPosition(targets[0]));
-			return;
-		}
-		const bnd=targets.reduce((bnd,target)=>{
-			const crrBnd=getBoundingArray(extractPosition(target));
-			crrBnd[2]+=crrBnd[0];
-			crrBnd[3]+=crrBnd[1];
+		const cBnd=container.getBoundingClientRect();
+		const bnd=(targets.length===1)?targets[0].getBoundingClientRect():targets.reduce((bnd,target)=>{
+			const crrBnd=target.getBoundingClientRect();
 			if(bnd===false){return crrBnd;}
-			return [
-				Math.min(bnd[0],crrBnd[0]),
-				Math.min(bnd[1],crrBnd[1]),
-				Math.max(bnd[2],crrBnd[2]),
-				Math.max(bnd[3],crrBnd[3]),
-			];
+			return {
+				left:Math.min(bnd.left,crrBnd.left),
+				right:Math.max(bnd.right,crrBnd.right),
+				top:Math.min(bnd.top,crrBnd.top),
+				bottom:Math.max(bnd.bottom,crrBnd.bottom),
+			};
 		},false);
 		setStyle({
-			position:window.getComputedStyle(targets[0]).position,
-			left:bnd[0]+'px',
-			top:bnd[1]+'px',
-			width:(bnd[2]-bnd[0])+'px',
-			height:(bnd[3]-bnd[1])+'px',
+			position:'absolute',
+			left:(bnd.left-cBnd.left)+'px',
+			top:(bnd.top-cBnd.top)+'px',
+			width:(bnd.right-bnd.left)+'px',
+			height:(bnd.bottom-bnd.top)+'px',
 		});
-	},[]);
-	const extractPosition=useCallback((target)=>{
-		if(!target){return {};}
-		const {position,left,top,width,height}=window.getComputedStyle(target);
-		return {position,left,top,width,height};
-	},[]);
-	const getBoundingArray=useCallback((obj)=>[
-		parseFloat(obj.left),
-		parseFloat(obj.top),
-		parseFloat(obj.width),
-		parseFloat(obj.height)
-	]);
+	},[container]);
+	const getRelBnd=useCallback((el)=>{
+		const bnd1=container.getBoundingClientRect();
+		const bnd2=el.getBoundingClientRect();
+		return {
+			...bnd2,
+			left:bnd2.left-bnd1.left,
+			right:bnd2.right-bnd1.left,
+			top:bnd2.top-bnd1.top,
+			bottom:bnd2.bottom-bnd1.top
+		};
+	},[container]);
 	
 	const observer=useMemo(()=>{
 		return new MutationObserver(mutations=>{
@@ -115,27 +110,28 @@ CP.BoundingBox=(props)=>{
 	const onMouseDown=useCallback((e)=>{
 		const control=e.target.closest('[data-control-action]');
 		if(!control){return setAction(false);}
-		if(onDuplicate && e.altKey && control.dataset.controlAction==='move'){
+		const action=control.dataset.controlAction;
+		const flags=parseInt(control.dataset.controlFlags);
+		if(onDuplicate && e.altKey && action==='move'){
 			onDuplicate(targets);
 		}
 		targets.forEach(target=>{
 			target.style.animation='none';
 			target.style.transition='none';
 		});
-		const orgBnd=getBoundingArray(window.getComputedStyle(ref.current));
-		const orgBnds=targets.map(target=>getBoundingArray(window.getComputedStyle(target)));
-		const [ol,ot,ow,oh]=orgBnd;
+		const orgBnd=ref.current.getBoundingClientRect();
+		const ox=orgBnd.right-(flags & 12)/8*orgBnd.width;
+		const oy=orgBnd.bottom-(flags & 3)/2*orgBnd.height;
+		if(action==='resize'){
+			targets.forEach(target=>{
+				const bnd=target.getBoundingClientRect();
+				target.style['transform-origin']=`${ox-bnd.left}px ${oy-bnd.top}px`;
+			});
+		}
 		setAction({
-			action:control.dataset.controlAction,
-			flags:parseInt(control.dataset.controlFlags),
-			org:{x:e.clientX,y:e.clientY},
+			action,flags,
+			org:{x:e.clientX,y:e.clientY,ox,oy},
 			orgBnd,
-			orgBnds,
-			coefs:orgBnds.map(bnd=>{
-				const [l,t,w,h]=bnd;
-				return [(l-ol)/ow,(t-ot)/oh,w/ow,h/oh];
-			}),
-			orgAspect:oh/ow,
 			keepAspect:e.shiftKey,
 			keepCenter:e.altKey,
 			targets
@@ -147,80 +143,36 @@ CP.BoundingBox=(props)=>{
 		const dy=e.clientY-action.org.y;
 		if(action.action==='move'){
 			targets.forEach((target,index)=>{
-				target.style.left=action.orgBnds[index][0]+dx+'px';
-				target.style.top=action.orgBnds[index][1]+dy+'px';
+				target.style.transform=`translate(${dx}px,${dy}px)`;
 			});
 		}
 		else if(action.action==='resize'){
-			let [ol,ot,ow,oh]=action.orgBnd;
-			let [l,t,w,h]=action.orgBnd;
-			if(!(action.flags&4)){
-				if(action.flags&8){
-					w+=dx;
-				}
-				else{
-					w-=dx;
-					l+=dx;
-				}
+			let s;
+			const sx=Math.abs(e.clientX-action.org.ox)/action.orgBnd.width;
+			const sy=Math.abs(e.clientY-action.org.oy)/action.orgBnd.height;
+			if(action.flags & 1){
+				s=action.keepAspect?`scale(${sx})`:`scaleX(${sx})`;
 			}
-			if(!(action.flags&1)){
-				if(action.flags&2){
-					h+=dy;
-				}
-				else{
-					h-=dy;
-					t+=dy;
-				}
+			else if(action.flags & 4){
+				s=action.keepAspect?`scale(${sy})`:`scaleY(${sy})`;
 			}
-			if(action.keepAspect){
-				let d;
-				if(action.flags&1){
-					h*=w/ow;
-					t+=(oh-h)/2;
-				}
-				else if(action.flags&4){
-					w*=h/oh;
-					l+=(ow-w)/2;
-				}
-				else{
-					if(w*oh>h*ow){
-						w=ow*h/oh;
-						if(!(action.flags&8)){
-							l=ol+ow-w;
-						}
-					}
-					else{
-						h=oh*w/ow;
-						if(!(action.flags&2)){
-							t=ot+oh-h;
-						}
-					}
-				}
+			else{
+				s=action.keepAspect?`scale(${Math.min(sx,sy)})`:`scale(${sx},${sy})`;
 			}
 			targets.forEach((target,index)=>{
-				const coef=action.coefs[index];
-				target.style.left=l+w*coef[0]+'px';
-				target.style.top=t+h*coef[1]+'px';
-				target.style.width=w*coef[2]+'px';
-				target.style.height=h*coef[3]+'px';
+				target.style.transform=s;
 			});
 		}
 	},[action]);
 	const onMouseUp=useCallback((e)=>{
+		if(onChange){
+			onChange(action.targets);
+		}
 		action.targets.map(target=>{
 			target.style.animation='';
 			target.style.transition='';
-		});
-		if(onChange){
-			onChange(action.targets.map(
-				target=>getBoundingArray(extractPosition(target))
-			));
-		}
-		action.targets.map(target=>{
-			target.style.left='';
-			target.style.top='';
-			target.style.width='';
-			target.style.height='';
+			target.style.transform='';
+			target.style['transform-origin']='';
 		});
 		setAction(false);
 	},[action,onChange]);
@@ -230,9 +182,7 @@ CP.BoundingBox=(props)=>{
 			target.style.height=window.getComputedStyle(target).height+'px';
 		});
 		if(onChange){
-			onChange(targets.map(
-				target=>getBoundingArray(extractPosition(target))
-			));
+			onChange(targets);
 		}
 	},[targets,onChange]);
 	
