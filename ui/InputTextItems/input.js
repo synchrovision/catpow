304bde233299553ef09fdb5abee528477a547299
synchrovision <16245284+synchrovision@users.noreply.github.com>
2023-03-21 19:03:22 +0900

    feat :+1: add InputTextItems UI

---
+++ ui/InputTextItems/input.js
@@ -0,0 +1,15 @@
+(() => {
+  // ../ui/InputTextItems/input.jsx
+  Catpow.UI.InputTextItems = (props) => {
+    const { name, defaultLabel, options, multiple = false } = props;
+    const { useState } = wp.element;
+    const [value, setValue] = useState(props.value);
+    return /* @__PURE__ */ wp.element.createElement(wp.element.Fragment, null, /* @__PURE__ */ wp.element.createElement(
+      Catpow.InputTextItems,
+      {
+        items: value,
+        onChange: setValue
+      }
+    ), /* @__PURE__ */ wp.element.createElement(Catpow.UI.HiddenValues, { name, value }));
+  };
+})();
---
+++ ui/InputTextItems/input.jsx
@@ -0,0 +1,14 @@
+﻿Catpow.UI.InputTextItems=(props)=>{
+	const {name,defaultLabel,options,multiple=false}=props;
+	const {useState}=wp.element;
+	const [value,setValue]=useState(props.value);
+	return (
+		<>
+			<Catpow.InputTextItems
+				items={value}
+				onChange={setValue}
+			/>
+			<Catpow.UI.HiddenValues name={name} value={value}/>
+		</>
+	);
+}
