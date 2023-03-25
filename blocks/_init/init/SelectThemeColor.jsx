91c3f71bea0eeef2a3ca012f0c7240a52e196d5f
synchrovision <16245284+synchrovision@users.noreply.github.com>
2023-03-25 20:48:33 +0900

    feat :+1: enable to modify tone at SelectThemeColor

--- blocks/_init/init/SelectThemeColor.jsx
+++ blocks/_init/init/SelectThemeColor.jsx
@@ -1,27 +1,34 @@
 ﻿import {CP} from './CP.jsx';
 
 CP.SelectThemeColor=(props)=>{
-	const {selected,onChange}=props;
-	const {useCallback,useMemo}=wp.element;
+	const {onChange}=props;
+	const {useCallback,useMemo,Fragment}=wp.element;
 	const {Icon}=wp.components;
 	const {bem}=Catpow.util;
 	const classes=bem('CP-SelectThemeColor');
 	
-	const data=useMemo(()=>CP.parseColorClass(selected),[selected]);
+	const proxy=useMemo(()=>CP.colorClassProxy(props.selected),[props.selected]);
+	const data=useMemo(()=>CP.parseColorClass(proxy.h),[proxy.h]);
 	
-	const Selections=useCallback((props)=>{
-		const {fixed=false,absolute=false,relative=false,active=false,selected}=props;
+	const ColorSelections=useCallback((props)=>{
+		const {fixed=false,absolute=false,relative=false,active=false,proxy}=props;
+		const {h,s,l}=proxy;
+		const hsl={h,s,l};
 		return (
-			<ul className={classes.items({fixed,absolute,relative,active})}>
-				<li className={classes.items.icon({active})}>
+			<ul className={classes.colors({fixed,absolute,relative,active})}>
+				<li className={classes.colors.icon({active})}>
 					<Icon icon={fixed?'lock':(absolute?'media-default':'excerpt-view')}/>
 				</li>
 				{Array.from(Array(13),(v,value)=>{
-					const colorClass=CP.generateColorClass({fixed,absolute,relative,value})
+					const colorClass=CP.generateColorClass({fixed,absolute,relative,value});
+					const active=colorClass===h;
 					return (
 						<li
-							className={classes.items.item(colorClass,{active:colorClass==selected,fixed,absolute,relative})}
-							onClick={()=>onChange(colorClass)}
+							className={classes.colors.item(colorClass,s,l,{active})}
+							onClick={()=>{
+								proxy.h=!active && colorClass;
+								onChange(proxy);
+							}}
 							key={colorClass}
 						> </li>
 					);
@@ -29,12 +36,44 @@ CP.SelectThemeColor=(props)=>{
 			</ul>
 		);
 	},[onChange]);
+	const ToneSelections=useCallback((props)=>{
+		const {proxy}=props;
+		const {h,s,l}=proxy;
+		const hsl={h,s,l};
+		return (
+			<ul className={classes.tones()}>
+				{['s','l'].map((r)=>(
+					<Fragment key={r}>
+						<li className={classes.colors.icon({active:!!hsl[r]})}>
+							<CP.ConfigIcon icon={{s:'contrast',l:'light'}[r]}/>
+						</li>
+						{Array.from(Array(5),(v,index)=>{
+							const value=index-2;
+							const toneClass=CP.generateToneClass({[r]:true,value})
+							const active=toneClass===hsl[r];
+							return (
+								<li
+									className={classes.tones.item(h,r==='s'?s:l,toneClass,{active})}
+									onClick={()=>{
+										proxy[r]=!active && toneClass;
+										onChange(proxy);
+									}}
+									key={toneClass}
+								> </li>
+							);
+						})}
+					</Fragment>
+				))}
+			</ul>
+		);
+	},[onChange]);
 
 	return (
 		<div className={classes()}>
-			<Selections selected={selected} fixed={true} active={data.fixed}/>
-			<Selections selected={selected} absolute={true} active={data.absolute}/>
-			<Selections selected={selected} relative={true} active={data.relative}/>
+			<ColorSelections proxy={proxy} fixed={true} active={data.fixed}/>
+			<ColorSelections proxy={proxy} absolute={true} active={data.absolute}/>
+			<ColorSelections proxy={proxy} relative={true} active={data.relative}/>
+			<ToneSelections proxy={proxy}/>
 		</div>
 	);
 };
