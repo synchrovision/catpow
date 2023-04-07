3d13cc336f09ccf7f3deb17728027fa7e5497db7
synchrovision <16245284+synchrovision@users.noreply.github.com>
2023-04-07 14:28:07 +0900

    feat :+1: update SelectTerms meta type

--- classes/meta/SelectTerms.php
+++ classes/meta/SelectTerms.php
@@ -2,7 +2,14 @@
 namespace Catpow\meta;
 
 class SelectTerms extends UI{
-	static $ui='SearchSelect',$output_type='checkbox_terms',$defaultParam=['defaultLabel'=>'---','multiple'=>true];
+	static $ui='SearchSelect',$is_bulk_input=true,$output_type='checkbox_terms',$defaultParam=['defaultLabel'=>'---','multiple'=>true];
+	
+	public static function get($data_type,$data_name,$id,$meta_name,$conf){
+		return checkbox_terms::get($data_type,$data_name,$id,$meta_name,$conf);
+	}
+	public static function set($data_type,$data_name,$id,$meta_name,$vals,$conf){
+		return checkbox_terms::set($data_type,$data_name,$id,$meta_name,$vals,$conf);
+	}
 	public static function fill_param($param,$meta){
 		$param=parent::fill_param($param,$meta);
 		$param['options']=checkbox_terms::get_selections($meta);
