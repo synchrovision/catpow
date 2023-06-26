4e683a3803f601cdd0772f59c4d6773151dc9865
synchrovision <hatano@catpow.info>
2023-06-26 16:44:05 +0900

    feat :+1: add api\blocks\parts class

---
+++ classes/api/blocks/parts.php
@@ -0,0 +1,34 @@
+<?php
+namespace Catpow\api\blocks;
+
+/**
+* CP.ServerSideRenderPart用のエンドポイント
+* SSRで用いる各種パーツのプレビューのHTMLを返す
+* レスポンスの構造はblocks/renderに倣う
+* 各パーツの出力はblocks/render/[パーツ名]クラスの
+* previewとrendarメソッドによって定義される
+*/
+
+class parts extends \Catpow\api{
+	public static 
+		$method=false,
+		$check_nonce=false;
+	public static function call($req,$res){
+		if(!empty($req['post_id'])){
+			$GLOBALS['post']=get_post($req['post_id']);
+		}
+		$rendered=$req['tmp_name'].' SSR';
+		$class_name="Catpow\\api\\blocks\\parts\\{$req['tmp_name']}";
+		ob_start();
+		$class_name::preview($req);
+		$rendered=ob_get_clean();
+		
+		//dequeue style and scripts that should already loaded.
+		wp_dequeue_script('catpow');
+		
+		$deps=\Catpow\util\wp::get_deps();
+		$res->set_data(compact('rendered','deps'));
+	}
+}
+
+?>
