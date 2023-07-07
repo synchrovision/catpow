cf5051e907614f370e50006723b1092eb645490c
synchrovision <hatano@catpow.info>
2023-07-07 13:23:39 +0900

    feat :+1: add block code generator for core/image block

---
+++ blocks/image/gen.php
@@ -0,0 +1,15 @@
+<?php
+$blockAttr=array_diff_key($attr,['src'=>1,'alt'=>1]);
+$classNames=array_merge(['wp-block-image'],explode(' ',$attr['className']??''));
+if(isset($attr['align'])){$classNames[]='align'.$attr['align'];}
+$classNames=array_unique($classNames);
+$imgAtts=array_merge(
+	['src'=>\cp::get_file_url('images/dummy.jpg'),'alt'=>''],
+	array_intersect_key($attr,['src'=>1,'alt'=>1,'width'=>1,'height'=>1])
+);
+if(isset($attr['id'])){$imgAtts['class']=sprintf(' class="wp-image-%d"',$attr['id']);}
+$imgAttr=implode(' ',array_map(function($key)use($imgAtts){return sprintf('%s="%s"',$key,$imgAtts[$key]);},$imgAtts));
+?>
+<!-- wp:image<?=!empty($blockAttr)?' '.json_encode($blockAttr,0500):''?> -->
+<figure class="<?=implode(' ',$classNames)?>"><img src="<?=$src?>" alt="<?=$alt?>"<?=isset($attr['id'])?:''?>/></figure>
+<!-- /wp:image -->
