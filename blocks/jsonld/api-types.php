b0269a4c8274c855a2dc8f1f5ca57fc737b5de37
synchrovision <hatano@catpow.info>
2023-08-03 20:48:42 +0900

    feat :+1: log error and skip process if failed to parse json

--- blocks/jsonld/api-types.php
+++ blocks/jsonld/api-types.php
@@ -8,6 +8,10 @@ foreach(glob(__DIR__.'/schema/*.json') as $json_file){
 }
 foreach(glob(__DIR__.'/schema/formal/*.json') as $json_file){
 	$data=json_decode(file_get_contents($json_file),true);
+	if(empty($data)){
+		error_log("failed to parse json : {$json_file}");
+		continue;
+	}
 	$formals[$data['name']]=$data;
 }
 $res->set_data(['types'=>$types,'formals'=>$formals]);
