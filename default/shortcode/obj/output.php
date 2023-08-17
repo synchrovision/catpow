cd1ae0b5e83c813695a41f4a3b0022bc3c9445e9
synchrovision <hatano@catpow.info>
2023-08-17 16:39:49 +0900

    feat :+1: enable to output url or href at obj shortcode

--- default/shortcode/obj/output.php
+++ default/shortcode/obj/output.php
@@ -1,4 +1,9 @@
 <?php
 namespace Catpow;
 $prm=shortcode_atts(array(0=>'name'),$atts);
-echo \cp::$content->object->{$prm[0]};
+
+switch($prm[0]){
+	case 'url':echo \cp::$content->get_the_url();break;
+	case 'href':$ulr=\cp::$content->get_the_url();echo substr($url,strpos($url,'://')+3);break;
+	default:echo \cp::$content->object->{$prm[0]};
+}
