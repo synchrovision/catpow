a302e66750f3a5c9cc1c65961bea3f35a944f3f3
synchrovision <hatano@catpow.info>
2023-09-18 18:44:42 +0900

    feat :+1: add leave popup block

---
+++ blocks/leavepopup/conf.php
@@ -0,0 +1,4 @@
+<?php
+$attributes=[
+	'classes'=>['source'=>'attribute','selector'=>'.wp-block-catpow-leavepopup','attribute'=>'class','default'=>'wp-block-catpow-leavepopup is-close is-for-site']
+];
