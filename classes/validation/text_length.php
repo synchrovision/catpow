6c5f1c928a6a2ad60052c4c7af4eae66a1acc5f3
synchrovision <hatano@catpow.info>
2023-11-16 17:22:39 +0900

    fix :bug:

--- classes/validation/text_length.php
+++ classes/validation/text_length.php
@@ -5,7 +5,7 @@ class text_length extends validation{
 	public static $message_keys=['length'];
 	
 	public static function is_valid(&$val,$conf,$input_id){
-		return mb_strlen($val)!==(int)$conf['length'];
+		return mb_strlen($val)===(int)$conf['length'];
 	}
 	public static function get_message_format($conf){
 		return __('%d文字で入力してください','catpow');
