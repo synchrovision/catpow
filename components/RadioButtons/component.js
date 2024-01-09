aca105f79a562ddea2c934b92880d4992630a8b7
synchrovision <hatano@catpow.info>
2024-01-09 13:17:40 +0900

    feat :+1: update RadioButtons component

--- components/RadioButtons/component.js
+++ components/RadioButtons/component.js
@@ -1,8 +1,11 @@
 (() => {
   // ../components/RadioButtons/component.jsx
   Catpow.RadioButtons = (props) => {
-    const { useMemo } = wp.element;
-    const { value, onChange } = props;
+    const { className = "RadioButtons", useState, useMemo } = wp.element;
+    const { onChange } = props;
+    const { bem } = Catpow.util;
+    const classes = useMemo(() => bem(className), []);
+    const [value, setValue] = useState(props.value ?? null);
     const options = useMemo(() => {
       if (Array.isArray(props.options)) {
         return props.options.map((option) => {
@@ -16,20 +19,21 @@
         return { label, value: props.options[label] };
       });
     }, [props.options]);
-    return /* @__PURE__ */ wp.element.createElement("div", { className: "RadioButtons" }, options.map((option) => {
+    return /* @__PURE__ */ wp.element.createElement("div", { className: classes() }, options.map((option) => {
       const selected = option.value === value;
       return /* @__PURE__ */ wp.element.createElement(
         "div",
         {
-          className: "RadioButton" + (selected ? " selected" : ""),
+          className: classes.button({ "is-selected": selected }),
           onClick: (e) => {
+            setValue(option.value);
             onChange(option.value);
           },
           role: "checkbox",
           "aria-checked": selected,
           key: option.label
         },
-        /* @__PURE__ */ wp.element.createElement("div", { className: "RadioButtonIcon" + (selected ? " selected" : "") }, " "),
+        /* @__PURE__ */ wp.element.createElement("div", { className: classes.button.icon({ "is-selected": selected }) }, " "),
         option.label
       );
     }));
