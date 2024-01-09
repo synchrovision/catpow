67346f2d46feb30fc1715b1e23ce828881ab541f
synchrovision <hatano@catpow.info>
2024-01-09 14:37:37 +0900

    feat :+1: update CheckBoxes component

--- components/CheckBoxes/component.jsx
+++ components/CheckBoxes/component.jsx
@@ -1,7 +1,10 @@
 ﻿Catpow.CheckBoxes=(props)=>{
-	const {useMemo}=wp.element;
+	const {className="CheckBoxes",useState,useMemo}=wp.element;
 	const {value=[],onChange}=props;
 	const {CheckBox}=Catpow;
+	const {bem}=Catpow.util;
+	const classes=useMemo(()=>bem(className),[]);
+	
 	const options=useMemo(()=>{
 		if(Array.isArray(props.options)){
 			return props.options.map((option)=>{
@@ -18,7 +21,7 @@
 		const flags={};
 		value.map((val)=>flags[val]=true);
 		return (
-			<div className={"CheckBoxes"}>
+			<div className={classes()}>
 				{options.map((option)=>(
 					<CheckBox
 						label={option.label}
@@ -36,13 +39,13 @@
 	}
 	
 	return (
-		<div className={"CheckBoxes"}>
+		<div className={classes()}>
 			{options.map((option)=>(
 				<CheckBox
 					label={option.label}
 					onChange={(selected)=>{
 						value[option.value]=selected;
-						onChange(option.value,selected,value)
+						onChange(option.value,selected,value);
 					}}
 					selected={value[option.value]}
 					key={option.label}
