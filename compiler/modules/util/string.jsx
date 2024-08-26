c40fe52c1dbe360d6e2dcd25048d921fac0803cf
synchrovision <trinitybluster@yahoo.co.jp>
2024-08-26 22:43:36 +0900

    feat :+1: add flags classNames translation functions

--- compiler/modules/util/string.jsx
+++ compiler/modules/util/string.jsx
@@ -11,3 +11,10 @@ export const flagsToWords=(flags)=>{
 	if(undefined === flags){return '';}
 	return Object.keys(flags).filter((word)=>flags[word]).join(' ');
 };
+export const classNamesToFlags=(classNames)=>classNames && classNames.split(' ').map(kebabToCamel).reduce((p,c)=>{p[c]=true;return p},{});
+export const flagsToClassNames=(flags)=>flags && Object.keys(flags).filter((f)=>flags[f]).map(camelToKebab).join(' ');
+
+export const camelToKebab=(str)=>str.replace(/(\w)([A-Z])/g,'$1-$2').toLowerCase();
+export const camelToSnake=(str)=>str.replace(/(\w)([A-Z])/g,'$1_$2').toLowerCase();
+export const kebabToCamel=(str)=>str.replace(/\-(\w)/g,(m)=>m[1].toUpperCase());
+export const snakeToCamel=(str)=>str.replace(/_(\w)/g,(m)=>m[1].toUpperCase());
