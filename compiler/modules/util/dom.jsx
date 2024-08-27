7d05fe3303506fe40b594d4118715bce7d2f7a6f
synchrovision <hatano@catpow.info>
2024-08-27 15:23:43 +0900

    feat :+1: enable to nest children array at el

--- compiler/modules/util/dom.jsx
+++ compiler/modules/util/dom.jsx
@@ -1,15 +1,22 @@
 export const el=(tag,props,children)=>{
 	const el=document.createElement(tag);
+	const appendChild=(child)=>{
+		if(child instanceof HTMLElement){
+			el.appendChild(child);
+		}
+		else if(typeof child === 'string'){
+			el.appendChild(document.createTextNode(child));
+		}
+		else if(Array.isArray(child)){
+			child.forEach(appendChild);
+		}
+		else{
+			console.error('can not append child : ',child);
+		}
+	}
 	Object.keys(props).forEach((key)=>{
 		el[key]=props[key];
 	});
-	if(Array.isArray(children)){
-		children.forEach((child)=>{
-			if(typeof child === 'string'){
-				el.appendChild(document.createTextNode(child));
-			}
-			else{el.appendChild(child);}
-		});
-	}
+	appendChild(children);
 	return el;
 };
