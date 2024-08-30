936885ce67a14f310da68ca12d7501d5408e9f1f
synchrovision <hatano@catpow.info>
2024-08-30 10:28:10 +0900

    feat :+1: switch process with arguments length

--- compiler/modules/util/rand.jsx
+++ compiler/modules/util/rand.jsx
@@ -1,10 +1,13 @@
 export const srand=(w=88675123)=>{
 	var x=123456789,y=362436069,z=521288629;
-	return (min,max)=>{
+	return function(){
 		let t;
 		t=x^(x<<11);
 		[x,y,z]=[y,z,w];
 		w=(w^(w>>>19))^(t^(t>>>8));
+		if(arguments.length===0){return w;}
+		if(arguments.length===1){return w%(arguments[0]+1);}
+		const [min,max]=arguments;
 		return min+Math.abs(w)%(max+1-min);
 	}
 };
