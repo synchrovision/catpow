4b454dab5a71e562191a0c54d6f68b7a122f1403
synchrovision <trinitybluster@yahoo.co.jp>
2024-09-01 03:04:28 +0900

    feat :+1: enable to add element with namespace

--- compiler/modules/util/dom.jsx
+++ compiler/modules/util/dom.jsx
@@ -1,7 +1,7 @@
-export const el=(tag,props,children)=>{
-	const el=document.createElement(tag);
+export const el=(tag,props,children,namespace)=>{
+	const el=namespace?document.createElementNS(namespace,tag):document.createElement(tag);;
 	const appendChild=(child)=>{
-		if(child instanceof HTMLElement){
+		if(child instanceof Node){
 			el.appendChild(child);
 		}
 		else if(typeof child === 'string'){
@@ -15,8 +15,11 @@ export const el=(tag,props,children)=>{
 		}
 	}
 	Object.keys(props).forEach((key)=>{
-		el[key]=props[key];
+		el.setAttribute(key,props[key]);
 	});
 	appendChild(children);
 	return el;
 };
+export const xhtmlEl=(tag,props,children)=>el(tag,props,children,'http://www.w3.org/1999/xhtml');
+export const svgEl=(tag,props,children)=>el(tag,props,children,'http://www.w3.org/2000/svg');
+export const mathEl=(tag,props,children)=>el(tag,props,children,'http://www.w3.org/1998/Math/MathML');
