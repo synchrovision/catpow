213519a146370e87545a0624bdae4e4f9947a20c
synchrovision <trinitybluster@yahoo.co.jp>
2024-09-22 20:17:01 +0900

    feat :+1: add colorToHsla function

--- compiler/modules/util/colors.jsx
+++ compiler/modules/util/colors.jsx
@@ -84,4 +84,21 @@ export const hsbToHex=(hsb)=>{
 		return Math.round(255*c).toString(16).padStart(2,'0');
 	};
 	return '#'+f(0)+f(4)+f(2);
-}
+};
+export const colorToHsla=(color)=>{
+	if(color.slice(0,3)==='hsl'){
+		const matches=color.match(/hsla?\(\s*(\d+),\s*([\d\.]+)%,\s*([\d\.]+)%(,\s*([\d\.]+))?/);
+		return {h:Math.round(matches[1]),s:Math.round(matches[2]),l:Math.round(matches[3]),a:parseFloat((matches[5]==null)?1:matches[5])};
+	}
+	if(color.slice(0,3)==='rgb'){
+		const matches=color.match(/rgba?\(\s*(\d+),\s*(\d+),\s*(\d+)(,\s*([\d\.]+))?/);
+		const rgba={r:Math.round(matches[1]),g:Math.round(matches[2]),b:Math.round(matches[3]),a:parseFloat((matches[5]==null)?1:matches[5])};
+		return Object.assign({a:rgba.a},hexToHsl(rgbToHex(rgba)));
+	}
+	if(color[0]==="#"){
+		let a=1;
+		if(color.length===4){color=color.replace(/#(\w)(\w)(\w)/,'#$1$1$2$2$3$3');}
+		if(color.length===9){a=parseInt(color.slice(7),16)/255;color=color.slice(0,7);}
+		return Object.assign({a},hexToHsl(color));
+	}
+};
