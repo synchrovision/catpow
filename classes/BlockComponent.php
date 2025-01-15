2942f194230fd89e32c7f623353788444e89a5f5
synchrovision <hatano@catpow.info>
2025-01-15 18:32:52 +0900

    feat 👍 add BlockComponent class

---
+++ classes/BlockComponent.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Catpowの各種ブロックを構成するコンポーネントのattributeをサーバーサイドでレンダリングするためのメソッド群
+ */
+namespace Catpow;
+
+class BlockComponent{
+	static $devices=[
+		'sp'=>[
+			'icon'=>'smartphone',
+			'width'=>480,
+			'media_query'=>'(max-width:640px)',
+			'sizes'=>'(max-width:640px) 480px',
+			'sizes_value'=>'480px',
+			'media_size'=>'medium_large',
+			'reg'=>'/[^,]+ 480w,/',
+			'rep'=>' 480w,'
+		],
+		'tb'=>[
+			'icon'=>'tablet',
+			'width'=>960,
+			'media_query'=>'(max-width:1280px)',
+			'sizes'=>'(max-width:1280px) 960px',
+			'sizes_value'=>'960px',
+			'media_size'=>'full',
+			'reg'=>'/[^,]+ 960w,/',
+			'rep'=>' 960w,'
+		],
+		'lt'=>[
+			'icon'=>'laptop',
+			'width'=>1440,
+			'media_query'=>'(max-width:1920px)',
+			'sizes'=>'(max-width:1920px) 1440px',
+			'sizes_value'=>'1440px',
+			'media_size'=>'full',
+			'reg'=>'/[^,]+ 1440w,/',
+			'rep'=>' 1440w,'
+		],
+		'pc'=>[
+			'icon'=>'desktop',
+			'width'=>1920,
+			'media_query'=>false,
+			'sizes'=>'100vw',
+			'sizes_value'=>'100vw',
+			'media_size'=>'full',
+			'reg'=>'/[^,]+$/',
+			'rep'=>''
+		]
+	];
+	public static function ResponsiveImage($props,$conf){
+		if(isset($conf['keys']) && !empty($props)){
+			foreach($conf['keys'] as $key=>$name){$props[$key]=$props[$name];}
+		}
+		if(empty($props['src'])){
+			$props=['mime'=>'image/jpg','src'=>CP::get_file_url('images/dummy.jpg'),'alt'=>__('ダミー画像','catpow')];
+		}
+		$type=strstr($props['mime'],'/',true);
+		$tag=$type==='image'?'picture':$type;
+		$sources='';
+		if(isset($props['sources'])){
+			$src_attr=$type==='image'?'srcset':'src';
+			foreach($props['sources'] as $source){
+				$sources.=sprintf('<source %s="%s" media="%s"/>',$src_attr,$source['srcset'],self::$devices[$source['device']]['media_query']);
+			}
+		}
+		$attr=sprintf('class="%s"',$props['className']?:'CP-ResponsiveImage');
+		if($type==='image'){
+			$base=sprintf('<img class="%s" src="%s" alt="%s"/>',$props['className'],$props['src'],$props['alt']);
+		}
+		else{
+			$attr.=sprintf(' autoplay loop playsinline muted');
+			$base=sprintf('<source src="%s"/>',$props['src']);
+		}
+		return sprintf(
+			'<%s %s>%s%s</%1$s>',
+			$tag,$attr,$sources,$base
+		);
+	}
+}
+
+
+?>
