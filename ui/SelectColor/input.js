2d2a6e3e3f7903321182672fe5420fbf4994f56e
synchrovision <hatano@catpow.info>
2025-02-04 03:01:47 +0900

    chore 🆙 migration

--- ui/SelectColor/input.js
+++ ui/SelectColor/input.js
@@ -4,6 +4,6 @@
     const { useState } = wp.element;
     const { name } = props;
     const [value, setValue] = useState(props.value);
-    return /* @__PURE__ */ wp.element.createElement(wp.element.Fragment, null, /* @__PURE__ */ wp.element.createElement(Catpow.SelectColorToneClass, { selected: value, onChange: (proxy) => setValue(proxy.classes) }), /* @__PURE__ */ wp.element.createElement(Catpow.UI.HiddenValues, { name, value }));
+    return /* @__PURE__ */ wp.element.createElement(wp.element.Fragment, null, /* @__PURE__ */ wp.element.createElement(Catpow.SelectColorToneClass, { selected: value, onChange: (proxy) => setValue(proxy.classes.split(" ")) }), /* @__PURE__ */ wp.element.createElement(Catpow.UI.HiddenValues, { name, value }));
   };
 })();
