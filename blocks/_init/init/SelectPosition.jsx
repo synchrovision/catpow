24da9a36c0569286d964aa7bc566c29640e20835
synchrovision <hatano@catpow.info>
2025-05-10 10:08:58 +0900

    feat 👍 add SelectPosition component

---
+++ blocks/_init/init/SelectPosition.jsx
@@ -0,0 +1,23 @@
+﻿import { CP } from "./CP.jsx";
+import { clsx } from "clsx";
+
+const isCustomPosition = (pos) => !pos.split(" ").every((p) => ["top", "bottom", "left", "right", "center"].includes(p));
+
+CP.SelectPosition = (props) => {
+	return (
+		<CP.Bem prefix="cp">
+			<table className="selectposition-">
+				<tbody className="_body">
+					{["top", "center", "bottom"].map((v) => (
+						<tr className="_row">
+							{["left", "center", "right"].map((h) => {
+								const val = `${v} ${h}`;
+								return <td className={clsx("_cell", { "is-active": val === props.value })} onClick={() => props.onChange(val)}></td>;
+							})}
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</CP.Bem>
+	);
+};
