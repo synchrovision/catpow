11f0100905a7d66bc2cb5a7bdae7cf7460969588
synchrovision <hatano@catpow.info>
2025-06-02 20:31:55 +0900

    refactor ♻️ use clsx and Bem component

--- blocks/_init/init/ItemControl.jsx
+++ blocks/_init/init/ItemControl.jsx
@@ -1,29 +1,30 @@
 ﻿import { CP } from "./CP.jsx";
-import { bem } from "catpow/util";
+import clsx from "clsx";
 
 CP.ItemControl = (props) => {
 	const { className = "", tag: Tag = "div", controls, float = true, children } = props;
 	const { useState } = wp.element;
-	const classes = bem("cp-itemcontrol");
 
 	const [open, setOpen] = useState(false);
 
 	return (
-		<Tag
-			className={classes(className, {
-				"is-open": open,
-				"is-position-absolute": float,
-			})}
-		>
-			{Object.keys(controls).map((key) => {
-				return <div className={classes.button("is-" + key)} onClick={controls[key]} key={key}></div>;
-			})}
-			{children && (
-				<>
-					<div className={classes.button("is-edit")} onClick={() => setOpen(!open)}></div>
-					<div className={classes.inputs()}>{children}</div>
-				</>
-			)}
-		</Tag>
+		<CP.Bem prefix="cp">
+			<Tag
+				className={clsx("itemcontrol-", className, {
+					"is-open": open,
+					"is-position-absolute": float,
+				})}
+			>
+				{Object.keys(controls).map((key) => {
+					return <div className={clsx("-button", "is-" + key)} onClick={controls[key]} key={key}></div>;
+				})}
+				{children && (
+					<>
+						<div className={clsx("-button", "is-edit")} onClick={() => setOpen(!open)}></div>
+						<div className="-inputs">{children}</div>
+					</>
+				)}
+			</Tag>
+		</CP.Bem>
 	);
 };
