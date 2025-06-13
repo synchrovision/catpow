9ff544e151b24cf3f24ffa0c1acdd2d415d1b81e
synchrovision <hatano@catpow.info>
2025-06-13 11:57:55 +0900

    chore 🆙 lint

--- components/AppManager/component.js
+++ components/AppManager/component.js
@@ -15,7 +15,7 @@
       }
     }, []);
     const [state, dispatch] = useReducer(reducer, { outerContents: false });
-    return /* @__PURE__ */ React.createElement(Fragment, null, /* @__PURE__ */ React.createElement(Catpow.AppManagerContext.Provider, { value: { state, dispatch } }, children), Object.keys(state.outerContents).map((name) => state.outerContents[name]));
+    return /* @__PURE__ */ wp.element.createElement(Fragment, null, /* @__PURE__ */ wp.element.createElement(Catpow.AppManagerContext.Provider, { value: { state, dispatch } }, children), Object.keys(state.outerContents).map((name) => state.outerContents[name]));
   };
   Catpow.OuterContents = (props) => {
     const { name, children } = props;
--- components/AppManager/component.jsx
+++ components/AppManager/component.jsx
@@ -1,36 +1,34 @@
-﻿Catpow.AppManagerContext=wp.element.createContext({});
-Catpow.AppManager=(props)=>{
-	const {children}=props;
-	const {Fragment,useCallback,useReducer}=wp.element;
-	
-	const reducer=useCallback((state,action)=>{
-		switch(action.type){
-			case 'update':
-				return {...state,...(action.data || {})};
-			case 'setOuterContents':{
-				const {name,children}=action;
-				return {...state,outerContents:{...state.outerContents,[name]:children}};
+﻿Catpow.AppManagerContext = wp.element.createContext({});
+Catpow.AppManager = (props) => {
+	const { children } = props;
+	const { Fragment, useCallback, useReducer } = wp.element;
+
+	const reducer = useCallback((state, action) => {
+		switch (action.type) {
+			case "update":
+				return { ...state, ...(action.data || {}) };
+			case "setOuterContents": {
+				const { name, children } = action;
+				return { ...state, outerContents: { ...state.outerContents, [name]: children } };
 			}
 		}
-	},[]);
-	const [state,dispatch]=useReducer(reducer,{outerContents:false});
-	
+	}, []);
+	const [state, dispatch] = useReducer(reducer, { outerContents: false });
+
 	return (
 		<Fragment>
-			<Catpow.AppManagerContext.Provider value={{state,dispatch}}>
-				{children}
-			</Catpow.AppManagerContext.Provider>
-			{Object.keys(state.outerContents).map((name)=>state.outerContents[name])}
+			<Catpow.AppManagerContext.Provider value={{ state, dispatch }}>{children}</Catpow.AppManagerContext.Provider>
+			{Object.keys(state.outerContents).map((name) => state.outerContents[name])}
 		</Fragment>
 	);
-}
-Catpow.OuterContents=(props)=>{
-	const {name,children}=props;
-	const {useCallback,useContext,useEffect}=wp.element;
-	const {state,dispatch}=useContext(Catpow.AppManagerContext);
-	useEffect(()=>{
-		dispatch({type:'setOuterContents',name,children})
-	},[name,children]);
-	
+};
+Catpow.OuterContents = (props) => {
+	const { name, children } = props;
+	const { useCallback, useContext, useEffect } = wp.element;
+	const { state, dispatch } = useContext(Catpow.AppManagerContext);
+	useEffect(() => {
+		dispatch({ type: "setOuterContents", name, children });
+	}, [name, children]);
+
 	return false;
-}
+};
