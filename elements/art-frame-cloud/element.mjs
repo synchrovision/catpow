ad5e668065d7fcaf77359c3e25fe2453f19a9518
synchrovision <hatano@catpow.info>
2025-06-20 13:30:47 +0900

    feat 👍 add art-frame-cloud element

---
+++ elements/art-frame-cloud/element.mjs
@@ -0,0 +1,185 @@
+// modules/src/util/dom.ts
+var el = (tag, props, children, namespace) => {
+  const el2 = namespace ? document.createElementNS(namespace, tag) : document.createElement(tag);
+  const appendChild = (child) => {
+    if (child == null) {
+      return;
+    }
+    if (child instanceof Node) {
+      el2.appendChild(child);
+    } else if (typeof child === "string") {
+      el2.appendChild(document.createTextNode(child));
+    } else if (Array.isArray(child)) {
+      child.forEach(appendChild);
+    } else {
+      console.error("can not append child : ", child);
+    }
+  };
+  if (props) {
+    Object.keys(props).forEach((key) => {
+      el2.setAttribute(key, props[key]);
+    });
+  }
+  appendChild(children);
+  return el2;
+};
+
+// modules/src/util/bem.jsx
+var applyBem = (el2, { ...ctx }) => {
+  if (Array.isArray(el2)) {
+    el2.forEach((el3) => {
+      applyBem(el3, ctx);
+    });
+    return el2;
+  }
+  if (!(el2 instanceof HTMLElement)) {
+    return el2;
+  }
+  let { className } = el2;
+  if (className) {
+    el2.className = className.split(" ").map((className2) => {
+      if (className2.slice(0, 2) === "--") {
+        return ctx.element + className2;
+      }
+      if (className2[0] === "-") {
+        return ctx.block = ctx.element = ctx.block + className2;
+      }
+      if (className2[0] === "_") {
+        return ctx.element = ctx.element + (ctx.element === ctx.block ? "__" : "-") + className2.slice(1);
+      }
+      if (className2.slice(-1) === "-") {
+        return ctx.block = ctx.element = ctx.prefix + "-" + className2.slice(0, -1);
+      }
+      if (className2.slice(-1) === "_") {
+        return ctx.element = ctx.block + "__" + className2.slice(0, -1);
+      }
+      return className2;
+    }).join(" ");
+    if (el2.className === className) {
+      const matches = className.match(/\b(([a-z]+)\-[a-z]+(\-[a-z]+)*)(__[a-z]+(\-[a-z]+)*)?\b/);
+      if (!matches) {
+        return el2;
+      }
+      if (!matches[1].startsWith(ctx.prefix)) {
+        ctx.prefix = matches[2];
+      }
+      ctx.block = matches[1];
+      ctx.element = matches[0];
+    }
+  } else {
+    el2.className = ctx.element = ctx.element + (ctx.element === ctx.block ? "__" : "-") + el2.tagName.toLowerCase();
+  }
+  for (let i = 0; i < el2.children.length; i++) {
+    applyBem(el2.children[i], ctx);
+  }
+  return el2;
+};
+
+// modules/src/util/calc.jsx
+var srand = (w = 88675123) => {
+  var x = 123456789, y = 362436069, z = 521288629;
+  return function() {
+    let t;
+    t = x ^ x << 11;
+    [x, y, z] = [y, z, w];
+    w = w ^ w >>> 19 ^ (t ^ t >>> 8);
+    if (arguments.length === 0) {
+      return w;
+    }
+    if (arguments.length === 1) {
+      return w % (arguments[0] + 1);
+    }
+    const [min, max] = arguments;
+    return min + Math.abs(w) % (max + 1 - min);
+  };
+};
+
+// _b32qmul77:/Users/hatanokazuhiro/Documents/repos.nosync/mandai/mandai_cup/wp-content/plugins/catpow/elements/art-frame-cloud/element/style.css
+var style_default = "";
+
+// ../elements/art-frame-cloud/element/index.mjs.jsx
+var ArtFrameCloud = class extends HTMLElement {
+  static observedAttributes = ["r", "b", "h", "f", "seed", "direction"];
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+  connectedCallback() {
+    this.render();
+  }
+  attributeChangedCallback(name, oldValue, newValue) {
+    this.render();
+  }
+  render() {
+    const params = { r: 6, b: 30, h: 60, f: 50, seed: 16, direction: "both" };
+    for (const key of Object.keys(params)) {
+      if (this.hasAttribute(key)) {
+        switch (typeof params[key]) {
+          case "object":
+            params[key] = JSON.parse(this.getAttribute(key));
+            break;
+          case "boolean":
+            params[key] = !/0|false|no/i.test(this.getAttribute(key));
+            break;
+          case "number":
+            params[key] = parseInt(this.getAttribute(key));
+            break;
+          default:
+            params[key] = this.getAttribute(key);
+            break;
+        }
+      }
+    }
+    const style = el("style", {}, [style_default]);
+    const body = el("div", { class: "_body" }, [el("slot")]);
+    const resizeObserver = new ResizeObserver((entries) => {
+      const { width, height } = entries[0].contentRect;
+      const { r, b, h, f, seed, direction } = params;
+      let d = "";
+      const rnd = srand(seed);
+      const cr = h / 2 + width * width / h / 8;
+      const cRad = Math.asin(width / (2 * cr)) * 2;
+      const urc = 1 - b / 200;
+      const ur = r * urc;
+      const uRad = cRad / Math.ceil(cRad / (Math.asin(r / 2 / cr) * 2));
+      const maxURad = uRad + uRad * f / 200;
+      const maxR = cr * Math.sin(maxURad / 2) * 2;
+      const maxUr = maxR * urc;
+      const uah = maxUr - Math.sqrt(maxUr * maxUr - maxR / 2 * (maxR / 2));
+      if (direction === "both" || direction === "top") {
+        d += ` M 0 ${h + uah}`;
+        let pRad = -cRad / 2;
+        for (let rad = uRad - cRad / 2; rad < cRad / 2; rad += uRad) {
+          const tRad = rad - uRad * f / 200 + uRad * rnd(0, f) / 100;
+          const tr = cr * Math.sin(tRad - pRad);
+          pRad = tRad;
+          const ur3 = tr * (urc + rnd(0, f) / 200);
+          const x = cr * Math.sin(tRad) + width / 2;
+          const y = cr - cr * Math.cos(tRad) + uah;
+          d += ` A ${ur3} ${ur3} 0 0 1 ${x} ${y}`;
+        }
+        const ur2 = r * urc;
+        d += ` A ${ur2} ${ur2} 0 0 1 ${width} ${h + uah}`;
+      } else {
+        d += ` M 0 0 L ${width} 0`;
+      }
+      if (direction === "both" || direction === "bottom") {
+        d += ` L ${width} ${height - h - uah}`;
+        for (let rad = uRad - cRad / 2; rad < cRad / 2; rad += uRad) {
+          const ur2 = r * (urc + rnd(0, f) / 200);
+          const tRad = rad - uRad * f / 200 + uRad * rnd(0, f) / 100;
+          const x = width / 2 - cr * Math.sin(tRad);
+          const y = height - (cr - cr * Math.cos(tRad) + uah);
+          d += ` A ${ur2} ${ur2} 0 0 1 ${x} ${y}`;
+        }
+        d += ` A ${ur} ${ur} 0 0 1 0 ${height - h - uah} Z`;
+      } else {
+        d += ` L ${width} ${height} L 0 ${height} Z`;
+      }
+      body.style.setProperty("clip-path", `path("${d}")`);
+    });
+    resizeObserver.observe(body);
+    this.shadowRoot.replaceChildren(style, applyBem(el("div", { class: "cloud-" }, [body]), { prefix: "art-frame" }));
+  }
+};
+customElements.define("art-frame-cloud", ArtFrameCloud);
---
+++ elements/art-frame-cloud/element/index.mjs.jsx
@@ -0,0 +1,90 @@
+import { el, svgEl, bem, hfloor, hceil, hunit, applyBem, srand } from "catpow/util";
+import cssCode from "css:./style.css";
+
+class ArtFrameCloud extends HTMLElement {
+	static observedAttributes = ["r", "b", "h", "f", "seed", "direction"];
+	constructor() {
+		super();
+		this.attachShadow({ mode: "open" });
+	}
+	connectedCallback() {
+		this.render();
+	}
+	attributeChangedCallback(name, oldValue, newValue) {
+		this.render();
+	}
+	render() {
+		const params = { r: 6, b: 30, h: 60, f: 50, seed: 16, direction: "both" };
+		for (const key of Object.keys(params)) {
+			if (this.hasAttribute(key)) {
+				switch (typeof params[key]) {
+					case "object":
+						params[key] = JSON.parse(this.getAttribute(key));
+						break;
+					case "boolean":
+						params[key] = !/0|false|no/i.test(this.getAttribute(key));
+						break;
+					case "number":
+						params[key] = parseInt(this.getAttribute(key));
+						break;
+					default:
+						params[key] = this.getAttribute(key);
+						break;
+				}
+			}
+		}
+		const style = el("style", {}, [cssCode]);
+		const body = el("div", { class: "_body" }, [el("slot")]);
+		const resizeObserver = new ResizeObserver((entries) => {
+			const { width, height } = entries[0].contentRect;
+			const { r, b, h, f, seed, direction } = params;
+			let d = "";
+			const rnd = srand(seed);
+			const cr = h / 2 + (width * width) / h / 8;
+			const cRad = Math.asin(width / (2 * cr)) * 2;
+			const urc = 1 - b / 200;
+			const ur = r * urc;
+			const uRad = cRad / Math.ceil(cRad / (Math.asin(r / 2 / cr) * 2));
+			const maxURad = uRad + (uRad * f) / 200;
+			const maxR = cr * Math.sin(maxURad / 2) * 2;
+			const maxUr = maxR * urc;
+			const uah = maxUr - Math.sqrt(maxUr * maxUr - (maxR / 2) * (maxR / 2));
+
+			if (direction === "both" || direction === "top") {
+				d += ` M 0 ${h + uah}`;
+				let pRad = -cRad / 2;
+				for (let rad = uRad - cRad / 2; rad < cRad / 2; rad += uRad) {
+					const tRad = rad - (uRad * f) / 200 + (uRad * rnd(0, f)) / 100;
+					const tr = cr * Math.sin(tRad - pRad);
+					pRad = tRad;
+					const ur = tr * (urc + rnd(0, f) / 200);
+					const x = cr * Math.sin(tRad) + width / 2;
+					const y = cr - cr * Math.cos(tRad) + uah;
+					d += ` A ${ur} ${ur} 0 0 1 ${x} ${y}`;
+				}
+				const ur = r * urc;
+				d += ` A ${ur} ${ur} 0 0 1 ${width} ${h + uah}`;
+			} else {
+				d += ` M 0 0 L ${width} 0`;
+			}
+			if (direction === "both" || direction === "bottom") {
+				d += ` L ${width} ${height - h - uah}`;
+				for (let rad = uRad - cRad / 2; rad < cRad / 2; rad += uRad) {
+					const ur = r * (urc + rnd(0, f) / 200);
+					const tRad = rad - (uRad * f) / 200 + (uRad * rnd(0, f)) / 100;
+					const x = width / 2 - cr * Math.sin(tRad);
+					const y = height - (cr - cr * Math.cos(tRad) + uah);
+					d += ` A ${ur} ${ur} 0 0 1 ${x} ${y}`;
+				}
+				d += ` A ${ur} ${ur} 0 0 1 0 ${height - h - uah} Z`;
+			} else {
+				d += ` L ${width} ${height} L 0 ${height} Z`;
+			}
+			body.style.setProperty("clip-path", `path("${d}")`);
+		});
+		resizeObserver.observe(body);
+		this.shadowRoot.replaceChildren(style, applyBem(el("div", { class: "cloud-" }, [body]), { prefix: "art-frame" }));
+	}
+}
+
+customElements.define("art-frame-cloud", ArtFrameCloud);
