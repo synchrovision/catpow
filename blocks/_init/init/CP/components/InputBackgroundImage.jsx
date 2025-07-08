06c8ef842de005fb74e77da186c00364008d8cf7
synchrovision <hatano@catpow.info>
2025-07-08 17:21:35 +0900

    feat 👍 add h param to air background image

--- blocks/_init/init/CP/components/InputBackgroundImage.jsx
+++ blocks/_init/init/CP/components/InputBackgroundImage.jsx
@@ -1,4 +1,5 @@
 ﻿import { translateColor } from "catpow/scssc";
+import { pfloor } from "catpow/util";
 const { useState, useMemo } = wp.element;
 const { __ } = wp.i18n;
 
@@ -136,13 +137,17 @@ const BackgroundImageDataGenerators = {
 		params: {
 			...baseGradientParams,
 			w: { minimum: 5, maximum: 200 },
+			h: { minimum: 5, maximum: 200 },
 			alpha: { minimum: 0, maximum: 100, multipleOf: 10 },
 		},
 		getData(params = {}) {
-			const { w = 50, alpha = 25 } = params;
-			const gradient1 = `radial-gradient(${100 + w}% 100% at 40% 0%,rgba(255,255,255,0),rgba(255,255,255,0) 60%,rgba(255,255,255,${alpha / 100}) 60%,rgba(255,255,255,0))`;
-			const gradient2 = `radial-gradient(${120 + w * 2}% 100% at 25% 100%,rgba(255,255,255,0),rgba(255,255,255,${alpha / 100}) 60%,rgba(255,255,255,0) 60%,rgba(255,255,255,0))`;
-			const gradient3 = `radial-gradient(${120 + w * 2}% 100% at 100% 100%,rgba(255,255,255,0),rgba(255,255,255,${alpha / 100}) 70%,rgba(255,255,255,0) 70%,rgba(255,255,255,0))`;
+			const { w = 50, h = 50, alpha = 25 } = params;
+			const p1 = 50 + pfloor(h / 5, 1),
+				p2 = 50 + pfloor(h / 10, 1),
+				p3 = 50 + pfloor(h / 3, 1);
+			const gradient1 = `radial-gradient(${80 + w / 2}% 100% at 40% 0%,rgba(255,255,255,0),rgba(255,255,255,0) ${p1}%,rgba(255,255,255,${alpha / 100}) ${p1}%,rgba(255,255,255,0))`;
+			const gradient2 = `radial-gradient(${100 + w}% 100% at 25% 100%,rgba(255,255,255,0),rgba(255,255,255,${alpha / 100}) ${p2}%,rgba(255,255,255,0) ${p2}%,rgba(255,255,255,0))`;
+			const gradient3 = `radial-gradient(${100 + w}% 100% at 100% 100%,rgba(255,255,255,0),rgba(255,255,255,${alpha / 100}) ${p3}%,rgba(255,255,255,0) ${p3}%,rgba(255,255,255,0))`;
 			const gradient4 = getBaseGradientCode(params);
 			return {
 				image: [gradient1, gradient2, gradient3, gradient4],
