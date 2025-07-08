3f724acce9079829528b5e849aebd59d5ab68423
synchrovision <hatano@catpow.info>
2025-07-08 14:01:41 +0900

    chore 🆙 lint

--- components/Console/component.js
+++ components/Console/component.js
@@ -4,38 +4,81 @@
     const { className = "cp-console", path } = props;
     const el = wp.element.createElement;
     const { useState, useCallback, useEffect, useRef, useMemo, useReducer } = wp.element;
-    const ControlItem = useCallback((props2) => {
-      const { state: state2, dispatch: dispatch2 } = props2;
-      const { SelectBox, CheckBoxes, RadioButtons, Buttons, Button, ModalForm } = Catpow;
-      const { name, type, label, desc } = props2;
-      const inputTypes = useMemo(() => ({
-        text: (props3) => /* @__PURE__ */ wp.element.createElement("input", { type: "text", onChange: (e) => {
-          dispatch2({ type: "setData", name, value: e.currentTarget.value });
-        }, value: state2.data[name] }),
-        select: (props3) => /* @__PURE__ */ wp.element.createElement(SelectBox, { onChange: (value) => {
-          dispatch2({ type: "setData", name, value });
-        }, options: props3.options, value: state2.data[name] }),
-        checkbox: (props3) => /* @__PURE__ */ wp.element.createElement(CheckBoxes, { onChange: (value) => {
-          dispatch2({ type: "setData", name, value });
-        }, options: props3.options, value: state2.data[name] }),
-        radio: (props3) => /* @__PURE__ */ wp.element.createElement(RadioButtons, { onChange: (value) => {
-          dispatch2({ type: "setData", name, value });
-        }, options: props3.options, value: state2.data[name] }),
-        submit: (props3) => /* @__PURE__ */ wp.element.createElement(Buttons, { onClick: (action) => {
-          submit(action, state2.data);
-        }, options: props3.options })
-      }), []);
-      return /* @__PURE__ */ wp.element.createElement("div", { className: className + "-controls-item" }, label && /* @__PURE__ */ wp.element.createElement("div", { className: "label" }, label), desc && /* @__PURE__ */ wp.element.createElement("div", { className: "desc" }, desc), /* @__PURE__ */ wp.element.createElement("div", { className: "body" }, el(inputTypes[type], props2)));
-    }, [className]);
+    const ControlItem = useCallback(
+      (props2) => {
+        const { state: state2, dispatch: dispatch2 } = props2;
+        const { SelectBox, CheckBoxes, RadioButtons, Buttons, Button, ModalForm } = Catpow;
+        const { name, type, label, desc } = props2;
+        const inputTypes = useMemo(
+          () => ({
+            text: (props3) => /* @__PURE__ */ wp.element.createElement(
+              "input",
+              {
+                type: "text",
+                onChange: (e) => {
+                  dispatch2({ type: "setData", name, value: e.currentTarget.value });
+                },
+                value: state2.data[name]
+              }
+            ),
+            select: (props3) => /* @__PURE__ */ wp.element.createElement(
+              SelectBox,
+              {
+                onChange: (value) => {
+                  dispatch2({ type: "setData", name, value });
+                },
+                options: props3.options,
+                value: state2.data[name]
+              }
+            ),
+            checkbox: (props3) => /* @__PURE__ */ wp.element.createElement(
+              CheckBoxes,
+              {
+                onChange: (value) => {
+                  dispatch2({ type: "setData", name, value });
+                },
+                options: props3.options,
+                value: state2.data[name]
+              }
+            ),
+            radio: (props3) => /* @__PURE__ */ wp.element.createElement(
+              RadioButtons,
+              {
+                onChange: (value) => {
+                  dispatch2({ type: "setData", name, value });
+                },
+                options: props3.options,
+                value: state2.data[name]
+              }
+            ),
+            submit: (props3) => /* @__PURE__ */ wp.element.createElement(
+              Buttons,
+              {
+                onClick: (action) => {
+                  submit(action, state2.data);
+                },
+                options: props3.options
+              }
+            )
+          }),
+          []
+        );
+        return /* @__PURE__ */ wp.element.createElement("div", { className: className + "-controls-item" }, label && /* @__PURE__ */ wp.element.createElement("div", { className: "label" }, label), desc && /* @__PURE__ */ wp.element.createElement("div", { className: "desc" }, desc), /* @__PURE__ */ wp.element.createElement("div", { className: "body" }, el(inputTypes[type], props2)));
+      },
+      [className]
+    );
     const ResultItem = useCallback((props2) => {
       const { type = "log", text } = props2;
-      const resultTypes = useMemo(() => ({
-        success: (props3) => /* @__PURE__ */ wp.element.createElement("div", { className: "text -success" }, props3.text),
-        error: (props3) => /* @__PURE__ */ wp.element.createElement("div", { className: "text -error" }, props3.text),
-        warn: (props3) => /* @__PURE__ */ wp.element.createElement("div", { className: "text -warn" }, props3.text),
-        notice: (props3) => /* @__PURE__ */ wp.element.createElement("div", { className: "text -notice" }, props3.text),
-        log: (props3) => /* @__PURE__ */ wp.element.createElement("div", { className: "text -log" }, props3.text)
-      }), []);
+      const resultTypes = useMemo(
+        () => ({
+          success: (props3) => /* @__PURE__ */ wp.element.createElement("div", { className: "text -success" }, props3.text),
+          error: (props3) => /* @__PURE__ */ wp.element.createElement("div", { className: "text -error" }, props3.text),
+          warn: (props3) => /* @__PURE__ */ wp.element.createElement("div", { className: "text -warn" }, props3.text),
+          notice: (props3) => /* @__PURE__ */ wp.element.createElement("div", { className: "text -notice" }, props3.text),
+          log: (props3) => /* @__PURE__ */ wp.element.createElement("div", { className: "text -log" }, props3.text)
+        }),
+        []
+      );
       return el(resultTypes[type], props2);
     }, []);
     const reducer = useCallback((state2, action) => {
--- components/Console/component.jsx
+++ components/Console/component.jsx
@@ -1,90 +1,132 @@
 ﻿/**
-* APIへ入力値を送信して結果を表示する
-*/
-Catpow.Console=(props)=>{
-	const {className='cp-console',path}=props;
-	const el=wp.element.createElement;
-	const {useState,useCallback,useEffect,useRef,useMemo,useReducer}=wp.element;
-	
-	const ControlItem=useCallback((props)=>{
-		const {state,dispatch}=props;
-		const {SelectBox,CheckBoxes,RadioButtons,Buttons,Button,ModalForm}=Catpow;
-		const {name,type,label,desc}=props;
-		const inputTypes=useMemo(()=>({
-			text:(props)=>(
-				<input type="text" onChange={(e)=>{dispatch({type:'setData',name,value:e.currentTarget.value});}} value={state.data[name]}/>
-			),
-			select:(props)=>(
-				<SelectBox onChange={(value)=>{dispatch({type:'setData',name,value});}} options={props.options} value={state.data[name]}/>
-			),
-			checkbox:(props)=>(
-				<CheckBoxes onChange={(value)=>{dispatch({type:'setData',name,value});}} options={props.options} value={state.data[name]}/>
-			),
-			radio:(props)=>(
-				<RadioButtons onChange={(value)=>{dispatch({type:'setData',name,value});}} options={props.options} value={state.data[name]}/>
-			),
-			submit:(props)=>(
-				<Buttons onClick={(action)=>{submit(action,state.data)}} options={props.options}/>
-			)
-		}),[]);
-		return (
-			<div className={className+'-controls-item'}>
-				{label && <div className="label">{label}</div>}
-				{desc && <div className="desc">{desc}</div>}
-				<div className="body">{el(inputTypes[type],props)}</div>
-			</div>
+ * APIへ入力値を送信して結果を表示する
+ */
+Catpow.Console = (props) => {
+	const { className = "cp-console", path } = props;
+	const el = wp.element.createElement;
+	const { useState, useCallback, useEffect, useRef, useMemo, useReducer } = wp.element;
+
+	const ControlItem = useCallback(
+		(props) => {
+			const { state, dispatch } = props;
+			const { SelectBox, CheckBoxes, RadioButtons, Buttons, Button, ModalForm } = Catpow;
+			const { name, type, label, desc } = props;
+			const inputTypes = useMemo(
+				() => ({
+					text: (props) => (
+						<input
+							type="text"
+							onChange={(e) => {
+								dispatch({ type: "setData", name, value: e.currentTarget.value });
+							}}
+							value={state.data[name]}
+						/>
+					),
+					select: (props) => (
+						<SelectBox
+							onChange={(value) => {
+								dispatch({ type: "setData", name, value });
+							}}
+							options={props.options}
+							value={state.data[name]}
+						/>
+					),
+					checkbox: (props) => (
+						<CheckBoxes
+							onChange={(value) => {
+								dispatch({ type: "setData", name, value });
+							}}
+							options={props.options}
+							value={state.data[name]}
+						/>
+					),
+					radio: (props) => (
+						<RadioButtons
+							onChange={(value) => {
+								dispatch({ type: "setData", name, value });
+							}}
+							options={props.options}
+							value={state.data[name]}
+						/>
+					),
+					submit: (props) => (
+						<Buttons
+							onClick={(action) => {
+								submit(action, state.data);
+							}}
+							options={props.options}
+						/>
+					),
+				}),
+				[]
+			);
+			return (
+				<div className={className + "-controls-item"}>
+					{label && <div className="label">{label}</div>}
+					{desc && <div className="desc">{desc}</div>}
+					<div className="body">{el(inputTypes[type], props)}</div>
+				</div>
+			);
+		},
+		[className]
+	);
+	const ResultItem = useCallback((props) => {
+		const { type = "log", text } = props;
+		const resultTypes = useMemo(
+			() => ({
+				success: (props) => <div className="text -success">{props.text}</div>,
+				error: (props) => <div className="text -error">{props.text}</div>,
+				warn: (props) => <div className="text -warn">{props.text}</div>,
+				notice: (props) => <div className="text -notice">{props.text}</div>,
+				log: (props) => <div className="text -log">{props.text}</div>,
+			}),
+			[]
 		);
-	},[className]);
-	const ResultItem=useCallback((props)=>{
-		const {type='log',text}=props;
-		const resultTypes=useMemo(()=>({
-			success:(props)=>(<div className="text -success">{props.text}</div>),
-			error:(props)=>(<div className="text -error">{props.text}</div>),
-			warn:(props)=>(<div className="text -warn">{props.text}</div>),
-			notice:(props)=>(<div className="text -notice">{props.text}</div>),
-			log:(props)=>(<div className="text -log">{props.text}</div>),
-		}),[]);
-		return el(resultTypes[type],props);
-	},[]);
-	const reducer=useCallback((state,action)=>{
-		switch(action.type){
-			case 'setData':{
-				state.data[action.name]=action.value;
-				return {...state};
+		return el(resultTypes[type], props);
+	}, []);
+	const reducer = useCallback((state, action) => {
+		switch (action.type) {
+			case "setData": {
+				state.data[action.name] = action.value;
+				return { ...state };
 			}
-			case 'setResults':{
-				state.results=action.results;
-				return {...state};
+			case "setResults": {
+				state.results = action.results;
+				return { ...state };
 			}
 		}
 		return state;
-	},[]);
-	const [state,dispatch]=useReducer(reducer,{
-		data:{},
-		results:[]
+	}, []);
+	const [state, dispatch] = useReducer(reducer, {
+		data: {},
+		results: [],
 	});
-	const submit=useCallback((action,data)=>{
-		wp.apiFetch({path:`/cp/v1/${path}/${action}`,method:'POST',data}).then((res)=>{
-			const {results,resubmit=false}=res;
-			dispatch({type:'setResults',results});
-			if(resubmit){submit(action,resubmit);}
-		}).catch((e)=>{
-			dispatch({type:'setResults',results:[{type:'error',text:e.message}]});
-		});
-	},[]);
-	
-	const flagsToWords=useCallback((flags)=>{
-		if(undefined === flags){return '';}
-		return Object.keys(flags).filter((word)=>flags[word]).join(' ');
-	},[]);
+	const submit = useCallback((action, data) => {
+		wp.apiFetch({ path: `/cp/v1/${path}/${action}`, method: "POST", data })
+			.then((res) => {
+				const { results, resubmit = false } = res;
+				dispatch({ type: "setResults", results });
+				if (resubmit) {
+					submit(action, resubmit);
+				}
+			})
+			.catch((e) => {
+				dispatch({ type: "setResults", results: [{ type: "error", text: e.message }] });
+			});
+	}, []);
+
+	const flagsToWords = useCallback((flags) => {
+		if (undefined === flags) {
+			return "";
+		}
+		return Object.keys(flags)
+			.filter((word) => flags[word])
+			.join(" ");
+	}, []);
 	return (
 		<div className={className}>
-			<div className={className+'-controls'}>
-				{props.controls.map((itemProps,key)=>el(ControlItem,{...itemProps,state,dispatch,key}))}
-			</div>
-			<div className={className+'-results'}>
-				{state.results.map((itemProps,key)=>el(ResultItem,{...itemProps,key}))}
-			</div>
+			<div className={className + "-controls"}>{props.controls.map((itemProps, key) => el(ControlItem, { ...itemProps, state, dispatch, key }))}</div>
+			<div className={className + "-results"}>{state.results.map((itemProps, key) => el(ResultItem, { ...itemProps, key }))}</div>
 		</div>
 	);
-}
+};
