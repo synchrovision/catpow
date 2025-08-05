1b58fbea67ab488dcdf45473b0e7f5dfa4498dce
synchrovision <hatano@catpow.info>
2025-08-05 14:23:23 +0900

    feat 👍 update finder focused component

--- components/Finder/Focused/component.js
+++ components/Finder/Focused/component.js
@@ -1,21 +1,125 @@
 (() => {
+  var __getOwnPropNames = Object.getOwnPropertyNames;
+  var __esm = (fn, res) => function __init() {
+    return fn && (res = (0, fn[__getOwnPropNames(fn)[0]])(fn = 0)), res;
+  };
+
+  // react-global:react
+  var react_default, useState, useEffect, useLayoutEffect, useRef, forwardRef, useMemo, useCallback, createContext, useContext, useReducer, createElement, cloneElement, isValidElement, Fragment;
+  var init_react = __esm({
+    "react-global:react"() {
+      react_default = window.wp.element;
+      useState = wp.element.useState;
+      useEffect = wp.element.useEffect;
+      useLayoutEffect = wp.element.useLayoutEffect;
+      useRef = wp.element.useRef;
+      forwardRef = wp.element.forwardRef;
+      useMemo = wp.element.useMemo;
+      useCallback = wp.element.useCallback;
+      createContext = wp.element.createContext;
+      useContext = wp.element.useContext;
+      useReducer = wp.element.useReducer;
+      createElement = wp.element.createElement;
+      cloneElement = wp.element.cloneElement;
+      isValidElement = wp.element.isValidElement;
+      Fragment = wp.element.Fragment;
+    }
+  });
+
+  // modules/src/util/string.jsx
+  var flagsToClassNames = (flags) => flags && Object.keys(flags).filter((f) => flags[f]).map(camelToKebab).join(" ");
+  var camelToKebab = (str) => str.replace(/(\w)([A-Z])/g, "$1-$2").toLowerCase();
+
+  // modules/src/component/Bem.jsx
+  init_react();
+  var applyBem = (component, { ...ctx }) => {
+    if (Array.isArray(component)) {
+      component.forEach((child) => {
+        applyBem(child, ctx);
+      });
+      return;
+    }
+    if (component?.props == null) {
+      return;
+    }
+    if (component.type == react_default.Fragment) {
+      applyBem(component.props.children, ctx);
+      return;
+    }
+    const {
+      props: { className, children }
+    } = component;
+    if (className) {
+      component.props.className = className.split(" ").map((className2) => {
+        if (className2.slice(0, 2) === "--") {
+          return ctx.element + className2;
+        }
+        if (className2[0] === "-") {
+          return ctx.block = ctx.element = ctx.block + className2;
+        }
+        if (className2[0] === "_") {
+          return ctx.element = ctx.element + (ctx.element === ctx.block ? "__" : "-") + className2.slice(1);
+        }
+        if (className2.slice(-1) === "-") {
+          return ctx.block = ctx.element = ctx.prefix + "-" + className2.slice(0, -1);
+        }
+        if (className2.slice(-1) === "_") {
+          return ctx.element = ctx.block + "__" + className2.slice(0, -1);
+        }
+        return className2;
+      }).join(" ");
+      if (component.props.className === className) {
+        const matches = className.match(/\b(([a-z]+)\-[a-z]+(\-[a-z]+)*)(__[a-z]+(\-[a-z]+)*)?\b/);
+        if (!matches) {
+          return;
+        }
+        if (!matches[1].startsWith(ctx.prefix)) {
+          ctx.prefix = matches[2];
+        }
+        ctx.block = matches[1];
+        ctx.element = matches[0];
+      }
+    } else if (typeof component.type === "string") {
+      component.props.className = ctx.element = ctx.element + (ctx.element === ctx.block ? "__" : "-") + component.type;
+    } else {
+      return;
+    }
+    if (children == null) {
+      return;
+    }
+    if (Array.isArray(children)) {
+      children.forEach((child) => {
+        applyBem(child, ctx);
+      });
+    } else {
+      applyBem(children, ctx);
+    }
+  };
+  var Bem = ({ prefix = "cp", block, element, children }) => {
+    const ctx = { prefix, block, element };
+    applyBem(children, ctx);
+    return /* @__PURE__ */ wp.element.createElement(wp.element.Fragment, null, children);
+  };
+
   // ../components/Finder/Focused/component.jsx
   Catpow.Finder.Focused = (props) => {
-    const { useState, useCallback, useContext } = wp.element;
+    const { useState: useState2, useCallback: useCallback2, useContext: useContext2 } = wp.element;
     const { __, sprintf } = wp.i18n;
-    const { state, dispatch, info } = useContext(Catpow.FinderContext);
+    const { state, dispatch, info } = useContext2(Catpow.FinderContext);
     const { roleGroups } = info;
     const { cols } = state.index;
-    const flagsToWords = useCallback((classes) => Object.keys(classes).filter((key) => classes[key]).join(" "), []);
-    const hasRoleGroup = useCallback((group) => {
-      return !roleGroups[group].every((role) => !state.colsByRole[role] || !state.colsByRole[role].length);
-    }, [state.colsByRole, roleGroups]);
-    const ucfirst = useCallback((str) => str.charAt(0).toUpperCase() + str.slice(1), []);
-    const flags = { FinderFocused: true };
+    const hasRoleGroup = useCallback2(
+      (group) => {
+        return !roleGroups[group].every((role) => !state.colsByRole[role] || !state.colsByRole[role].length);
+      },
+      [state.colsByRole, roleGroups]
+    );
+    const ucfirst = useCallback2((str) => str.charAt(0).toUpperCase() + str.slice(1), []);
+    const flags = { cpFinderFocused: true };
     Object.keys(roleGroups).map((group) => {
       flags["has" + ucfirst(group)] = hasRoleGroup(group);
     });
-    return /* @__PURE__ */ wp.element.createElement("div", { className: flagsToWords(flags) }, /* @__PURE__ */ wp.element.createElement("table", { className: "items" }, Object.keys(roleGroups).map((group) => {
+    return /* @__PURE__ */ wp.element.createElement(Bem, { prefix: "cp-finder" }, /* @__PURE__ */ wp.element.createElement("div", { className: flagsToClassNames(flags) }, /* @__PURE__ */ wp.element.createElement("table", { className: "_items" }, Object.keys(roleGroups).map((group) => {
       if (!hasRoleGroup(group)) {
         return false;
       }
@@ -23,8 +127,8 @@
         if (!state.colsByRole[role] || !state.colsByRole[role].length) {
           return false;
         }
-        return state.colsByRole[role].map((col) => /* @__PURE__ */ wp.element.createElement("tr", { className: "item" }, /* @__PURE__ */ wp.element.createElement("th", { className: "label" }, col.label), /* @__PURE__ */ wp.element.createElement("td", { className: "value" }, /* @__PURE__ */ wp.element.createElement(Catpow.Output, { conf: col, ...state.focused[col.name] }))));
+        return state.colsByRole[role].map((col) => /* @__PURE__ */ wp.element.createElement("tr", { className: "_item" }, /* @__PURE__ */ wp.element.createElement("th", { className: "_label" }, col.label), /* @__PURE__ */ wp.element.createElement("td", { className: "_value" }, /* @__PURE__ */ wp.element.createElement(Catpow.Output, { conf: col, ...state.focused[col.name] }))));
       });
-    })));
+    }))));
   };
 })();
--- components/Finder/Focused/component.jsx
+++ components/Finder/Focused/component.jsx
@@ -1,37 +1,50 @@
-﻿Catpow.Finder.Focused=(props)=>{
-	const {useState,useCallback,useContext}=wp.element;
-	const {__,sprintf}=wp.i18n;
-	const {state,dispatch,info}=useContext(Catpow.FinderContext);
-	const {roleGroups}=info;
-	const {cols}=state.index;
-	
-	const flagsToWords=useCallback((classes)=>Object.keys(classes).filter((key)=>classes[key]).join(' '),[]);
-	const hasRoleGroup=useCallback((group)=>{
-		return !roleGroups[group].every((role)=>!state.colsByRole[role] || !state.colsByRole[role].length);
-	},[state.colsByRole,roleGroups]);
-	const ucfirst=useCallback((str)=>str.charAt(0).toUpperCase()+str.slice(1),[]);
-	
-	const flags={FinderFocused:true};
-	Object.keys(roleGroups).map((group)=>{
-		flags['has'+ucfirst(group)]=hasRoleGroup(group);
-	});
-	
-	return (
-		<div className={flagsToWords(flags)}>
-			<table className="items">
-			{Object.keys(roleGroups).map((group)=>{
-				if(!hasRoleGroup(group)){return false;}
-				return roleGroups[group].map((role)=>{
-					if(!state.colsByRole[role] || !state.colsByRole[role].length){return false;}
-					return state.colsByRole[role].map((col)=>(
-						<tr className="item">
-							<th className="label">{col.label}</th>
-							<td className="value"><Catpow.Output conf={col} {...state.focused[col.name]}/></td>
-						</tr>
-					))
-				});
-			})}
-			</table>
-		</div>
+﻿import { Bem } from "catpow/component";
+import { flagsToClassNames } from "catpow/util";
+
+Catpow.Finder.Focused = (props) => {
+	const { useState, useCallback, useContext } = wp.element;
+	const { __, sprintf } = wp.i18n;
+	const { state, dispatch, info } = useContext(Catpow.FinderContext);
+	const { roleGroups } = info;
+	const { cols } = state.index;
+
+	const hasRoleGroup = useCallback(
+		(group) => {
+			return !roleGroups[group].every((role) => !state.colsByRole[role] || !state.colsByRole[role].length);
+		},
+		[state.colsByRole, roleGroups]
 	);
-}
+	const ucfirst = useCallback((str) => str.charAt(0).toUpperCase() + str.slice(1), []);
+
+	const flags = { cpFinderFocused: true };
+	Object.keys(roleGroups).map((group) => {
+		flags["has" + ucfirst(group)] = hasRoleGroup(group);
+	});
+
+	return (
+		<Bem prefix="cp-finder">
+			<div className={flagsToClassNames(flags)}>
+				<table className="_items">
+					{Object.keys(roleGroups).map((group) => {
+						if (!hasRoleGroup(group)) {
+							return false;
+						}
+						return roleGroups[group].map((role) => {
+							if (!state.colsByRole[role] || !state.colsByRole[role].length) {
+								return false;
+							}
+							return state.colsByRole[role].map((col) => (
+								<tr className="_item">
+									<th className="_label">{col.label}</th>
+									<td className="_value">
+										<Catpow.Output conf={col} {...state.focused[col.name]} />
+									</td>
+								</tr>
+							));
+						});
+					})}
+				</table>
+			</div>
+		</Bem>
+	);
+};
