b01519ef79eaff32b6d3a56a52d61701d056955b
synchrovision <hatano@catpow.info>
2025-08-05 11:21:47 +0900

    feat 👍 update AlignClassToolbar component

--- blocks/_init/init/CP/components/AlignClassToolbar.jsx
+++ blocks/_init/init/CP/components/AlignClassToolbar.jsx
@@ -1,12 +1,29 @@
-﻿export const AlignClassToolbar = (props) => {
+﻿const { useMemo } = wp.element;
+
+export const AlignClassToolbar = (props) => {
 	const { BlockAlignmentToolbar } = wp.blockEditor;
-	const aligns = ["left", "center", "right"];
+	const { aligns = { isAlignLeft: "left", isAlignCenter: "center", isAlignRight: "right" } } = props;
+
+	const options = useMemo(() => {
+		const options = {};
+		if (Array.isArray(aligns)) {
+			for (const val of aligns) {
+				options[val] = val;
+			}
+		} else {
+			for (const [key, val] of Object.entries(aligns)) {
+				options[val] = key;
+			}
+		}
+		return options;
+	}, [aligns]);
+
 	return (
 		<BlockAlignmentToolbar
-			value={CP.getSelectiveClass(props, aligns)}
-			controls={props.aligns || aligns}
+			value={CP.getSelectiveClassLabel(props, aligns)}
+			controls={Object.keys(options)}
 			onChange={(align) => {
-				CP.switchSelectiveClass(props, aligns, align, props.key);
+				CP.switchSelectiveClass(props, Object.values(options), options[align], props.key);
 			}}
 		/>
 	);
