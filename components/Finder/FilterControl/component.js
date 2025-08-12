be422833113468b1649e4b76333eb17818896081
synchrovision <hatano@catpow.info>
2025-08-12 17:07:10 +0900

    edit 📝 update Finder/FilterControl component

--- components/Finder/FilterControl/component.js
+++ components/Finder/FilterControl/component.js
@@ -9,17 +9,9 @@
     return /* @__PURE__ */ wp.element.createElement("div", { className: "cp-finder-control cp-finder-filtercontrol" }, /* @__PURE__ */ wp.element.createElement("ul", { className: "items" }, /* @__PURE__ */ wp.element.createElement("li", { className: "item" + (Object.keys(state.query).length ? " active" : "") }, /* @__PURE__ */ wp.element.createElement("div", { className: "icon dashicons dashicons-filter", onClick: () => setOpen(!open) }), /* @__PURE__ */ wp.element.createElement(Catpow.Popover, { open }, /* @__PURE__ */ wp.element.createElement("table", { className: "cp-finder-filtercontrol__table" }, /* @__PURE__ */ wp.element.createElement("tbody", { className: "cp-finder-filtercontrol__table-tbody" }, Object.keys(state.index.cols).map((key) => {
       const col = state.index.cols[key];
       if (col.role === "group") {
-        return /* @__PURE__ */ wp.element.createElement("tr", { className: "row", key }, /* @__PURE__ */ wp.element.createElement("th", { className: "label" }, col.label), /* @__PURE__ */ wp.element.createElement("td", { className: "inputs" }, Object.keys(col.dict).map((val) => {
+        return /* @__PURE__ */ wp.element.createElement("tr", { className: "cp-finder-filtercontrol__table-tbody-tr", key }, /* @__PURE__ */ wp.element.createElement("th", { className: "cp-finder-filtercontrol__table-tbody-tr-th" }, col.label), /* @__PURE__ */ wp.element.createElement("td", { className: "cp-finder-filtercontrol__table-tbody-tr-td" }, Object.keys(col.dict).map((val) => {
           const isActive = state.query[key] && state.query[key].indexOf(val) !== -1;
-          return /* @__PURE__ */ wp.element.createElement(
-            Catpow.CheckBox,
-            {
-              label: col.dict[val],
-              selected: isActive,
-              onChange: (selected) => dispatch({ type: (selected ? "add" : "remove") + "Query", key, val }),
-              key: val
-            }
-          );
+          return /* @__PURE__ */ wp.element.createElement(Catpow.CheckBox, { label: col.dict[val], selected: isActive, onChange: (selected) => dispatch({ type: (selected ? "add" : "remove") + "Query", key, val }), key: val });
         })));
       }
       return false;
--- components/Finder/FilterControl/component.jsx
+++ components/Finder/FilterControl/component.jsx
@@ -1,46 +1,41 @@
 ﻿/**
-* Finderの表示項目を絞り込みするコンポーネント
-*/
+ * Finderの表示項目を絞り込みするコンポーネント
+ */
 
-Catpow.Finder.FilterControl=(props)=>{
-	const {useState,useContext}=wp.element;
-	const {__}=wp.i18n;
-	const {state,dispatch}=useContext(Catpow.FinderContext);
-	const [open,setOpen]=useState(false);
-	const {cols}=state.index;
-	
+Catpow.Finder.FilterControl = (props) => {
+	const { useState, useContext } = wp.element;
+	const { __ } = wp.i18n;
+	const { state, dispatch } = useContext(Catpow.FinderContext);
+	const [open, setOpen] = useState(false);
+	const { cols } = state.index;
+
 	return (
 		<div className="cp-finder-control cp-finder-filtercontrol">
 			<ul className="items">
-				<li className={'item'+(Object.keys(state.query).length?' active':'')}>
-					<div className={"icon dashicons dashicons-filter"} onClick={()=>setOpen(!open)}></div>
-					<Catpow.Popover　open={open}>
+				<li className={"item" + (Object.keys(state.query).length ? " active" : "")}>
+					<div className={"icon dashicons dashicons-filter"} onClick={() => setOpen(!open)}></div>
+					<Catpow.Popover open={open}>
 						<table className="cp-finder-filtercontrol__table">
 							<tbody className="cp-finder-filtercontrol__table-tbody">
-							{Object.keys(state.index.cols).map((key)=>{
-								const col=state.index.cols[key];
-								if(col.role==='group'){
-									return (
-										<tr className="row" key={key}>
-											<th className="label">{col.label}</th>
-											<td className="inputs">
-												{Object.keys(col.dict).map((val)=>{
-													const isActive=state.query[key] && state.query[key].indexOf(val)!==-1;
-													return (
-														<Catpow.CheckBox
-															label={col.dict[val]}
-															selected={isActive}
-															onChange={(selected)=>dispatch({type:(selected?'add':'remove')+'Query',key,val})}
-															key={val}
-														/>
-													);
-												})}
-											</td>
-										</tr>
-									);
-								}
-								return false;
-							})}
+								{Object.keys(state.index.cols).map((key) => {
+									const col = state.index.cols[key];
+									if (col.role === "group") {
+										return (
+											<tr className="cp-finder-filtercontrol__table-tbody-tr" key={key}>
+												<th className="cp-finder-filtercontrol__table-tbody-tr-th">{col.label}</th>
+												<td className="cp-finder-filtercontrol__table-tbody-tr-td">
+													{Object.keys(col.dict).map((val) => {
+														const isActive = state.query[key] && state.query[key].indexOf(val) !== -1;
+														return (
+															<Catpow.CheckBox label={col.dict[val]} selected={isActive} onChange={(selected) => dispatch({ type: (selected ? "add" : "remove") + "Query", key, val })} key={val} />
+														);
+													})}
+												</td>
+											</tr>
+										);
+									}
+									return false;
+								})}
 							</tbody>
 						</table>
 					</Catpow.Popover>
@@ -48,4 +43,4 @@ Catpow.Finder.FilterControl=(props)=>{
 			</ul>
 		</div>
 	);
-}
+};
