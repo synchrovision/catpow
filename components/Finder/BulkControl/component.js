a0d932608a9b0045a85369a36835abff741a7489
synchrovision <hatano@catpow.info>
2025-08-13 18:48:50 +0900

    feat 👍 update Finder/BulkControl component

--- components/Finder/BulkControl/component.js
+++ components/Finder/BulkControl/component.js
@@ -25,80 +25,71 @@
       });
       return options2;
     }, [state.bulk]);
-    const exec_bulk = useCallback(async (action) => {
-      try {
-        const conf = state.bulk[action];
-        const vals = await show_modal(conf);
-        wp.apiFetch({
-          path: state.apiPath + "/bulk/exec/" + action,
-          method: "POST",
-          data: { rows: state.selectedRows.map((row) => row._id), vals }
-        }).then((res) => {
-          if (callback) {
-            callback({ action, res, state, dispatch });
-          }
-          if (res.remove) {
-            dispatch({ type: "removeRows", rows: state.selectedRows });
-          }
-          if (res.update) {
-            dispatch({ type: "updateRows", rows: res.update });
-          }
-          if (res.message) {
-            dispatch({ type: "showMessage", ...res.message });
-          }
-          if (res.download) {
-            Catpow.util.download(res.download.data, res.download.name || state.name + ".csv", "text/csv");
-          }
-        });
-      } catch (err) {
-        return false;
-      }
-    }, [state, dispatch]);
-    const show_modal = useCallback((conf) => {
-      const { ModalForm, Buttons } = Catpow;
-      const { Input, Button } = ModalForm;
-      return new Promise((resolve, reject) => {
-        if (!conf.inputs) {
-          resolve(false);
-          return;
+    const exec_bulk = useCallback(
+      async (action) => {
+        try {
+          const conf = state.bulk[action];
+          const vals = await show_modal(conf);
+          wp.apiFetch({
+            path: state.apiPath + "/bulk/exec/" + action,
+            method: "POST",
+            data: { rows: state.selectedRows.map((row) => row._id), vals }
+          }).then((res) => {
+            if (callback) {
+              callback({ action, res, state, dispatch });
+            }
+            if (res.remove) {
+              dispatch({ type: "removeRows", rows: state.selectedRows });
+            }
+            if (res.update) {
+              dispatch({ type: "updateRows", rows: res.update });
+            }
+            if (res.message) {
+              dispatch({ type: "showMessage", message: res.message });
+            }
+            if (res.download) {
+              Catpow.util.download(res.download.data, res.download.name || state.name + ".csv", "text/csv");
+            }
+          });
+        } catch (err) {
+          console.error(err);
+          return false;
         }
-        setModal(
-          /* @__PURE__ */ wp.element.createElement(
-            ModalForm,
-            {
-              onComplete: (values) => {
-                setModal(false);
-                if (values.accept) {
-                  resolve(values);
-                } else {
-                  reject(false);
+      },
+      [state, dispatch]
+    );
+    const show_modal = useCallback(
+      (conf) => {
+        const { ModalForm, Buttons } = Catpow;
+        const { Input, Button } = ModalForm;
+        return new Promise((resolve, reject) => {
+          if (!conf.inputs) {
+            resolve(false);
+            return;
+          }
+          setModal(
+            /* @__PURE__ */ wp.element.createElement(
+              ModalForm,
+              {
+                onComplete: (values) => {
+                  setModal(false);
+                  if (values.accept) {
+                    resolve(values);
+                  } else {
+                    reject(false);
+                  }
                 }
-              }
-            },
-            /* @__PURE__ */ wp.element.createElement("div", { className: "cp-finder-bulkcontrol-form" }, /* @__PURE__ */ wp.element.createElement("h3", { className: "title" }, conf.label), /* @__PURE__ */ wp.element.createElement("ul", { className: "inputs" }, conf.inputs.map((props2) => {
-              const { label, desc, caption, ...otherPorps } = props2;
-              return /* @__PURE__ */ wp.element.createElement("li", { className: "item" }, label && /* @__PURE__ */ wp.element.createElement("h4", { className: "label" }, label), desc && /* @__PURE__ */ wp.element.createElement("p", { className: "desc" }, desc), /* @__PURE__ */ wp.element.createElement("div", { className: "input" }, /* @__PURE__ */ wp.element.createElement(Input, { ...otherPorps })), caption && /* @__PURE__ */ wp.element.createElement("p", { className: "caption" }, caption));
-            })), /* @__PURE__ */ wp.element.createElement(Buttons, null, /* @__PURE__ */ wp.element.createElement(
-              Button,
-              {
-                label: __("\u30AD\u30E3\u30F3\u30BB\u30EB", "catpow"),
-                className: "negative",
-                name: "accept",
-                value: false
-              }
-            ), /* @__PURE__ */ wp.element.createElement(
-              Button,
-              {
-                label: __("\u5B9F\u884C", "catpow"),
-                className: "primary",
-                name: "accept",
-                value: true
-              }
-            )))
-          )
-        );
-      });
-    }, [setModal]);
+              },
+              /* @__PURE__ */ wp.element.createElement("div", { className: "cp-finder-bulkcontrol-form" }, /* @__PURE__ */ wp.element.createElement("h3", { className: "title" }, conf.label), /* @__PURE__ */ wp.element.createElement("ul", { className: "inputs" }, conf.inputs.map((props2) => {
+                const { label, desc, caption, ...otherPorps } = props2;
+                return /* @__PURE__ */ wp.element.createElement("li", { className: "item" }, label && /* @__PURE__ */ wp.element.createElement("h4", { className: "label" }, label), desc && /* @__PURE__ */ wp.element.createElement("p", { className: "desc" }, desc), /* @__PURE__ */ wp.element.createElement("div", { className: "input" }, /* @__PURE__ */ wp.element.createElement(Input, { ...otherPorps })), caption && /* @__PURE__ */ wp.element.createElement("p", { className: "caption" }, caption));
+              })), /* @__PURE__ */ wp.element.createElement(Buttons, null, /* @__PURE__ */ wp.element.createElement(Button, { label: __("\u30AD\u30E3\u30F3\u30BB\u30EB", "catpow"), className: "negative", name: "accept", value: false }), /* @__PURE__ */ wp.element.createElement(Button, { label: __("\u5B9F\u884C", "catpow"), className: "primary", name: "accept", value: true })))
+            )
+          );
+        });
+      },
+      [setModal]
+    );
     return /* @__PURE__ */ wp.element.createElement("div", { className: "cp-finder-control cp-finder-bulkcontrol" }, /* @__PURE__ */ wp.element.createElement("ul", { className: "items" }, /* @__PURE__ */ wp.element.createElement("li", { className: "item" + (open ? " active" : "") }, /* @__PURE__ */ wp.element.createElement("div", { className: "inputs" }, /* @__PURE__ */ wp.element.createElement(
       Catpow.SelectBox,
       {
--- components/Finder/BulkControl/component.jsx
+++ components/Finder/BulkControl/component.jsx
@@ -1,123 +1,135 @@
 ﻿/**
-* Finderで選択された項目をbulk APIで処理するコンポーネント
-*/
+ * Finderで選択された項目をbulk APIで処理するコンポーネント
+ */
 
-Catpow.Finder.BulkControl=(props)=>{
-	const {callback}=props;
-	const {useState,useCallback,useMemo,useEffect,useContext}=wp.element;
-	const {__}=wp.i18n;
-	const {state,dispatch}=useContext(Catpow.FinderContext);
-	const [value,setValue]=useState(false);
-	const [modal,setModal]=useState(false);
-	const {cols}=state.index;
-	
-	useEffect(()=>{
+Catpow.Finder.BulkControl = (props) => {
+	const { callback } = props;
+	const { useState, useCallback, useMemo, useEffect, useContext } = wp.element;
+	const { __ } = wp.i18n;
+	const { state, dispatch } = useContext(Catpow.FinderContext);
+	const [value, setValue] = useState(false);
+	const [modal, setModal] = useState(false);
+	const { cols } = state.index;
+
+	useEffect(() => {
 		wp.apiFetch({
-			path:state.apiPath+'/bulk/index'
-		}).then((bulk)=>{
-			dispatch({type:'update',data:{bulk}});
+			path: state.apiPath + "/bulk/index",
+		}).then((bulk) => {
+			dispatch({ type: "update", data: { bulk } });
 		});
-	},[]);
-	const options=useMemo(()=>{
-		if(!state.bulk){return {};}
-		const options={};
-		Object.keys(state.bulk).map((name)=>{
-			options[state.bulk[name].label]=name;
+	}, []);
+	const options = useMemo(() => {
+		if (!state.bulk) {
+			return {};
+		}
+		const options = {};
+		Object.keys(state.bulk).map((name) => {
+			options[state.bulk[name].label] = name;
 		});
 		return options;
-	},[state.bulk]);
-	
-	const exec_bulk=useCallback(async(action)=>{
-		try{
-			const conf=state.bulk[action];
-			const vals=await show_modal(conf);
-			
-			wp.apiFetch({
-				path:state.apiPath+'/bulk/exec/'+action,
-				method: 'POST',
-				data:{rows:state.selectedRows.map((row)=>row._id),vals}
-			}).then((res)=>{
-				if(callback){
-					callback({action,res,state,dispatch});
+	}, [state.bulk]);
+
+	const exec_bulk = useCallback(
+		async (action) => {
+			try {
+				const conf = state.bulk[action];
+				const vals = await show_modal(conf);
+
+				wp.apiFetch({
+					path: state.apiPath + "/bulk/exec/" + action,
+					method: "POST",
+					data: { rows: state.selectedRows.map((row) => row._id), vals },
+				}).then((res) => {
+					if (callback) {
+						callback({ action, res, state, dispatch });
+					}
+					if (res.remove) {
+						dispatch({ type: "removeRows", rows: state.selectedRows });
+					}
+					if (res.update) {
+						dispatch({ type: "updateRows", rows: res.update });
+					}
+					if (res.message) {
+						dispatch({ type: "showMessage", message: res.message });
+					}
+					if (res.download) {
+						Catpow.util.download(res.download.data, res.download.name || state.name + ".csv", "text/csv");
+					}
+				});
+			} catch (err) {
+				console.error(err);
+				return false;
+			}
+		},
+		[state, dispatch]
+	);
+	const show_modal = useCallback(
+		(conf) => {
+			const { ModalForm, Buttons } = Catpow;
+			const { Input, Button } = ModalForm;
+			return new Promise((resolve, reject) => {
+				if (!conf.inputs) {
+					resolve(false);
+					return;
 				}
-				if(res.remove){
-					dispatch({type:'removeRows',rows:state.selectedRows});
-				}
-				if(res.update){
-					dispatch({type:'updateRows',rows:res.update});
-				}
-				if(res.message){
-					dispatch({type:'showMessage',...res.message});
-				}
-				if(res.download){
-					Catpow.util.download(res.download.data,res.download.name || state.name+'.csv','text/csv');
-				}
+				setModal(
+					<ModalForm
+						onComplete={(values) => {
+							setModal(false);
+							if (values.accept) {
+								resolve(values);
+							} else {
+								reject(false);
+							}
+						}}
+					>
+						<div className="cp-finder-bulkcontrol-form">
+							<h3 className="title">{conf.label}</h3>
+							<ul className="inputs">
+								{conf.inputs.map((props) => {
+									const { label, desc, caption, ...otherPorps } = props;
+									return (
+										<li className="item">
+											{label && <h4 className="label">{label}</h4>}
+											{desc && <p className="desc">{desc}</p>}
+											<div className="input">
+												<Input {...otherPorps} />
+											</div>
+											{caption && <p className="caption">{caption}</p>}
+										</li>
+									);
+								})}
+							</ul>
+							<Buttons>
+								<Button label={__("キャンセル", "catpow")} className="negative" name="accept" value={false} />
+								<Button label={__("実行", "catpow")} className="primary" name="accept" value={true} />
+							</Buttons>
+						</div>
+					</ModalForm>
+				);
 			});
-		}
-		catch(err){return false;}
-	},[state,dispatch]);
-	const show_modal=useCallback((conf)=>{
-		const {ModalForm,Buttons}=Catpow;
-		const {Input,Button}=ModalForm;
-		return new Promise((resolve,reject)=>{
-			if(!conf.inputs){resolve(false);return;}
-			setModal(
-				<ModalForm
-					onComplete={(values)=>{
-						setModal(false);
-						if(values.accept){resolve(values);}
-						else{reject(false);}
-					}}
-				>
-					<div className="cp-finder-bulkcontrol-form">
-						<h3 className="title">{conf.label}</h3>
-						<ul className="inputs">
-							{conf.inputs.map((props)=>{
-								const {label,desc,caption,...otherPorps}=props;
-								return (
-									<li className="item">
-										{label && <h4 className="label">{label}</h4>}
-										{desc && <p className="desc">{desc}</p>}
-										<div className="input"><Input {...otherPorps}/></div>
-										{caption && <p className="caption">{caption}</p>}
-									</li>
-								)
-							})}
-						</ul>
-						<Buttons>
-							<Button
-								label={__('キャンセル','catpow')}
-								className="negative"
-								name="accept"
-								value={false}
-							/>
-							<Button
-								label={__('実行','catpow')}
-								className="primary"
-								name="accept"
-								value={true}
-							/>
-						</Buttons>
-					</div>
-				</ModalForm>
-			);
-		});
-	},[setModal]);
-	
+		},
+		[setModal]
+	);
+
 	return (
 		<div className="cp-finder-control cp-finder-bulkcontrol">
 			<ul className="items">
-				<li className={'item'+(open?' active':'')}>
+				<li className={"item" + (open ? " active" : "")}>
 					<div className="inputs">
 						<Catpow.SelectBox
-							label={__('一括処理','catpow')}
+							label={__("一括処理", "catpow")}
 							value={value}
 							options={options}
-							onChange={(val)=>{setValue(val);}}
+							onChange={(val) => {
+								setValue(val);
+							}}
 						/>
 						<Catpow.Button
-							label={__('実行','catpow')}
-							onClick={(e)=>{exec_bulk(value)}}
+							label={__("実行", "catpow")}
+							onClick={(e) => {
+								exec_bulk(value);
+							}}
 						/>
 					</div>
 				</li>
@@ -125,4 +137,4 @@ Catpow.Finder.BulkControl=(props)=>{
 			{modal}
 		</div>
 	);
-}
+};
