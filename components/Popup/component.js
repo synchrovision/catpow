21031fb58a884b6a035cec087fbc24b6cb47b96c
synchrovision <hatano@catpow.info>
2025-08-13 16:26:32 +0900

    feat 👍 update Popup component

--- components/Popup/component.js
+++ components/Popup/component.js
@@ -2,26 +2,30 @@
   // ../components/Popup/component.jsx
   Catpow.Popup = (props) => {
     const { children, open, onClose, onClosed, closeButton = false, closeOnClickAway = true } = props;
-    const { useState, useEffect } = wp.element;
+    const { useState, useEffect, useRef } = wp.element;
     const [state, setPopupState] = useState("closed");
     useEffect(() => setPopupState(open ? "open" : "close"), [open]);
-    return /* @__PURE__ */ wp.element.createElement(Catpow.External, { id: "PopupContainer", className: "cp-popup-container" }, /* @__PURE__ */ wp.element.createElement(
-      "div",
-      {
-        className: "cp-popup " + state,
-        onAnimationEnd: () => {
-          if (state === "close") {
+    const ref = useRef({});
+    useEffect(() => {
+      if (state === "close") {
+        setTimeout(() => {
+          Promise.all(ref.current.getAnimations({ subTree: true }).map((animation) => animation.finished)).then(() => {
             setPopupState("closed");
             onClosed && onClosed();
+          });
+        }, 100);
+      }
+    }, [state]);
+    return /* @__PURE__ */ wp.element.createElement(Catpow.External, { id: "PopupContainer", className: "cp-popup__container" }, /* @__PURE__ */ wp.element.createElement("div", { className: "cp-popup is-" + state, ref }, /* @__PURE__ */ wp.element.createElement(
+      "div",
+      {
+        className: "cp-popup__bg",
+        onClick: () => {
+          if (closeOnClickAway) {
+            onClose();
           }
         }
-      },
-      /* @__PURE__ */ wp.element.createElement("div", { className: "cp-popup-bg", onClick: () => {
-        if (closeOnClickAway) {
-          onClose();
-        }
-      } }),
-      /* @__PURE__ */ wp.element.createElement("div", { className: "cp-popup-body" }, /* @__PURE__ */ wp.element.createElement("div", { className: "cp-popup-contents" }, children), closeButton && /* @__PURE__ */ wp.element.createElement("div", { className: "cp-popup-control" }, /* @__PURE__ */ wp.element.createElement("div", { className: "close", onClick: onClose })))
-    ));
+      }
+    ), /* @__PURE__ */ wp.element.createElement("div", { className: "cp-popup__body" }, /* @__PURE__ */ wp.element.createElement("div", { className: "cp-popup-contents" }, children), closeButton && /* @__PURE__ */ wp.element.createElement("div", { className: "cp-popup-control" }, /* @__PURE__ */ wp.element.createElement("div", { className: "cp-popup-control__close", onClick: onClose })))));
   };
 })();
--- components/Popup/component.jsx
+++ components/Popup/component.jsx
@@ -1,31 +1,42 @@
-﻿Catpow.Popup=(props)=>{
-	const {children,open,onClose,onClosed,closeButton=false,closeOnClickAway=true}=props;
-	const {useState,useEffect}=wp.element;
-	const [state,setPopupState]=useState('closed');
-	
-	useEffect(()=>setPopupState(open?'open':'close'),[open]);
-	
+﻿Catpow.Popup = (props) => {
+	const { children, open, onClose, onClosed, closeButton = false, closeOnClickAway = true } = props;
+	const { useState, useEffect, useRef } = wp.element;
+	const [state, setPopupState] = useState("closed");
+
+	useEffect(() => setPopupState(open ? "open" : "close"), [open]);
+	const ref = useRef({});
+
+	useEffect(() => {
+		if (state === "close") {
+			setTimeout(() => {
+				Promise.all(ref.current.getAnimations({ subTree: true }).map((animation) => animation.finished)).then(() => {
+					setPopupState("closed");
+					onClosed && onClosed();
+				});
+			}, 100);
+		}
+	}, [state]);
+
 	return (
-		<Catpow.External id="PopupContainer" className="cp-popup-container">
-			<div
-				className={'cp-popup '+state}
-				onAnimationEnd={()=>{
-					if(state==='close'){
-						setPopupState('closed');
-						onClosed && onClosed();
-					}
-				}}
-			>
-				<div className="cp-popup-bg" onClick={()=>{if(closeOnClickAway){onClose()}}}></div>
-				<div className="cp-popup-body">
+		<Catpow.External id="PopupContainer" className="cp-popup__container">
+			<div className={"cp-popup is-" + state} ref={ref}>
+				<div
+					className="cp-popup__bg"
+					onClick={() => {
+						if (closeOnClickAway) {
+							onClose();
+						}
+					}}
+				></div>
+				<div className="cp-popup__body">
 					<div className="cp-popup-contents">{children}</div>
 					{closeButton && (
 						<div className="cp-popup-control">
-							<div className="close" onClick={onClose}></div>
+							<div className="cp-popup-control__close" onClick={onClose}></div>
 						</div>
 					)}
 				</div>
 			</div>
 		</Catpow.External>
 	);
-}
+};
