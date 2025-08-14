a26c9d90b9b3bc1d99a3665c17de30cc41321e01
synchrovision <hatano@catpow.info>
2025-08-14 12:28:46 +0900

    feat 👍 update t-media-text block

--- blocks/t-media-text/editor_script.js
+++ blocks/t-media-text/editor_script.js
@@ -8,7 +8,7 @@
     category: "catpow-mail",
     parent: CP.mailContensContainer,
     attributes: {
-      classes: { source: "attribute", selector: "table", attribute: "class", default: "wp-block-catpow-t-media-text" },
+      classes: { source: "attribute", selector: "table", attribute: "class", default: "wp-block-catpow-t-media-text has-vertical-align-top has-image-left" },
       src: { source: "attribute", selector: "[src]", attribute: "src", default: wpinfo.theme_url + "/images/dummy.jpg" },
       alt: { source: "attribute", selector: "[src]", attribute: "alt" },
       imageCode: { source: "text", selector: "td.is-image-cell", default: wpinfo.theme_url + "/images/dummy.jpg" },
@@ -24,19 +24,20 @@
       var states = CP.classNamesToFlags(classes);
       const selectiveClasses = useMemo(() => {
         const selectiveClasses2 = [
-          { name: "valign", type: "buttons", label: "\u5782\u76F4\u65B9\u5411\u63C3\u3048", values: { isValignTop: __("\u4E0A\u63C3\u3048", "catpow"), isValignCenter: __("\u4E2D\u592E\u63C3\u3048", "catpow"), isValignBottom: __("\u4E0B\u63C3\u3048", "catpow") } },
+          "imagePosition",
+          "verticalAlign",
+          { name: "range", input: "range", label: "\u753B\u50CF\u306E\u5E45", key: "width", min: 50, max: 400, step: 10 },
           {
             name: "template",
             label: "\u30C6\u30F3\u30D7\u30EC\u30FC\u30C8",
             values: "isTemplate",
             sub: [{ name: "imageCode", label: "\u753B\u50CF\u51FA\u529B\u30B3\u30FC\u30C9", input: "text", key: "imageCode" }]
-          },
-          { name: "range", input: "range", label: "\u753B\u50CF\u306E\u5E45", key: "width", min: 50, max: 400, step: 10 }
+          }
         ];
         wp.hooks.applyFilters("catpow.blocks.t-media-text.selectiveClasses", CP.finderProxy(selectiveClasses2));
         return selectiveClasses2;
       }, []);
-      return /* @__PURE__ */ wp.element.createElement(wp.element.Fragment, null, /* @__PURE__ */ wp.element.createElement(CP.Bem, { prefix: "wp-block-catpow" }, /* @__PURE__ */ wp.element.createElement("table", { width: "100%", className: classes }, /* @__PURE__ */ wp.element.createElement("tbody", null, /* @__PURE__ */ wp.element.createElement("tr", null, /* @__PURE__ */ wp.element.createElement("td", { className: "_td is-image-cell", width }, /* @__PURE__ */ wp.element.createElement(
+      return /* @__PURE__ */ wp.element.createElement(wp.element.Fragment, null, /* @__PURE__ */ wp.element.createElement(CP.Bem, { prefix: "wp-block-catpow" }, /* @__PURE__ */ wp.element.createElement("table", { width: "100%", className: classes }, /* @__PURE__ */ wp.element.createElement("tbody", null, states.hasImageLeft ? /* @__PURE__ */ wp.element.createElement("tr", null, /* @__PURE__ */ wp.element.createElement("td", { className: "_td is-image-cell", width }, /* @__PURE__ */ wp.element.createElement(
         CP.SelectResponsiveImage,
         {
           set: setAttributes,
@@ -47,14 +48,25 @@
           height: "auto",
           isTemplate: states.isTemplate
         }
-      )), /* @__PURE__ */ wp.element.createElement("td", { className: "_td is-spacer-cell" }), /* @__PURE__ */ wp.element.createElement("td", { className: "_td is-text-cell" }, /* @__PURE__ */ wp.element.createElement(InnerBlocks, null)))))), /* @__PURE__ */ wp.element.createElement(BlockControls, null, /* @__PURE__ */ wp.element.createElement(CP.VerticalAlignClassToolbar, { set: setAttributes, attr: attributes })), /* @__PURE__ */ wp.element.createElement(InspectorControls, null, /* @__PURE__ */ wp.element.createElement(CP.SelectClassPanel, { title: "\u30AF\u30E9\u30B9", icon: "art", set: setAttributes, attr: attributes, selectiveClasses }), /* @__PURE__ */ wp.element.createElement(PanelBody, { title: "CLASS", icon: "admin-generic", initialOpen: false }, /* @__PURE__ */ wp.element.createElement(TextareaControl, { label: "\u30AF\u30E9\u30B9", onChange: (classes2) => setAttributes({ classes: classes2 }), value: classes }))));
+      )), /* @__PURE__ */ wp.element.createElement("td", { className: "_td is-spacer-cell" }), /* @__PURE__ */ wp.element.createElement("td", { className: "_td is-text-cell" }, /* @__PURE__ */ wp.element.createElement(InnerBlocks, null))) : /* @__PURE__ */ wp.element.createElement("tr", null, /* @__PURE__ */ wp.element.createElement("td", { className: "_td is-text-cell" }, /* @__PURE__ */ wp.element.createElement(InnerBlocks, null)), /* @__PURE__ */ wp.element.createElement("td", { className: "_td is-spacer-cell" }), /* @__PURE__ */ wp.element.createElement("td", { className: "_td is-image-cell", width }, /* @__PURE__ */ wp.element.createElement(
+        CP.SelectResponsiveImage,
+        {
+          set: setAttributes,
+          attr: attributes,
+          keys: { src: "src", alt: "alt", code: "imageCode" },
+          size: "large",
+          width: "100%",
+          height: "auto",
+          isTemplate: states.isTemplate
+        }
+      )))))), /* @__PURE__ */ wp.element.createElement(BlockControls, null, /* @__PURE__ */ wp.element.createElement(CP.ImagePositionClassToolbar, { set: setAttributes, attr: attributes }), /* @__PURE__ */ wp.element.createElement(CP.VerticalAlignClassToolbar, { set: setAttributes, attr: attributes })), /* @__PURE__ */ wp.element.createElement(InspectorControls, null, /* @__PURE__ */ wp.element.createElement(CP.SelectClassPanel, { title: "\u30AF\u30E9\u30B9", icon: "art", set: setAttributes, attr: attributes, selectiveClasses }), /* @__PURE__ */ wp.element.createElement(PanelBody, { title: "CLASS", icon: "admin-generic", initialOpen: false }, /* @__PURE__ */ wp.element.createElement(TextareaControl, { label: "\u30AF\u30E9\u30B9", onChange: (classes2) => setAttributes({ classes: classes2 }), value: classes }))));
     },
     save({ attributes, className, setAttributes }) {
       const { InnerBlocks } = wp.blockEditor;
       const { classes, src, alt, imageCode, width } = attributes;
       const primaryClass = "wp-block-catpow-t-media-text";
       var states = CP.classNamesToFlags(classes);
-      return /* @__PURE__ */ wp.element.createElement(CP.Bem, { prefix: "wp-block-catpow" }, /* @__PURE__ */ wp.element.createElement("table", { width: "100%", className: classes }, /* @__PURE__ */ wp.element.createElement("tbody", null, /* @__PURE__ */ wp.element.createElement("tr", null, /* @__PURE__ */ wp.element.createElement("td", { className: "_td is-image-cell", width }, /* @__PURE__ */ wp.element.createElement(CP.ResponsiveImage, { attr: attributes, keys: { src: "src", alt: "alt", code: "imageCode" }, size: "large", width: "100%", height: "auto", isTemplate: states.isTemplate })), /* @__PURE__ */ wp.element.createElement("td", { className: "_td is-spacer-cell" }), /* @__PURE__ */ wp.element.createElement("td", { className: "_td is-text-cell" }, /* @__PURE__ */ wp.element.createElement(InnerBlocks.Content, null))))));
+      return /* @__PURE__ */ wp.element.createElement(CP.Bem, { prefix: "wp-block-catpow" }, /* @__PURE__ */ wp.element.createElement("table", { width: "100%", className: classes }, /* @__PURE__ */ wp.element.createElement("tbody", null, states.hasImageLeft ? /* @__PURE__ */ wp.element.createElement("tr", null, /* @__PURE__ */ wp.element.createElement("td", { className: "_td is-image-cell", width }, /* @__PURE__ */ wp.element.createElement(CP.ResponsiveImage, { attr: attributes, keys: { src: "src", alt: "alt", code: "imageCode" }, size: "large", width: "100%", height: "auto", isTemplate: states.isTemplate })), /* @__PURE__ */ wp.element.createElement("td", { className: "_td is-spacer-cell" }), /* @__PURE__ */ wp.element.createElement("td", { className: "_td is-text-cell" }, /* @__PURE__ */ wp.element.createElement(InnerBlocks.Content, null))) : /* @__PURE__ */ wp.element.createElement("tr", null, /* @__PURE__ */ wp.element.createElement("td", { className: "_td is-text-cell" }, /* @__PURE__ */ wp.element.createElement(InnerBlocks.Content, null)), /* @__PURE__ */ wp.element.createElement("td", { className: "_td is-spacer-cell" }), /* @__PURE__ */ wp.element.createElement("td", { className: "_td is-image-cell", width }, /* @__PURE__ */ wp.element.createElement(CP.ResponsiveImage, { attr: attributes, keys: { src: "src", alt: "alt", code: "imageCode" }, size: "large", width: "100%", height: "auto", isTemplate: states.isTemplate }))))));
     }
   });
 })();
--- blocks/t-media-text/editor_script.jsx
+++ blocks/t-media-text/editor_script.jsx
@@ -7,7 +7,7 @@ wp.blocks.registerBlockType("catpow/t-media-text", {
 	category: "catpow-mail",
 	parent: CP.mailContensContainer,
 	attributes: {
-		classes: { source: "attribute", selector: "table", attribute: "class", default: "wp-block-catpow-t-media-text" },
+		classes: { source: "attribute", selector: "table", attribute: "class", default: "wp-block-catpow-t-media-text has-vertical-align-top has-image-left" },
 		src: { source: "attribute", selector: "[src]", attribute: "src", default: wpinfo.theme_url + "/images/dummy.jpg" },
 		alt: { source: "attribute", selector: "[src]", attribute: "alt" },
 		imageCode: { source: "text", selector: "td.is-image-cell", default: wpinfo.theme_url + "/images/dummy.jpg" },
@@ -24,14 +24,15 @@ wp.blocks.registerBlockType("catpow/t-media-text", {
 
 		const selectiveClasses = useMemo(() => {
 			const selectiveClasses = [
-				{ name: "valign", type: "buttons", label: "垂直方向揃え", values: { isValignTop: __("上揃え", "catpow"), isValignCenter: __("中央揃え", "catpow"), isValignBottom: __("下揃え", "catpow") } },
+				"imagePosition",
+				"verticalAlign",
+				{ name: "range", input: "range", label: "画像の幅", key: "width", min: 50, max: 400, step: 10 },
 				{
 					name: "template",
 					label: "テンプレート",
 					values: "isTemplate",
 					sub: [{ name: "imageCode", label: "画像出力コード", input: "text", key: "imageCode" }],
 				},
-				{ name: "range", input: "range", label: "画像の幅", key: "width", min: 50, max: 400, step: 10 },
 			];
 			wp.hooks.applyFilters("catpow.blocks.t-media-text.selectiveClasses", CP.finderProxy(selectiveClasses));
 			return selectiveClasses;
@@ -42,27 +43,48 @@ wp.blocks.registerBlockType("catpow/t-media-text", {
 				<CP.Bem prefix="wp-block-catpow">
 					<table width="100%" className={classes}>
 						<tbody>
-							<tr>
-								<td className="_td is-image-cell" width={width}>
-									<CP.SelectResponsiveImage
-										set={setAttributes}
-										attr={attributes}
-										keys={{ src: "src", alt: "alt", code: "imageCode" }}
-										size="large"
-										width="100%"
-										height="auto"
-										isTemplate={states.isTemplate}
-									/>
-								</td>
-								<td className="_td is-spacer-cell"></td>
-								<td className="_td is-text-cell">
-									<InnerBlocks />
-								</td>
-							</tr>
+							{states.hasImageLeft ? (
+								<tr>
+									<td className="_td is-image-cell" width={width}>
+										<CP.SelectResponsiveImage
+											set={setAttributes}
+											attr={attributes}
+											keys={{ src: "src", alt: "alt", code: "imageCode" }}
+											size="large"
+											width="100%"
+											height="auto"
+											isTemplate={states.isTemplate}
+										/>
+									</td>
+									<td className="_td is-spacer-cell"></td>
+									<td className="_td is-text-cell">
+										<InnerBlocks />
+									</td>
+								</tr>
+							) : (
+								<tr>
+									<td className="_td is-text-cell">
+										<InnerBlocks />
+									</td>
+									<td className="_td is-spacer-cell"></td>
+									<td className="_td is-image-cell" width={width}>
+										<CP.SelectResponsiveImage
+											set={setAttributes}
+											attr={attributes}
+											keys={{ src: "src", alt: "alt", code: "imageCode" }}
+											size="large"
+											width="100%"
+											height="auto"
+											isTemplate={states.isTemplate}
+										/>
+									</td>
+								</tr>
+							)}
 						</tbody>
 					</table>
 				</CP.Bem>
 				<BlockControls>
+					<CP.ImagePositionClassToolbar set={setAttributes} attr={attributes} />
 					<CP.VerticalAlignClassToolbar set={setAttributes} attr={attributes} />
 				</BlockControls>
 				<InspectorControls>
@@ -84,15 +106,27 @@ wp.blocks.registerBlockType("catpow/t-media-text", {
 			<CP.Bem prefix="wp-block-catpow">
 				<table width="100%" className={classes}>
 					<tbody>
-						<tr>
-							<td className="_td is-image-cell" width={width}>
-								<CP.ResponsiveImage attr={attributes} keys={{ src: "src", alt: "alt", code: "imageCode" }} size="large" width="100%" height="auto" isTemplate={states.isTemplate} />
-							</td>
-							<td className="_td is-spacer-cell"></td>
-							<td className="_td is-text-cell">
-								<InnerBlocks.Content />
-							</td>
-						</tr>
+						{states.hasImageLeft ? (
+							<tr>
+								<td className="_td is-image-cell" width={width}>
+									<CP.ResponsiveImage attr={attributes} keys={{ src: "src", alt: "alt", code: "imageCode" }} size="large" width="100%" height="auto" isTemplate={states.isTemplate} />
+								</td>
+								<td className="_td is-spacer-cell"></td>
+								<td className="_td is-text-cell">
+									<InnerBlocks.Content />
+								</td>
+							</tr>
+						) : (
+							<tr>
+								<td className="_td is-text-cell">
+									<InnerBlocks.Content />
+								</td>
+								<td className="_td is-spacer-cell"></td>
+								<td className="_td is-image-cell" width={width}>
+									<CP.ResponsiveImage attr={attributes} keys={{ src: "src", alt: "alt", code: "imageCode" }} size="large" width="100%" height="auto" isTemplate={states.isTemplate} />
+								</td>
+							</tr>
+						)}
 					</tbody>
 				</table>
 			</CP.Bem>
