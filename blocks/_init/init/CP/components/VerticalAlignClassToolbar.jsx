100b202200db2296a85d5aaf434317a3c64287b9
synchrovision <hatano@catpow.info>
2025-08-14 12:26:32 +0900

    feat 👍 update VerticalAlignClassToolbar component

--- blocks/_init/init/CP/components/VerticalAlignClassToolbar.jsx
+++ blocks/_init/init/CP/components/VerticalAlignClassToolbar.jsx
@@ -1,12 +1,18 @@
-﻿export const VerticalAlignClassToolbar = (props) => {
+﻿const { useMemo } = wp.element;
+
+export const VerticalAlignClassToolbar = (props) => {
+	const { set, attr, classKey = "classes", aligns = { top: "has-vertical-align-top", center: "has-vertical-align-middle", bottom: "has-vertical-align-bottom" } } = props;
 	const { BlockVerticalAlignmentToolbar } = wp.blockEditor;
-	const aligns = ["top", "center", "bottom"];
+
+	const classSet = useMemo(() => new Set(attr[classKey]?.split(" ") || []), [attr[classKey]]);
+	const alignClassSet = useMemo(() => new Set(Object.values(aligns)), [aligns]);
+
 	return (
 		<BlockVerticalAlignmentToolbar
-			value={CP.getSelectiveClass(props, aligns)}
-			controls={props.aligns || aligns}
+			value={Object.entries(aligns).find(([key, val]) => classSet.has(val))?.[0] || "top"}
+			controls={Object.keys(aligns)}
 			onChange={(align) => {
-				CP.switchSelectiveClass(props, aligns, align, props.key);
+				set({ [classKey]: [...classSet.difference(alignClassSet).add(aligns[align])].join(" ") });
 			}}
 		/>
 	);
