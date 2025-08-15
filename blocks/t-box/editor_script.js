34eafadcf845b71892a88a09cb5b5ae8d8d3cc38
synchrovision <hatano@catpow.info>
2025-08-16 05:14:41 +0900

    feat 👍 update t-box block

--- blocks/t-box/editor_script.js
+++ blocks/t-box/editor_script.js
@@ -10,32 +10,36 @@
     attributes: {
       classes: { source: "attribute", selector: ".wp-block-catpow-t-box", attribute: "class", default: "wp-block-catpow-t-box has-mail-content-width" },
       width: { source: "attribute", selector: "table", attribute: "width", default: "600" },
-      padding: { source: "attribute", selector: "table", attribute: "cellpadding", default: "0" }
+      paddingTop: { type: "number", default: 1 },
+      paddingInline: { type: "number", default: 1 },
+      paddingBottom: { type: "number", default: 1 }
     },
     example: CP.example,
     edit({ attributes, className, setAttributes }) {
       const { useState, useMemo } = wp.element;
       const { InnerBlocks, InspectorControls } = wp.blockEditor;
       const { PanelBody, TextareaControl } = wp.components;
-      const { classes, width, padding } = attributes;
+      const { classes, width, paddingTop, paddingInline, paddingBottom } = attributes;
       var states = CP.classNamesToFlags(classes);
       const selectiveClasses = useMemo(() => {
         const selectiveClasses2 = [
           "colorScheme",
           { name: "body", type: "buttons", label: "\u80CC\u666F\u8272", values: { hasBgNone: "\u306A\u3057", hasBgNormal: "\u901A\u5E38", hasBgStrong: "\u5F37\u8ABF", hasBgAchromatic: "\u767D\u9ED2" } },
           { name: "range", input: "range", label: __("\u5E45", "catpow"), key: "width", min: 400, max: 800, step: 10 },
-          { name: "range", input: "range", label: __("\u4F59\u767D", "catpow"), key: "padding", min: 0, max: 100, step: 5 }
+          { name: "range", input: "range", label: __("\u4E0A\u4F59\u767D", "catpow"), key: "paddingTop", min: 0, max: 10 },
+          { name: "range", input: "range", label: __("\u6A2A\u4F59\u767D", "catpow"), key: "paddingInline", min: 0, max: 10 },
+          { name: "range", input: "range", label: __("\u4E0B\u4F59\u767D", "catpow"), key: "paddingBottom", min: 0, max: 10 }
         ];
         wp.hooks.applyFilters("catpow.blocks.t-box.selectiveClasses", CP.finderProxy(selectiveClasses2));
         return selectiveClasses2;
       }, []);
-      return /* @__PURE__ */ wp.element.createElement(wp.element.Fragment, null, /* @__PURE__ */ wp.element.createElement(CP.Bem, { prefix: "wp-block-catpow" }, /* @__PURE__ */ wp.element.createElement("div", { className: classes }, /* @__PURE__ */ wp.element.createElement("table", { width, cellPadding: padding, style: { width: `${width}px`, padding: `${padding}px` }, align: "center" }, /* @__PURE__ */ wp.element.createElement("tbody", null, /* @__PURE__ */ wp.element.createElement("tr", null, /* @__PURE__ */ wp.element.createElement("td", null, /* @__PURE__ */ wp.element.createElement(InnerBlocks, null))))))), /* @__PURE__ */ wp.element.createElement(InspectorControls, null, /* @__PURE__ */ wp.element.createElement(CP.SelectClassPanel, { title: "\u30AF\u30E9\u30B9", icon: "art", set: setAttributes, attr: attributes, selectiveClasses, initialOpen: true }), /* @__PURE__ */ wp.element.createElement(PanelBody, { title: "CLASS", icon: "admin-generic", initialOpen: false }, /* @__PURE__ */ wp.element.createElement(TextareaControl, { label: "\u30AF\u30E9\u30B9", onChange: (classes2) => setAttributes({ classes: classes2 }), value: classes }))));
+      return /* @__PURE__ */ wp.element.createElement(wp.element.Fragment, null, /* @__PURE__ */ wp.element.createElement(CP.Bem, { prefix: "wp-block-catpow" }, /* @__PURE__ */ wp.element.createElement("div", { className: classes }, /* @__PURE__ */ wp.element.createElement("table", { width, cstyle: { width: `${width}px` }, align: "center" }, /* @__PURE__ */ wp.element.createElement("tbody", null, paddingTop > 0 && /* @__PURE__ */ wp.element.createElement("tr", null, /* @__PURE__ */ wp.element.createElement("td", { className: "_td is-spacer-cell", style: { height: `${paddingTop}rem` }, colspan: paddingInline > 0 ? 3 : null })), /* @__PURE__ */ wp.element.createElement("tr", null, paddingInline > 0 && /* @__PURE__ */ wp.element.createElement("tr", null, /* @__PURE__ */ wp.element.createElement("td", { className: "_td is-spacer-cell", style: { width: `${paddingInline}rem` }, width: `${paddingInline}rem` })), /* @__PURE__ */ wp.element.createElement("td", null, /* @__PURE__ */ wp.element.createElement(InnerBlocks, null)), paddingInline > 0 && /* @__PURE__ */ wp.element.createElement("tr", null, /* @__PURE__ */ wp.element.createElement("td", { className: "_td is-spacer-cell", style: { width: `${paddingInline}rem` }, width: `${paddingInline}rem` }))), paddingBottom > 0 && /* @__PURE__ */ wp.element.createElement("tr", null, /* @__PURE__ */ wp.element.createElement("td", { className: "_td is-spacer-cell", style: { height: `${paddingBottom}rem` }, colspan: paddingInline > 0 ? 3 : null })))))), /* @__PURE__ */ wp.element.createElement(InspectorControls, null, /* @__PURE__ */ wp.element.createElement(CP.SelectClassPanel, { title: "\u30AF\u30E9\u30B9", icon: "art", set: setAttributes, attr: attributes, selectiveClasses, initialOpen: true }), /* @__PURE__ */ wp.element.createElement(PanelBody, { title: "CLASS", icon: "admin-generic", initialOpen: false }, /* @__PURE__ */ wp.element.createElement(TextareaControl, { label: "\u30AF\u30E9\u30B9", onChange: (classes2) => setAttributes({ classes: classes2 }), value: classes }))));
     },
     save({ attributes, className, setAttributes }) {
       const { InnerBlocks } = wp.blockEditor;
-      const { classes, width, padding } = attributes;
+      const { classes, width, paddingTop, paddingInline, paddingBottom } = attributes;
       var states = CP.classNamesToFlags(classes);
-      return /* @__PURE__ */ wp.element.createElement(CP.Bem, { prefix: "wp-block-catpow" }, /* @__PURE__ */ wp.element.createElement("div", { className: classes }, /* @__PURE__ */ wp.element.createElement("table", { width, cellPadding: padding, style: { width: `${width}px`, padding: `${padding}px` }, align: "center" }, /* @__PURE__ */ wp.element.createElement("tbody", null, /* @__PURE__ */ wp.element.createElement("tr", null, /* @__PURE__ */ wp.element.createElement("td", null, /* @__PURE__ */ wp.element.createElement(InnerBlocks.Content, null)))))));
+      return /* @__PURE__ */ wp.element.createElement(CP.Bem, { prefix: "wp-block-catpow" }, /* @__PURE__ */ wp.element.createElement("div", { className: classes }, /* @__PURE__ */ wp.element.createElement("table", { width, style: { width: `${width}px` }, align: "center" }, /* @__PURE__ */ wp.element.createElement("tbody", null, paddingTop > 0 && /* @__PURE__ */ wp.element.createElement("tr", null, /* @__PURE__ */ wp.element.createElement("td", { className: "_td is-spacer-cell", style: { height: `${paddingTop}rem` }, colspan: paddingInline > 0 ? 3 : null })), /* @__PURE__ */ wp.element.createElement("tr", null, paddingInline > 0 && /* @__PURE__ */ wp.element.createElement("tr", null, /* @__PURE__ */ wp.element.createElement("td", { className: "_td is-spacer-cell", style: { width: `${paddingInline}rem` }, width: `${paddingInline}rem` })), /* @__PURE__ */ wp.element.createElement("td", null, /* @__PURE__ */ wp.element.createElement(InnerBlocks.Content, null)), paddingInline > 0 && /* @__PURE__ */ wp.element.createElement("tr", null, /* @__PURE__ */ wp.element.createElement("td", { className: "_td is-spacer-cell", style: { width: `${paddingInline}rem` }, width: `${paddingInline}rem` }))), paddingBottom > 0 && /* @__PURE__ */ wp.element.createElement("tr", null, /* @__PURE__ */ wp.element.createElement("td", { className: "_td is-spacer-cell", style: { height: `${paddingBottom}rem` }, colspan: paddingInline > 0 ? 3 : null }))))));
     }
   });
 })();
--- blocks/t-box/editor_script.jsx
+++ blocks/t-box/editor_script.jsx
@@ -9,14 +9,16 @@ wp.blocks.registerBlockType("catpow/t-box", {
 	attributes: {
 		classes: { source: "attribute", selector: ".wp-block-catpow-t-box", attribute: "class", default: "wp-block-catpow-t-box has-mail-content-width" },
 		width: { source: "attribute", selector: "table", attribute: "width", default: "600" },
-		padding: { source: "attribute", selector: "table", attribute: "cellpadding", default: "0" },
+		paddingTop: { type: "number", default: 1 },
+		paddingInline: { type: "number", default: 1 },
+		paddingBottom: { type: "number", default: 1 },
 	},
 	example: CP.example,
 	edit({ attributes, className, setAttributes }) {
 		const { useState, useMemo } = wp.element;
 		const { InnerBlocks, InspectorControls } = wp.blockEditor;
 		const { PanelBody, TextareaControl } = wp.components;
-		const { classes, width, padding } = attributes;
+		const { classes, width, paddingTop, paddingInline, paddingBottom } = attributes;
 		var states = CP.classNamesToFlags(classes);
 
 		const selectiveClasses = useMemo(() => {
@@ -24,7 +26,9 @@ wp.blocks.registerBlockType("catpow/t-box", {
 				"colorScheme",
 				{ name: "body", type: "buttons", label: "背景色", values: { hasBgNone: "なし", hasBgNormal: "通常", hasBgStrong: "強調", hasBgAchromatic: "白黒" } },
 				{ name: "range", input: "range", label: __("幅", "catpow"), key: "width", min: 400, max: 800, step: 10 },
-				{ name: "range", input: "range", label: __("余白", "catpow"), key: "padding", min: 0, max: 100, step: 5 },
+				{ name: "range", input: "range", label: __("上余白", "catpow"), key: "paddingTop", min: 0, max: 10 },
+				{ name: "range", input: "range", label: __("横余白", "catpow"), key: "paddingInline", min: 0, max: 10 },
+				{ name: "range", input: "range", label: __("下余白", "catpow"), key: "paddingBottom", min: 0, max: 10 },
 			];
 			wp.hooks.applyFilters("catpow.blocks.t-box.selectiveClasses", CP.finderProxy(selectiveClasses));
 			return selectiveClasses;
@@ -34,13 +38,33 @@ wp.blocks.registerBlockType("catpow/t-box", {
 			<>
 				<CP.Bem prefix="wp-block-catpow">
 					<div className={classes}>
-						<table width={width} cellPadding={padding} style={{ width: `${width}px`, padding: `${padding}px` }} align="center">
+						<table width={width} cstyle={{ width: `${width}px` }} align="center">
 							<tbody>
+								{paddingTop > 0 && (
+									<tr>
+										<td className="_td is-spacer-cell" style={{ height: `${paddingTop}rem` }} colspan={paddingInline > 0 ? 3 : null}></td>
+									</tr>
+								)}
 								<tr>
+									{paddingInline > 0 && (
+										<tr>
+											<td className="_td is-spacer-cell" style={{ width: `${paddingInline}rem` }} width={`${paddingInline}rem`}></td>
+										</tr>
+									)}
 									<td>
 										<InnerBlocks />
 									</td>
+									{paddingInline > 0 && (
+										<tr>
+											<td className="_td is-spacer-cell" style={{ width: `${paddingInline}rem` }} width={`${paddingInline}rem`}></td>
+										</tr>
+									)}
 								</tr>
+								{paddingBottom > 0 && (
+									<tr>
+										<td className="_td is-spacer-cell" style={{ height: `${paddingBottom}rem` }} colspan={paddingInline > 0 ? 3 : null}></td>
+									</tr>
+								)}
 							</tbody>
 						</table>
 					</div>
@@ -57,18 +81,38 @@ wp.blocks.registerBlockType("catpow/t-box", {
 
 	save({ attributes, className, setAttributes }) {
 		const { InnerBlocks } = wp.blockEditor;
-		const { classes, width, padding } = attributes;
+		const { classes, width, paddingTop, paddingInline, paddingBottom } = attributes;
 		var states = CP.classNamesToFlags(classes);
 		return (
 			<CP.Bem prefix="wp-block-catpow">
 				<div className={classes}>
-					<table width={width} cellPadding={padding} style={{ width: `${width}px`, padding: `${padding}px` }} align="center">
+					<table width={width} style={{ width: `${width}px` }} align="center">
 						<tbody>
+							{paddingTop > 0 && (
+								<tr>
+									<td className="_td is-spacer-cell" style={{ height: `${paddingTop}rem` }} colspan={paddingInline > 0 ? 3 : null}></td>
+								</tr>
+							)}
 							<tr>
+								{paddingInline > 0 && (
+									<tr>
+										<td className="_td is-spacer-cell" style={{ width: `${paddingInline}rem` }} width={`${paddingInline}rem`}></td>
+									</tr>
+								)}
 								<td>
 									<InnerBlocks.Content />
 								</td>
+								{paddingInline > 0 && (
+									<tr>
+										<td className="_td is-spacer-cell" style={{ width: `${paddingInline}rem` }} width={`${paddingInline}rem`}></td>
+									</tr>
+								)}
 							</tr>
+							{paddingBottom > 0 && (
+								<tr>
+									<td className="_td is-spacer-cell" style={{ height: `${paddingBottom}rem` }} colspan={paddingInline > 0 ? 3 : null}></td>
+								</tr>
+							)}
 						</tbody>
 					</table>
 				</div>
