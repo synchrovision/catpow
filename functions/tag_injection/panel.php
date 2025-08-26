7f45809f716b7a034d8d85790169c43571526a5e
synchrovision <hatano@catpow.info>
2025-08-26 18:49:03 +0900

    edit 📝 update panel for tag_injection

--- functions/tag_injection/panel.php
+++ functions/tag_injection/panel.php
@@ -1,12 +1,12 @@
 <?php
 namespace Catpow;
 ?>
-<?php foreach(CP::$content->conf['meta'] as $name=>$conf): ?>
 <dl>
+<?php foreach(CP::$content->conf['meta'] as $name=>$conf): ?>
 	<dt><i class="fas fa-code"></i><?=$conf['label']?></dt>
 	<dd><?php input($name); ?></dd>
-</dl>
 <?php endforeach; ?>
+</dl>
 <ul class="cp-admin-buttons">
 	<li class="item"><?php button('<i class="fas fa-sync-alt"></i>登録','action'); ?></li>
 </ul>
