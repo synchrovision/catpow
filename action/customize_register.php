33ef9ddb120a3fc470b99836e1c85d41afa93ac9
synchrovision <hatano@catpow.info>
2026-01-13 19:14:52 +0900

    feat 👍 add Font Size customize panel

--- action/customize_register.php
+++ action/customize_register.php
@@ -1,9 +1,12 @@
 <?php
 use Catpow\util\style_config;
 
+//remove wp color customize
 $wp_customize->remove_control('background_color');
 remove_theme_mod('background_color');
 $wp_customize->remove_control('header_textcolor');
+
+//Color
 $wp_customize->add_setting(new Catpow\customize\setting($wp_customize,'colors',[
 	'transport'=>'postMessage'
 ]));
@@ -16,12 +19,11 @@ $wp_customize->add_control(new Catpow\customize\control($wp_customize,'colors',[
 ]));
 add_action("customize_save_colors",["Catpow\\util\\style_config",'update']);
 
+//Font
 $wp_customize->add_section('fonts',[
 	'title'=>__('フォント','catpow'),
 	'priority'=>40
 ]);
-	
-
 foreach(style_config::get_font_roles() as $role=>$font_setting){
 	$setting_id="fonts[{$role}]";
 	$wp_customize->add_setting($setting_id,['default'=>$font_setting['default'],'transport'=>'postMessage']);
@@ -33,3 +35,20 @@ foreach(style_config::get_font_roles() as $role=>$font_setting){
 	
 }
 add_action("customize_save_fonts",["Catpow\\util\\style_config",'update']);
+
+//Font Size
+$wp_customize->add_setting(new Catpow\customize\setting($wp_customize,'font_sizes',[
+	'transport'=>'postMessage'
+]));
+$wp_customize->add_section('font_sizes',[
+	'title'=>__('文字サイズ','catpow'),
+	'priority'=>40
+]);
+$wp_customize->add_control(new Catpow\customize\control($wp_customize,'font_sizes',[
+	'type'=>'FontSizes',
+	'section'=>'font_sizes',
+	'param'=>[
+		'roles'=>style_config::get_font_size_roles()
+	]
+]));
+add_action("customize_save_font_sizes",["Catpow\\util\\style_config",'update']);
---
+++ components/Customize/FontSizes/component.jsx
@@ -0,0 +1,81 @@
+﻿import { useState, useCallback, useMemo } from "react";
+import { Bem, DataSet, LineChartInput, Legend } from "catpow/component";
+import { range } from "catpow/util";
+
+const labels = {
+	rows: ["見出し(vw)", "見出し(rem)", "本文(vw)", "本文(rem)"],
+	cells: [[[...range(1, 6)].map((i) => `h${i}(vw)`), [...range(1, 6)].map((i) => `h${i}(rem)`), [...range(1, 6)].map((i) => `p${i}(vw)`), [...range(1, 6)].map((i) => `p${i}(rem)`)]],
+};
+const colors = {
+	rows: ["oklch(50% 40% 220)", "oklch(50% 80% 220)", "oklch(60% 10% 180)", "oklch(60% 20% 180)"],
+};
+const classNames = {
+	rows: ["is-row-heading is-row-vw", "is-row-heading is-row-rem", "is-row-paragraph is-row-vw", "is-row-paragraph is-row-rem"],
+};
+
+const extractValuesFromSize = (size) => {
+	const m = size.match(/min\((.+?)vw,(.+?)rem/);
+	return [m[1] * 4, m[2] * 16];
+};
+const sizesToValues = (sizes, rolesByShorthand) => {
+	const values = [[], [], [], []];
+	[...range(1, 6)].reverse().forEach((i) => {
+		const [hvw, hrem] = extractValuesFromSize(sizes[`h${i}`] || rolesByShorthand[`h${i}`].default);
+		const [pvw, prem] = extractValuesFromSize(sizes[`p${i}`] || rolesByShorthand[`p${i}`].default);
+		values[0].push(hvw);
+		values[1].push(hrem);
+		values[2].push(pvw);
+		values[3].push(prem);
+	});
+	return values;
+};
+
+const convertValuesToSizes = (vwValue, remValue) => {
+	return `min(${vwValue * 0.25}vw,${remValue * 0.0625}rem)`;
+};
+const valuesToSizes = (values) => {
+	const sizes = {};
+	[...range(1, 6)].reverse().forEach((i) => {
+		sizes[`h${i}`] = convertValuesToSizes(values[0][6 - i], values[1][6 - i]);
+		sizes[`p${i}`] = convertValuesToSizes(values[2][6 - i], values[3][6 - i]);
+	});
+	return sizes;
+};
+
+const steps = {
+	8: 0,
+	24: 1,
+	48: 2,
+	96: 4,
+	192: 8,
+	400: 16,
+};
+
+Catpow.Customize.FontSizes = (props) => {
+	const {
+		value: sizes,
+		onChange,
+		param: { roles },
+	} = props;
+
+	const rolesByShorthand = useMemo(() => Object.values(roles).reduce((p, c) => ({ ...p, [c.shorthand]: c }), {}), [roles]);
+
+	const [values, setValues] = useState(sizesToValues(sizes, rolesByShorthand));
+
+	const onChangeHandle = useCallback(
+		(values) => {
+			setValues(values);
+			onChange(valuesToSizes(values));
+		},
+		[onChange, setValues]
+	);
+
+	return (
+		<Bem>
+			<DataSet values={values} labels={labels} colors={colors} classNames={classNames} steps={steps} onChange={onChangeHandle}>
+				<Legend />
+				<LineChartInput width={300} height={600} />
+			</DataSet>
+		</Bem>
+	);
+};
