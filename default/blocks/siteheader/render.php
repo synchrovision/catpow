180d6b6ec13384d47d1a5e46d6999cc74c90912c
synchrovision <hatano@catpow.info>
2026-01-28 18:51:36 +0900

    edit 📝 update siteheader block

--- default/blocks/siteheader/render.php
+++ default/blocks/siteheader/render.php
@@ -1,35 +1,42 @@
 <?php
 namespace Catpow;
-$cls="wp-block-catpow-siteheader";
+HTML::render(['header#siteHeader.wp-block-catpow-siteheader--',
+	['div._contents',
+		['h1._logo',
+			get_custom_logo(),
+			['small._catch',get_bloginfo('description')]
+		]
+	],
+	['ul._menu',
+		'children'=>array_map(fn($item)=>['li._item',
+			['a._link','href'=>$item->url,$item->title]
+		],[...loop('nav/header')])
+	],
+	['ul._primary',
+		'children'=>array_map(fn($item)=>['li._item',
+			['a._link','href'=>$item->url,$item->title]
+		],[...loop('nav/primary')])
+	],
+	['button._button',
+		['span._icon',' ']
+	],
+	['button._back',
+		['span._icon',' ']
+	],
+]);
 ?>
-<header id="siteHeader" class="<?=$cls?>">
-	<div class="<?=$cls?>__contents">
-		<h1 class="<?=$cls?>__contents-logo">
-			<?php the_custom_logo(); ?>
-			<small class="<?=$cls?>__contents-logo-catch"><?= bloginfo('description');?></small>
-		</h1>
-	</div>
-	<div class="<?=$cls?>__menu has-color-scheme-inverted">
-		<?php menu('header/nav_menu'); ?>
-	</div>
-	<div class="<?=$cls?>__contact">
-		<?php menu('primary'); ?>
-	</div>
-
-	<button class="<?=$cls?>__button">
-		<span class="<?=$cls?>__button-icon"> </span>
-	</button>
-	<button class="<?=$cls?>__back"></button>
-	<script type="text/javascript">
-		document.addEventListener('DOMContentLoaded',function(){
-			const siteHeader=document.getElementById('siteHeader');
-			siteHeader.querySelector('.<?=$cls?>__button').addEventListener('click',()=>{
-				siteHeader.classList.toggle('is-open');
-			});
-			var reseizeObserver=new ResizeObserver(function(entries){
-				document.documentElement.style.setProperty('--cp-page-top-offset',siteHeader.getBoundingClientRect().bottom+'px');
-			});
-			reseizeObserver.observe(siteHeader);
+<script type="text/javascript">
+	document.addEventListener('DOMContentLoaded',function(){
+		const siteHeader=document.getElementById('siteHeader');
+		siteHeader.querySelector('.wp-block-catpow-siteheader__button').addEventListener('click',()=>{
+			siteHeader.classList.toggle('is-open');
 		});
-	</script>
-</header>
+		siteHeader.querySelector('.wp-block-catpow-siteheader__back').addEventListener('click',()=>{
+			window.scrollTo(0,0);
+		});
+		var reseizeObserver=new ResizeObserver(function(entries){
+			document.documentElement.style.setProperty('--cp-page-top-offset',siteHeader.getBoundingClientRect().bottom+'px');
+		});
+		reseizeObserver.observe(siteHeader);
+	});
+</script>
