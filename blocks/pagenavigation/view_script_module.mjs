69495456bb178dabe00ce0cfc5ebec3644c1b8bd
synchrovision <hatano@catpow.info>
2026-02-03 18:49:34 +0900

    chore 🆙 lint

--- blocks/pagenavigation/view_script_module.mjs
+++ blocks/pagenavigation/view_script_module.mjs
@@ -1,18 +1,16 @@
-import {store,getElement,getContext,withScope} from '@wordpress/interactivity';
+import { store, getElement, getContext, withScope } from "@wordpress/interactivity";
 
-
-store("pagenavigation",{
-	state:{
+store("pagenavigation", {
+	state: {},
+	actions: {
+		onClickToggle(e) {
+			const openItems = getContext().openItems;
+			const id = e.currentTarget.closest("[data-post-id]").dataset.postId;
+			openItems[id] = !openItems[id];
+		},
 	},
-	actions:{
-		onClickToggle(e){
-			const openItems=getContext().openItems;
-			const id=e.currentTarget.closest('[data-post-id]').dataset.postId;
-			openItems[id]=!openItems[id];
-		}
+	callbacks: {
+		isItemActive: () => getContext().activeItems[getElement().attributes["data-post-id"]],
+		isItemOpen: () => getContext().openItems[getElement().attributes["data-post-id"]],
 	},
-	callbacks:{
-		isItemActive:()=>getContext().activeItems[getElement().attributes['data-post-id']],
-		isItemOpen:()=>getContext().openItems[getElement().attributes['data-post-id']]
-	}
 });
