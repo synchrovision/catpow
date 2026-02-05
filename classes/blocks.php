439adbe589dc1597f934c135f7d96e5e3c6fe6c7
synchrovision <hatano@catpow.info>
2026-02-05 17:53:46 +0900

    edit 📝 enqueue dashicons at editor

--- classes/blocks.php
+++ classes/blocks.php
@@ -5,7 +5,7 @@ class blocks{
 	public static $deps=[
 		'editor_init'=>['wp-blocks','wp-i18n','wp-element','wp-editor','wp-plugins','wp-edit-post','catpow','catpow.colorTone','wpinfo'],
 		'editor_script'=>['wp-blocks','wp-i18n','wp-element','wp-editor','wp-plugins','wp-edit-post','catpow','catpow.colorTone','wpinfo'],
-		'editor_style'=>['wp-edit-blocks','font_awesome'],
+		'editor_style'=>['wp-edit-blocks','font_awesome','dashicons'],
 		'view_script'=>['catpow'],
 		'view_script_module'=>['@wordpress/interactivity'],
 		'view_style'=>[],
