6f03ee17425eb5c80c2ee99fe64d217d190cef0e
synchrovision <hatano@catpow.info>
2026-03-27 20:11:21 +0900

    chore 🆙 remove debug code

--- blocks/leavepopup/render.php
+++ blocks/leavepopup/render.php
@@ -1,7 +1,6 @@
 <?php
 namespace Catpow;
 if($attr['target']==='site' && $_SERVER['SERVER_NAME'] === parse_url($_SERVER['HTTP_REFERER']??'', PHP_URL_HOST)){
-	var_dump([$_SERVER['SERVER_NAME'] ,parse_url($_SERVER['HTTP_REFERER']??'', PHP_URL_HOST)]);
 	return;
 }
 echo $content;
