e95e48082203c9eee90e4dd2cab6c5dff51b0b77
synchrovision <hatano@catpow.info>
2026-04-29 11:47:21 +0900

    feat 👍 update formbuttons block

--- blocks/formbuttons/gen.php
+++ blocks/formbuttons/gen.php
@@ -1,15 +1,24 @@
+<?php
+$classes=$attr['classes']??'is-level3 has-item-size-medium';
+?>
 <!-- wp:catpow/formbuttons -->
-<ul class="wp-block-catpow-formbuttons buttons <?=$attr['classes']??''?>">
+<ul class="wp-block-catpow-formbuttons cp-buttons <?=$classes?>">
 <?php foreach((array)$attr['items'] as $item): ?>
-	<li class="item <?=$item['classes']??''?>">
-		<div class="button" data-action="<?=$item['action']??''?>" data-callback="<?=$item['callback']??''?>" data-target="<?=$item['target']??''?>" ignore-message="<?=$item['ignoreMessage']??''?>"<?=empty($item['event'])?'':sprintf(' data-event="%s"',$item['event'])?>>
-			<?php if(preg_match('/\bhasIcon\b/',$item['classes']??'')):?>
-				<span class="icon">
+	<li class="wp-block-catpow-formbuttons__item <?=$item['classes']??''?>">
+		<?php if(preg_match('/\bhas-micro-copy\b/',$classes)): ?>
+			<span class="wp-block-catpow-formbuttons__item-copy"><?=$item['microcopy']??''?></span>
+		<?php endif; ?>
+		<div class="wp-block-catpow-formbuttons-button" role="button" data-action="<?=$item['action']??''?>" data-event="<?=$item['event']??''?>" data-yss-event="<?=$item['yss-event']??''?>" data-yjad-event="<?=$item['yjad-event']??''?>" data-fbp-event="<?=$item['fbp-event']??''?>">
+			<?php if(preg_match('/\bhas-icon\b/',$item['classes']??'')):?>
+				<div class="wp-block-catpow-formbuttons-button__icon">
 					<img src="<?=$item['iconSrc']??''?>" alt="<?=$item['iconAlt']??''?>"/>
-				</span>
+				</div>
 			<?php endif; ?>
-			<?=$item['text']?>
+			<span class="wp-block-catpow-formbuttons-button__text"><?=$item['text']?></span>
 		</div>
+		<?php if(preg_match('/\bhas-caption\b/',$classes)): ?>
+			<span class="wp-block-catpow-formbuttons__item-caption"><?=$item['caption']??''?></span>
+		<?php endif; ?>
 	</li>
 <?php endforeach ?>
 </ul>
