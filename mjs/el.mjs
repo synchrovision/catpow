746568e8063935ac7426139f177f9d869042565b
synchrovision <hatano@catpow.info>
2026-05-11 10:10:32 +0900

    feat 👍 add mjs/el.mjs

---
+++ mjs/el.mjs
@@ -0,0 +1,34 @@
+export const el = (tag, props, children, namespace) => {
+	const el = namespace ? document.createElementNS(namespace, tag) : document.createElement(tag);
+	const appendChild = (child) => {
+		if (child == null) {
+			return;
+		}
+		if (child instanceof Node) {
+			el.appendChild(child);
+		} else if (typeof child === "string") {
+			el.appendChild(document.createTextNode(child));
+		} else if (Array.isArray(child)) {
+			child.forEach(appendChild);
+		} else {
+			console.error("can not append child : ", child);
+		}
+	};
+	if (props) {
+		Object.keys(props).forEach((key) => {
+			if (typeof props[key] === "function") {
+				const matches = key.match(/^on(\w+)$/);
+				if (matches) {
+					el.addEventListener(matches[1].toLowerCase(), props[key]);
+				}
+			} else {
+				el.setAttribute(key, props[key]);
+			}
+		});
+	}
+	appendChild(children);
+	return el;
+};
+export const xhtmlEl = (tag, props, children) => el(tag, props, children, "http://www.w3.org/1999/xhtml");
+export const svgEl = (tag, props, children) => el(tag, props, children, "http://www.w3.org/2000/svg");
+export const mathEl = (tag, props, children) => el(tag, props, children, "http://www.w3.org/1998/Math/MathML");
