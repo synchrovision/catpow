cf0a71dc1022c711e793b5ed769e9b9e65fdd2bb
synchrovision <hatano@catpow.info>
2026-06-09 11:51:03 +0900

    feat 👍 add nav block

---
+++ blocks/nav/conf.php
@@ -0,0 +1,6 @@
+<?php
+$attributes=[
+	'classes'=>['type'=>'string','default'=>'is-level3 has-item-size-small'],
+	'vars'=>['type'=>'object'],
+	'nav_name'=>['type'=>'string','default'=>key(cp::$config['nav_datas'])],
+];
