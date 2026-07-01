b1ddef7108d40df31f5c9883b82ad10a827dc8e2
synchrovision <hatano@catpow.info>
2026-07-01 18:17:08 +0900

    edit 📝 update buttons block

--- blocks/buttons/conf.php
+++ blocks/buttons/conf.php
@@ -3,7 +3,7 @@ $block_class='wp-block-catpow-buttons';
 $buttons_class='cp-buttons';
 $button_class='cp-button';
 $attributes=[
-	"classes"=>["source"=>'attribute',"selector"=>".{$block_class}","attribute"=>'class',"default"=>"{$block_class} {$buttons_class} has-item-size-medium"],
+	"classes"=>["source"=>'attribute',"selector"=>".{$block_class}","attribute"=>'class',"default"=>"{$block_class} {$buttons_class} is-level3 has-item-size-medium"],
 	'vars'=>['type'=>'object','default'=>[]],
 	"items"=>[
 		"source"=>'query',
@@ -23,7 +23,7 @@ $attributes=[
 		],
 		"default"=>[
 			[
-				"classes"=>"{$block_class}__item is-default",
+				"classes"=>"{$block_class}__item is-rank-primary",
 				"text"=>'お問合せ',
 				"url"=>'[home_url]/contact'
 			]
