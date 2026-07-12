a5b06cb5099922491cf4557ee897a4ef36c0047b
synchrovision <hatano@catpow.info>
2026-07-12 09:26:12 +0900

    feat 👍 getClosestBlockAttributesComputed function

--- blocks/_init/init/CP/functions/block.js
+++ blocks/_init/init/CP/functions/block.js
@@ -6,3 +6,24 @@
 		return wp.blocks.createBlock(...block);
 	});
 };
+export const getClosestBlockAttributesComputed = (callback, block, itemsKey, index, subItemsKey, subIndex) => {
+	if (itemsKey && block.attributes[itemsKey][index]) {
+		if (subItemsKey && block.attributes[itemsKey][index][subItemsKey][subIndex]) {
+			const result = callback(block.attributes[itemsKey][index][subItemsKey][subIndex]);
+			if (result != null) return result;
+		}
+		const result = callback(block.attributes[itemsKey][index]);
+		if (result != null) return result;
+	}
+	const result = callback(block.attributes);
+	if (result != null) return result;
+	const { getBlockParents, getBlockAttributes } = wp.data.select("core/block-editor");
+	for (const parentBlockClientId of getBlockParents(block.clientId, true)) {
+		const parentBlockAttributes = getBlockAttributes(parentBlockClientId);
+		if (parentBlockAttributes) {
+			const result = callback(parentBlockAttributes);
+			if (result != null) return result;
+		}
+	}
+	return null;
+};
