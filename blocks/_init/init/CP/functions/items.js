5820e00547d36387bf080b16e35bf0948e39a99c
synchrovision <hatano@catpow.info>
2026-07-12 15:57:10 +0900

    feat 👍 update items function

--- blocks/_init/init/CP/functions/items.js
+++ blocks/_init/init/CP/functions/items.js
@@ -4,29 +4,52 @@
 export const selectNextItem = (tag) => {
 	window.getSelection().anchorNode.parentNode.closest(tag).nextElementSibling.querySelector("[contentEditable]").focus();
 };
-export const saveItem = ({ items, itemsKey, set }) => {
-	set({ [itemsKey || "items"]: JSON.parse(JSON.stringify(items)) });
+export const getTheItemIndex = (props) => {
+	const { items, index, attributes, itemKeys = [] } = props;
+	if (items) return index;
+	const [itemsKey, itemIndex, subItemsKey, subItemIndex] = itemKeys;
+	if (subItemsKey) return subItemIndex;
+	return itemIndex;
+};
+export const getTheItem = (props) => {
+	var { items, index, attributes, itemKeys = [] } = props;
+	if (items) return items[index];
+	const [itemsKey, itemIndex, subItemsKey, subItemIndex] = itemKeys;
+	if (subItemsKey) return attributes?.[itemsKey]?.[itemIndex]?.[subItemsKey][subItemIndex];
+	return attributes?.[itemsKey]?.[itemIndex];
+};
+export const getTheItems = (props) => {
+	var { items, attributes, itemKeys = [] } = props;
+	if (items) return items;
+	const [itemsKey, itemIndex, subItemsKey] = itemKeys;
+	if (subItemsKey) return attributes?.[itemsKey]?.[itemIndex]?.[subItemsKey];
+	return attributes?.[itemsKey];
+};
+export const saveItem = ({ items, itemsKey, set, attributes, setAttributes, itemKeys }) => {
+	if (items) return set({ [itemsKey || "items"]: JSON.parse(JSON.stringify(items)) });
+	setAttributes({ [itemKeys[0]]: JSON.parse(JSON.stringify(attributes[itemKeys[0]])) });
 };
 export const deleteItem = (props) => {
-	var { items, index } = props;
-	items.splice(index, 1);
+	getTheItems(props).splice(getTheItemIndex(props), 1);
 	saveItem(props);
 };
 export const cloneItem = (props) => {
-	var { tag, items, index } = props;
-	items.splice(index, 0, JSON.parse(JSON.stringify(items[index])));
+	const { tag } = props;
+	getTheItems(props).splice(getTheItemIndex(props), 0, JSON.parse(JSON.stringify(getTheItem(props))));
 	saveItem(props);
 	selectNextItem(tag);
 };
 export const upItem = (props) => {
-	var { items, index } = props;
-	if (!items[index - 1]) return false;
-	items.splice(index - 1, 2, items[index], items[index - 1]);
+	const items = getTheItems(props);
+	const index = getTheItemIndex(props);
+	if (index <= 0) return false;
+	getTheItems(props).splice(index - 1, 2, items[index], items[index - 1]);
 	saveItem(props);
 };
 export const downItem = (props) => {
-	var { items, index } = props;
-	if (!items[index + 1]) return false;
+	const items = getTheItems(props);
+	const index = getTheItemIndex(props);
+	if (index > items.length - 2) return false;
 	items.splice(index, 2, items[index + 1], items[index]);
 	saveItem(props);
 };
