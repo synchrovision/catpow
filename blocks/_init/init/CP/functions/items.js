999a4a8633c08ab54fb3c3fd9c300e274dc34e88
synchrovision <hatano@catpow.info>
2026-07-14 08:13:22 +0900

    chore 🆙 remove deprecated functions

--- blocks/_init/init/CP/functions/items.js
+++ blocks/_init/init/CP/functions/items.js
@@ -53,23 +53,9 @@ export const downItem = (props) => {
 	items.splice(index, 2, items[index + 1], items[index]);
 	saveItem(props);
 };
-
-export const getItemByKeyAndIndex = ({ attr }, keys, index) => {
-	let item = attr || {};
-	if (keys) {
-		if (Array.isArray(keys)) {
-			console.assert(Array.isArray(index) && index.length === keys.length, "index and keys should be same length");
-			for (let i in keys) {
-				item = item?.[keys[i]]?.[index[i]];
-			}
-			return item || {};
-		} else {
-			const items = item[keys] || [];
-			item = items[index] || {};
-		}
-	}
-	return item;
-};
+/**
+ * @deprecated
+ **/
 export const updateItemByKeyAndIndex = ({ attr, set }, keys, index, item) => {
 	if (keys) {
 		if (Array.isArray(keys)) {
@@ -90,107 +76,4 @@ export const updateItemByKeyAndIndex = ({ attr, set }, keys, index, item) => {
 			set({ [keys]: Object.assign({}, attr[keys] || {}, item) });
 		}
 	}
-};
-
-export const switchItemColor = ({ items, index, set }, color, itemsKey) => {
-	if (itemsKey === undefined) {
-		itemsKey = "items";
-	}
-	let classArray = (items[index].classes || "").split(" ");
-	let i = classArray.findIndex((cls) => cls.slice(0, 5) === "color");
-	if (i === -1) {
-		if (color) {
-			classArray.push("color" + color);
-		}
-	} else {
-		if (color) {
-			classArray.splice(i, 1, "color" + color);
-		} else {
-			classArray.splice(i, 1);
-		}
-	}
-	items[index].classes = classArray.join(" ");
-	set({ [itemsKey]: JSON.parse(JSON.stringify(items)) });
-};
-export const getItemColor = ({ items, index }) => {
-	let c = (items[index].classes || "").split(" ").find((cls) => cls.slice(0, 5) === "color");
-	if (!c) {
-		return 0;
-	}
-	return parseInt(c.slice(5));
-};
-
-export const switchItemPattern = ({ items, index, set }, pattern, itemsKey) => {
-	if (itemsKey === undefined) {
-		itemsKey = "items";
-	}
-	let classArray = (items[index].classes || "").split(" ");
-	let i = classArray.findIndex((cls) => cls.slice(0, 7) === "pattern");
-	if (i === -1) {
-		if (pattern) {
-			classArray.push("pattern" + pattern);
-		}
-	} else {
-		if (pattern) {
-			classArray.splice(i, 1, "pattern" + pattern);
-		} else {
-			classArray.splice(i, 1);
-		}
-	}
-	items[index].classes = classArray.join(" ");
-	set({ [itemsKey]: JSON.parse(JSON.stringify(items)) });
-};
-export const getItemPattern = ({ items, index }) => {
-	let p = (items[index].classes || "").split(" ").find((cls) => cls.slice(0, 7) === "pattern");
-	if (!p) {
-		return 0;
-	}
-	return parseInt(p.slice(7));
-};
-
-export const switchItemSelectiveClass = ({ items, index, set }, values, value, itemsKey) => {
-	if (itemsKey === undefined) {
-		itemsKey = "items";
-	}
-	let classArray = (items[index].classes || "").split(" ");
-	if (!Array.isArray(values) && _.isObject(values)) {
-		values = Object.keys(values);
-	}
-	classArray = _.difference(classArray, values);
-	if (Array.isArray(value)) {
-		classArray = classArray.concat(value);
-	} else {
-		classArray.push(value);
-	}
-	items[index].classes = classArray.join(" ");
-	set({ [itemsKey]: JSON.parse(JSON.stringify(items)) });
-};
-export const getItemSelectiveClass = ({ items, index }, values) => {
-	if (!items[index].classes) {
-		return false;
-	}
-	let classArray = (items[index].classes || "").split(" ");
-	if (!Array.isArray(values) && _.isObject(values)) {
-		values = Object.keys(values);
-	}
-	return _.intersection(classArray, values).shift();
-};
-
-export const toggleItemClass = ({ items, index, set }, value, itemsKey) => {
-	if (itemsKey === undefined) {
-		itemsKey = "items";
-	}
-	let classArray = (items[index].classes || "").split(" ");
-	let i = classArray.indexOf(value);
-	if (i === -1) {
-		classArray.push(value);
-	} else {
-		classArray.splice(i, 1);
-	}
-	items[index].classes = classArray.join(" ");
-	set({ [itemsKey]: JSON.parse(JSON.stringify(items)) });
-};
-export const hasItemClass = ({ items, index }, value) => {
-	let classArray = (items[index].classes || "").split(" ");
-	return classArray.indexOf(value) !== -1;
 };
