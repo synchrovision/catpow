a1764080e96d1575b0c551e83752718dbde57854
synchrovision <hatano@catpow.info>
2026-08-21 19:44:38 +0900

    edit 📝 update extractEventDispatcherAttributes function

--- blocks/_init/init/CP/functions/event.js
+++ blocks/_init/init/CP/functions/event.js
@@ -1,11 +1,11 @@
 ﻿export const extractEventDispatcherAttributes = (blockTypeName, attributes, itemKeys) => {
 	const blockType = wp.data.select("core/blocks").getBlockType(blockTypeName);
-	const [itemsKey, itemIndex, subItemsKey, subItemIndex] = itemKeys;
+	const [itemsKey, subItemsKey] = itemKeys;
 	const item = CP.getTheItem({ attributes, itemKeys });
-	const atts = (subItemsKey ? blockType.attributes[itemsKey]?.query?.[subItemsKey] : blockType.attributes[itemsKey])?.eventDispatcherAttributes;
+	const atts = subItemsKey ? blockType.attributes[itemsKey]?.query?.[subItemsKey] : blockType.attributes[itemsKey];
 	const eventDispatcherAttributes = {};
-	if (atts?.query) {
-		atts.query.map((attr_name) => {
+	if (atts?.eventDispatcherAttributes) {
+		atts.eventDispatcherAttributes.map((attr_name) => {
 			eventDispatcherAttributes[atts.query[attr_name].attribute] = item[attr_name];
 		});
 	}
