1b51f9c263a3079cbc325e6505d10aadd5f1d516
synchrovision <hatano@catpow.info>
2026-08-21 18:39:50 +0900

    edit 📝 migrate block attributes filters

--- functions/embed_icon/blocks/_init/filters.php
+++ functions/embed_icon/blocks/_init/filters.php
@@ -10,8 +10,10 @@ add_filter('cp_block_attributes_iconHolder',function($atts,$args){
 },10,2);
 add_filter('cp_block_items_attributes_iconHolder',function($items,$args){
 	$items['query']['embedIconSrc']=array_merge(["source"=>'attribute',"selector"=>'.embed-icon',"attribute"=>'data-src'],$args);
-	for($i=0;$i<count($items['default']);$i++){
-		$items['default'][$i]['embedIconSrc']=\cp::get_file_url("images/dummy_icon.svg");
+	if(is_array($items['default'])){
+		for($i=0;$i<count($items['default']);$i++){
+			$items['default'][$i]['embedIconSrc']=\cp::get_file_url("images/dummy_icon.svg");
+		}
 	}
 	return $items;
 },10,2);
