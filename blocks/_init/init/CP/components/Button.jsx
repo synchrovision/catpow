63ede07222e6296008f0907e32343ba6b53e16df
synchrovision <hatano@catpow.info>
2026-08-21 18:47:16 +0900

    feat 👍 update Button component

--- blocks/_init/init/CP/components/Button.jsx
+++ blocks/_init/init/CP/components/Button.jsx
@@ -3,51 +3,63 @@ const { __ } = wp.i18n;
 const { RichText } = wp.blockEditor;
 
 export const Button = (props) => {
-	const { tag: Tag = "div", className, attributes, isLink = true, itemKeys, keys = {}, ...otherProps } = props;
+	const { tag: Tag = "div", blockTypeName, attributes, isLink = true, itemKeys, keys = {}, ...otherProps } = props;
 
 	const item = (itemKeys ? CP.getTheItem(props) : attributes) || {};
-	const states = CP.classNamesToFlags(className);
+	const states = CP.classNamesToFlags(item.buttonClasses);
 
 	const LinkTag = isLink ? "a" : "span";
-	const linkProps = isLink ? { href: item[keys.href] || "" } : {};
-	if (isLink && /^\w+:\/\//.test(item[keys.href])) {
+	const linkProps = isLink ? { href: item.buttonHref || "" } : {};
+	if (isLink && /^\w+:\/\//.test(item.buttonHref)) {
 		Object.assign(linkProps, { rel: "noopener", target: "_blank" });
 	}
 
 	return (
 		<CP.Bem prefix="wp-block-catpow">
-			<Tag className={className}>
-				{states.hasMicroCopy && <RichText.Content tagName="span" className="_copy cp-button__copy" value={item.copy} />}
-				<LinkTag className="-button cp-button__link" {...otherProps} {...linkProps}>
+			<Tag className={item.buttonClasses}>
+				{states.hasMicroCopy && <RichText.Content tagName="span" className="_copy cp-button__copy" value={item.buttonCopy} />}
+				<LinkTag className="-button cp-button__link" {...otherProps} {...CP.extractEventDispatcherAttributes(blockTypeName, attributes, itemKeys)} {...linkProps}>
 					{states.hasIcon && <CP.OutputIcon className="_icon cp-button__link-icon" item={item} />}
-					<RichText.Content tagName="span" className="_text cp-button__link-text" value={item.text} />
+					<RichText.Content tagName="span" className="_text cp-button__link-text" value={item.buttonText} />
 				</LinkTag>
-				{states.hasCaption && <RichText.Content tagName="span" className="_caption cp-button__caption" value={item.caption} />}
+				{states.hasCaption && <RichText.Content tagName="span" className="_caption cp-button__caption" value={item.buttonCaption} />}
 			</Tag>
 		</CP.Bem>
 	);
 };
 Button.Edit = (props) => {
-	const { tag = "div", className, setAttributes, attributes, isItem, itemKeys, keys = {}, ...otherProps } = props;
+	const { tag = "div", blockTypeName, setAttributes, attributes, isItem, itemKeys, keys = {}, ...otherProps } = props;
 	const { useMemo, useEffect, useState } = wp.element;
 
 	const item = useMemo(() => (itemKeys ? CP.getTheItem(props) : attributes) || {}, [attributes, itemKeys]);
-	const states = CP.classNamesToFlags(className);
+	const states = CP.classNamesToFlags(item.buttonClasses);
 
 	const ItemComponent = isItem ? CP.Item : PlainComponent;
 
 	return (
 		<CP.Bem prefix="wp-block-catpow">
-			<ItemComponent tag={tag} className={className} {...{ setAttributes, attributes, itemKeys }}>
+			<ItemComponent tag={tag} className={item.buttonClasses} {...{ setAttributes, attributes, itemKeys }}>
 				{states.hasMicroCopy && (
-					<RichText tagName="span" className="_copy cp-button__copy" value={item.copy} placeholder={__("マイクロコピー", "catpow")} onChange={(copy) => CP.updateItem(props, { copy })} />
+					<RichText
+						tagName="span"
+						className="_copy cp-button__copy"
+						value={item.buttonCopy}
+						placeholder={__("マイクロコピー", "catpow")}
+						onChange={(buttonCopy) => CP.updateItem(props, { buttonCopy })}
+					/>
 				)}
 				<CP.Link.Edit className="-button cp-button__link" {...{ setAttributes, attributes, itemKeys, keys }}>
 					{states.hasIcon && <CP.OutputIcon className="_icon cp-button__link-icon" item={item} />}
-					<RichText tagName="span" className="_text cp-button__link-text" value={item.text} onChange={(text) => CP.updateItem(props, { text })} />
+					<RichText tagName="span" className="_text cp-button__link-text" value={item.buttonText} onChange={(buttonText) => CP.updateItem(props, { buttonText })} />
 				</CP.Link.Edit>
 				{states.hasCaption && (
-					<RichText tagName="span" className="_caption cp-button__caption" value={item.caption} placeholder={__("キャプション", "catpow")} onChange={(caption) => CP.updateItem(props, { caption })} />
+					<RichText
+						tagName="span"
+						className="_caption cp-button__caption"
+						value={item.buttonCaption}
+						placeholder={__("キャプション", "catpow")}
+						onChange={(buttonCaption) => CP.updateItem(props, { buttonCaption })}
+					/>
 				)}
 			</ItemComponent>
 		</CP.Bem>
