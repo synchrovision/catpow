e942c932136067008e6589aefb77fc08ce0138bf
synchrovision <hatano@catpow.info>
2026-08-24 16:36:17 +0900

    edit 📝 update listed block

--- blocks/listed/conf.php
+++ blocks/listed/conf.php
@@ -2,7 +2,7 @@
 $block_class="wp-block-catpow-listed";
 $attributes=[
 	"isTemplate"=>["type"=>"boolean","default"=>false],
-	"classes"=>["source"=>"attribute","selector"=>"ul","attribute"=>"class","default"=>"wp-block-catpow-listed is-level3 is-type-card has-item-size-medium has-header has-title has-title-caption has-image has-text has-link"],
+	"classes"=>["source"=>"attribute","selector"=>"ul","attribute"=>"class","default"=>"wp-block-catpow-listed is-level3 is-type-card has-content-width has-spacing-type-block has-item-size-medium has-header has-title has-title-caption has-image has-text has-link"],
 	"commonItemClasses"=>["type"=>"string"],
 	"commonItemHeaderClasses"=>["type"=>"string"],
 	"vars"=>['type'=>'object'],
